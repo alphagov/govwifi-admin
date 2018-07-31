da3f99ac282f8b9aeb345da926b0a294b068e0d2
Tom Taylor <tommotaylor@gmail.com>
2018-07-31 14:06:57 +0100

    Finish draft signup as organisation

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,4 +1,7 @@
 class SignupsController < ApplicationController
   def start
   end
+
+  def organisation
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   get '/healthcheck', to: 'monitoring#healthcheck'
   get '/signup/start', to: 'signups#start'
+  get '/signup/organisation', to: 'signups#organisation'
 end
