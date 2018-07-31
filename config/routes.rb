ba8f1a61197564a4f4b87d87b122f8ebadfce8ad
Tom Taylor <tommotaylor@gmail.com>
2018-07-31 14:12:07 +0100

    Finish draft confirmation page

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
 
   get '/signup/start', to: 'signups#start'
   get '/signup/organisation', to: 'signups#organisation'
+  get '/signup/confirmation', to: 'signups#confirmation'
 end
