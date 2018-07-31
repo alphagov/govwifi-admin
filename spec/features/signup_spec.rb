31e8b540b34a175efa7490c6c9087e2c417f6928
Tom Taylor <tommotaylor@gmail.com>
2018-07-31 12:48:29 +0100

    Finish draft of signup start page

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -2,8 +2,15 @@ RSpec.feature "Signup Process" do
   context "as an Organisation" do
     it "should direct the user through the flow" do
       visit "/signup/start"
+      
       expect(page).to have_css("h1.govuk-heading-xl", text: "For Users")
       expect(page).to have_css("h1.govuk-heading-xl", text: "For Teams")
+      expect(page).to have_css("button.govuk-button", text: "How to Sign Up")
+      expect(page).to have_css("button.govuk-button", text: "Sign up Organisation")
+      expect(page).to have_css("a", text: "Login")
+      expect(page).to have_css("h2.govuk-heading-l", text: "What is GovWifi?")
+      expect(page).to have_css("h2.govuk-heading-l", text: "Benefits")
+      expect(page).to have_css("h2.govuk-heading-l", text: "How it works")
     end
   end
 end
