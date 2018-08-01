d21f3c78b0fb6f2476f04ed89e6ac5f7f3fecd82
Adrian Clay <adrian@madetech.com>
2018-08-01 14:44:16 +0100

    Add support for IE8
    Pull in the IE8 specific stylesheets, which restores functionality
    within IE8.
    
    Also add the HTML5 shiv.
    
    This was following the guidance from
     https://github.com/alphagov/govuk-frontend/blob/master/docs/installation/supporting-internet-explorer-8.md

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,12 +5,15 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w(
   assets/images/govuk-logotype-crown.png
+  application_ie8.css
+  html5shiv.js
 )
 
 
 Rails.application.config.assets.paths += [
   Rails.root.join('node_modules/govuk-frontend/assets/images'),
   Rails.root.join('node_modules/govuk-frontend/assets/fonts'),
-  Rails.root.join('node_modules/govuk-frontend')
+  Rails.root.join('node_modules/govuk-frontend'),
+  Rails.root.join('node_modules/html5shiv/dist')
 ]
 
