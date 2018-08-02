e110bbbc24fa97ce4d620457de4e3b960ab84e46
Tom Taylor <tommotaylor@gmail.com>
2018-08-02 16:06:07 +0100

    Fix before actions

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,8 +1,7 @@
 class ConfirmationsController < Devise::ConfirmationsController
   # Remove the first skip_before_filter (:require_no_authentication) if you
   # don't want to enable logged users to access the confirmation page.
-  skip_before_filter :require_no_authentication
-  skip_before_filter :authenticate_user!
+  skip_before_action :authenticate_user!
 
   # PUT /resource/confirmation
   def update
