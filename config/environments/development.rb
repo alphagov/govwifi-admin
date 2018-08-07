d356171d8688996de44366c0b1b07ec847710587
Tom Taylor <tommotaylor@gmail.com>
2018-08-07 12:14:10 +0100

    Remove paperclip reference

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,8 +48,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  # Paperclip gem
-  # config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.perform_deliveries = false
   config.action_mailer.raise_delivery_errors = true
