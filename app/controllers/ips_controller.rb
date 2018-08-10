537ad00e6e1dcc8e56df4cfffc10706031b38e23
Tom Taylor <tommotaylor@gmail.com>
2018-08-10 11:01:26 +0100

    Save IP from form

--- app/controllers/ips_controller.rb
+++ app/controllers/ips_controller.rb
@@ -1,6 +1,17 @@
 class IpsController < ApplicationController
   skip_before_action :authenticate_user!
-  
+
   def new
+    @ip = Ip.new
+  end
+
+  def create
+    Ip.create(ip_params)
+  end
+
+private
+
+  def ip_params
+    params.require(:ip).permit(:address)
   end
 end
--- spec/features/add_an_ip_address_spec.rb
+++ spec/features/add_an_ip_address_spec.rb
@@ -6,4 +6,10 @@ describe 'Add an IP to my account' do
   it 'displays the page' do
     expect(page).to have_content('Add an IP')
   end
+
+  it 'creates the IP' do
+    fill_in 'address', with: '123213213'
+    click_on 'save'
+    expect(Ip.count).to eq(1)
+  end
 end
