41fce174a252fcd7c415b5b8e8e983f6ca8d14ef
Kyle <kjdchapman@gmail.com>
2018-08-13 14:20:52 +0100

    Change hash arguments for linting

--- spec/use_cases/administrator/check_if_whitelisted_email_spec.rb
+++ spec/use_cases/administrator/check_if_whitelisted_email_spec.rb
@@ -3,31 +3,31 @@ describe CheckIfWhitelistedEmail do
 
   it 'rejects an empty email address' do
     result = subject.execute('')
-    expect(result).to eq({ success: false })
+    expect(result).to eq(success: false)
   end
 
   it 'approves an address from the gov.uk domain' do
     result = subject.execute('someone@gov.uk')
-    expect(result).to eq({ success: true })
+    expect(result).to eq(success: true)
   end
 
   it 'approves an address from a gov.uk subdomain' do
     result = subject.execute('someone@other.gov.uk')
-    expect(result).to eq({ success: true })
+    expect(result).to eq(success: true)
   end
 
   it 'rejects an address not from a gov.uk domain' do
     result = subject.execute('someone@notgov.uk')
-    expect(result).to eq({ success: false })
+    expect(result).to eq(success: false)
   end
 
   it 'rejects an address not from a gov.uk subdomain' do
     result = subject.execute('someone@other.notgov.uk')
-    expect(result).to eq({ success: false })
+    expect(result).to eq(success: false)
   end
 
   it 'rejects an address with gov in the local-part' do
     result = subject.execute('fake.gov.uk@unrelateddomain.com')
-    expect(result).to eq({ success: false })
+    expect(result).to eq(success: false)
   end
 end
