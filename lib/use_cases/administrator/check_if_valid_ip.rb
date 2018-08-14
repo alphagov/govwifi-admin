25b320f41a0a715b96d61ad0a78e6b30278da4aa
Tom Taylor <tommotaylor@gmail.com>
2018-08-14 16:57:56 +0100

    Restrict to simple IPv4 IPs only

--- lib/use_cases/administrator/check_if_valid_ip.rb
+++ lib/use_cases/administrator/check_if_valid_ip.rb
@@ -1,16 +1,25 @@
 class CheckIfValidIp
   def execute(address)
-    { success: address_valid?(address) }
+    { success: address_valid_for_radius?(address) }
   end
 
 private
 
-  def address_valid?(address)
+  def address_valid_for_radius?(address)
     return false if address.nil?
+    return false if address_is_subnet?(address)
 
+    address_is_ipv4?(address)
+  end
+
+  def address_is_subnet?(address)
+    address.include?("/")
+  end
+
+  def address_is_ipv4?(address)
     begin
-      IPAddr.new(address)
-      true
+      ip_object = IPAddr.new(address)
+      ip_object.ipv4?
     rescue IPAddr::InvalidAddressError
       false
     end
--- spec/use_cases/administrator/check_if_valid_ip_spec.rb
+++ spec/use_cases/administrator/check_if_valid_ip_spec.rb
@@ -48,27 +48,29 @@ describe CheckIfValidIp do
     end
   end
 
-  context 'valid' do
-    context 'with a valid IPv4 address' do
-      let(:address) { '172.16.254.1' }
-
-      it 'returns true' do
-        result = subject.execute(address)
-        expect(result).to eq(success: true)
-      end
-    end
-
-    context 'with a valid suffixed IPv4 address' do
+  context 'not IPv4' do
+    context 'with a valid subnet IPv4 address' do
       let(:address) { '192.168.1.15/24' }
 
-      it 'returns true' do
+      it 'returns false' do
         result = subject.execute(address)
-        expect(result).to eq(success: true)
+        expect(result).to eq(success: false)
       end
     end
 
     context 'with a valid IPv6 address' do
       let(:address) { '2001:db8:0:1234:0:567:8:1' }
+
+      it 'returns false' do
+        result = subject.execute(address)
+        expect(result).to eq(success: false)
+      end
+    end
+  end
+
+  context 'valid' do
+    context 'with a valid IPv4 address' do
+      let(:address) { '172.16.254.1' }
 
       it 'returns true' do
         result = subject.execute(address)
