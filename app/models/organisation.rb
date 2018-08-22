cf9071f59117110a088e86e5bfb712d119534fc3
Tom Taylor <tommotaylor@gmail.com>
2018-08-22 15:39:25 +0100

    Add validation to name

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,3 +1,5 @@
 class Organisation < ApplicationRecord
   has_many :users
+
+  validates_uniqueness_of :name
 end
--- spec/models/organisation_spec.rb
+++ spec/models/organisation_spec.rb
@@ -1,5 +1,6 @@
 describe Organisation do
   context 'associations' do
     it { should have_many(:users) }
+    it { should validate_uniqueness_of(:name) }
   end
 end
