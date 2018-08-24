d56d7d01ffd0a73ae3c658be5bc582470c7baf09
Tom Taylor <tommotaylor@gmail.com>
2018-08-24 12:27:43 +0100

    Reinsert password confirmation check

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -6,7 +6,10 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
 
   def update
     with_unconfirmed_confirmable do
-      if @confirmable.update(user_params)
+      if user_params[:password] != user_params[:password_confirmation]
+        @confirmable.errors.add(:password, "must match confirmation")
+        render_show_page
+      elsif @confirmable.update(user_params)
         confirm_user
       else
         render_show_page
