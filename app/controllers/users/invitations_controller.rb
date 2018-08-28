f4cef425a4ba809c5566629c2d6ae649f118ad6d
Tom Taylor <tommotaylor@gmail.com>
2018-08-28 15:09:32 +0100

    Set organisation id in create rather than new

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,11 +1,11 @@
 class Users::InvitationsController < Devise::InvitationsController
-  # Overrides https://github.com/scambra/devise_invitable/blob/master/app/controllers/devise/invitations_controller.rb#L12
-  def new
-    self.resource = User.new(organisation_id: current_user.organisation_id)
-    render :new
-  end
+  before_action :add_organisation_to_params, only: :create
 
 private
+
+  def add_organisation_to_params
+    params[:user][:organisation_id] = current_user.organisation_id
+  end
 
   # Overrides https://github.com/scambra/devise_invitable/blob/master/app/controllers/devise/invitations_controller.rb#L105
   def invite_params
