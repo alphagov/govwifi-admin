c062c1cf5c847a973c5d2086891bd009298641f2
Kyle <kjdchapman@gmail.com>
2018-08-28 17:07:58 +0100

    Improve UX of add IP page (#78)
    This commit changes various markup and messages to better illustrate 
    to users the purpose and functionality of the 'Add IPs' page.
    
    It also modifies the feature specs that were checking against these
    pages.
    
    Finally, it makes sure there are no hanging reference to 'Locations', as 
    these are going to be a separate concept to IPs, and we don't want to 
    cause confusion.

--- spec/features/support/activation_notice.rb
+++ spec/features/support/activation_notice.rb
@@ -1,5 +1,5 @@
 shared_examples 'shows activation notice' do
-  it 'tells the user it takes 24 hours to activate' do
-    expect(page).to have_content '24 Hours to Activate'
+  it 'tells the user IPs are not immediately available' do
+    expect(page).to have_content 'New IPs are not immediately available'
   end
 end
