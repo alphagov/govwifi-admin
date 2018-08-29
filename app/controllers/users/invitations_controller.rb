cf948e2ab9dc253def6822a19625b130850d76c7
Tom Taylor <tommotaylor@gmail.com>
2018-08-29 16:37:30 +0100

    Refactor validate method in invitations controller

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -3,14 +3,26 @@ class Users::InvitationsController < Devise::InvitationsController
 
 private
 
-  def validate_invited_user
-    return if User.find_by(email: invite_params[:email])
-    @user = User.new(invite_params)
-    render :new, resource: @user unless @user.validate
-  end
-
   def add_organisation_to_params
     params[:user][:organisation_id] = current_user.organisation_id
+  end
+
+  def validate_invited_user
+    return if invited_user_already_exists?
+    render_invite_user_page if user_is_invalid?
+  end
+
+  def user_is_invalid?
+    @user = User.new(invite_params)
+    !@user.validate
+  end
+
+  def render_invite_user_page
+    render :new, resource: @user
+  end
+
+  def invited_user_already_exists?
+    !!User.find_by(email: invite_params[:email])
   end
 
   # Overrides https://github.com/scambra/devise_invitable/blob/master/app/controllers/devise/invitations_controller.rb#L105
--- spec/features/team/resend_invitation_spec.rb
+++ spec/features/team/resend_invitation_spec.rb
@@ -24,33 +24,33 @@ describe "Resend invitation to team member" do
       expect { click_button("resend invite") }.to \
         change { InviteUseCaseSpy.invite_count }.by(1)
     end
-  end
 
-  context "signup from resent invitation" do
-    let(:invite_link) { InviteUseCaseSpy.last_invite_url }
-    let(:invited_user) { User.find_by(email: invited_user_email) }
+    context "signup from resent invitation" do
+      let(:invite_link) { InviteUseCaseSpy.last_invite_url }
+      let(:invited_user) { User.find_by(email: invited_user_email) }
 
-    before do
-      visit invite_link
-    end
-
-    it "displays the sign up page" do
-      expect(page).to have_content("Create your account")
-    end
-
-    context "signing up as an invited user" do
       before do
-        fill_in "Your name", with: "Ron Swanson"
-        fill_in "Password", with: "password"
-        fill_in "Confirm password", with: "password"
-        click_on "Create my account"
+        visit invite_link
       end
 
-      it "confirms the user" do
-        expect(invited_user.confirmed?).to eq(true)
-        expect(invited_user.name).to eq("Ron Swanson")
-        expect(page).to have_content("Logout")
-        expect(page).to have_content(user.organisation.name)
+      it "displays the sign up page" do
+        expect(page).to have_content("Create your account")
+      end
+
+      context "signing up as an invited user" do
+        before do
+          fill_in "Your name", with: "Ron Swanson"
+          fill_in "Password", with: "password"
+          fill_in "Confirm password", with: "password"
+          click_on "Create my account"
+        end
+
+        it "confirms the user" do
+          expect(invited_user.confirmed?).to eq(true)
+          expect(invited_user.name).to eq("Ron Swanson")
+          expect(page).to have_content("Logout")
+          expect(page).to have_content(user.organisation.name)
+        end
       end
     end
   end
