9d31182d4784391e13574fb3a0c2097418012ecc
Kyle <kjdchapman@gmail.com>
2018-08-29 16:55:01 +0100

    Redirect back to "List" page when IPs created (#83)
    This commit adds some basic styling for the "IP added" success message
    that shows after this new pathing.
    
    This success message is shown separately to error messages, and 
    required the suppression of a Devise message - isn't ideal, but 
    works for now.
    
    Also further improves the specs around this functionality - better 
    failure data, better layout, less testing of internals.

--- spec/features/add_an_ip_address_spec.rb
+++ spec/features/add_an_ip_address_spec.rb
@@ -9,61 +9,78 @@ describe 'Add an IP to my account' do
   include_examples 'confirmation use case spy'
   include_examples 'notifications service'
 
-  context 'when logged out' do
-    before do
-      visit new_ip_path
-    end
-
-    it_behaves_like 'not signed in'
-  end
-
   context 'when logged in' do
-    let(:user) { create(:user, :confirmed, :with_organisation) }
     before do
-      sign_in_user user
+      sign_in_user create(:user)
       visit ips_path
-      expect(page).to have_content "Add IP"
       click_on "Add IP Address"
     end
 
-    context 'before saving the IP' do
-      it_behaves_like 'shows activation notice'
+    it_behaves_like 'shows activation notice'
 
-      it 'displays the form' do
-        expect(page).to have_content('Enter IP Address (IPv4 only)')
+    it 'asks me to enter an IP' do
+      expect(page).to have_content('Enter IP Address (IPv4 only)')
+    end
+
+    context 'and that IP is valid' do
+      before do
+        fill_in 'address', with: '10.0.0.1'
+        click_on 'Save'
+      end
+
+      it 'shows a success message' do
+        expect(page).to have_content('IP Added')
+      end
+
+      it 'shows me the new IP' do
+        expect(page).to have_content('10.0.0.1')
+      end
+
+      context 'when I add a second IP' do
+        before do
+          visit new_ip_path
+          fill_in 'address', with: '10.0.0.2'
+          click_on 'Save'
+        end
+
+        it 'shows both new IPs' do
+          expect(page).to have_content('10.0.0.1')
+          expect(page).to have_content('10.0.0.2')
+        end
       end
     end
 
-    context 'with an invalid IP address' do
+    context 'and that IP is invalid' do
       before do
-        fill_in 'address', with: 'InvalidIP'
-        expect { click_on 'Save' }.to change { Ip.count }.by(0)
+        fill_in 'address', with: '10.wrong.0.1'
+        click_on 'Save'
       end
 
       it_behaves_like 'errors in form'
 
-      it 'displays the form with error message' do
+      it 'asks me to re-enter my IP' do
         expect(page).to have_content('Enter IP Address')
+      end
+
+      it 'tells me what I entered was invalid' do
         expect(page).to have_content(
           'Address must be a valid IPv4 address (without subnet)'
         )
       end
-    end
 
-    context 'after successfully saving an IP' do
-      before do
-        fill_in 'address', with: '10.0.0.1'
-        expect { click_on 'Save' }.to change { Ip.count }.by 1
-      end
+      context 'when looking back at the list' do
+        before { visit ips_path }
 
-      it_behaves_like 'shows activation notice'
-
-      it 'shows the successful confirmation page' do
-        expect(Ip.first.user).to eq(user)
-        expect(page).to have_content('IP Added')
-        expect(page).to have_content(Ip.first.address)
-        expect(page).to have_content(user.radius_secret_key)
+        it 'has not added the invalid IP' do
+          expect(page).to_not have_content('10.wrong.0.1')
+        end
       end
     end
+  end
+
+  context 'when logged out' do
+    before { visit new_ip_path }
+
+    it_behaves_like 'not signed in'
   end
 end
