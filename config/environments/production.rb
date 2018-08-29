09391862dfebd15f597f3196b541bb3ff25a6b92
Emile <emile.swarts123@gmail.com>
2018-08-29 17:59:15 +0100

    Missed config values for staging and production
    We stub out AWS in development and test, but want it function as normal
    on staging and production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host: "admin-platform.wifi.service.gov.uk" }
+  config.aws_config = {}
 end
