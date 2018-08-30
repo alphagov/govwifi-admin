0a363c7febe5e5000b6033adbf932f4a45634f02
Emile <emile.swarts123@gmail.com>
2018-08-30 10:46:15 +0100

    Style health checks and display region of server
    Make the health check listing look more reasonable
    This doesn't include the navigation which will come in the next PR

--- app/controllers/health_checks_controller.rb
+++ app/controllers/health_checks_controller.rb
@@ -3,6 +3,9 @@ class HealthChecksController < ApplicationController
     london_ips = ENV.fetch('LONDON_RADIUS_IPS').split(',')
     dublin_ips = ENV.fetch('DUBLIN_RADIUS_IPS').split(',')
 
-    @health_checks = UseCases::Administrator::HealthChecks::CalculateHealth.new.execute(ips: london_ips + dublin_ips)
+    health_checks = UseCases::Administrator::HealthChecks::CalculateHealth.new.execute(ips: london_ips + dublin_ips)
+
+    @health_checks_dublin = health_checks.select { |h| dublin_ips.include?(h.fetch(:ip_address)) }
+    @health_checks_london = health_checks.select { |h| london_ips.include?(h.fetch(:ip_address)) }
   end
 end
