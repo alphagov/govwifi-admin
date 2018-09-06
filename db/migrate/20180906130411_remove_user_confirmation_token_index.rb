fab70e39262c72c3cbeeed3641d60554f43cf0db
Tom Taylor <tommotaylor@gmail.com>
2018-09-06 14:05:49 +0100

    Remove Users Confirmation Token Index

---
+++ db/migrate/20180906130411_remove_user_confirmation_token_index.rb
@@ -0,0 +1,5 @@
+class RemoveUserConfirmationTokenIndex < ActiveRecord::Migration[5.2]
+  def change
+    remove_index :users, column: :confirmation_token
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_31_103946) do
+ActiveRecord::Schema.define(version: 2018_09_06_130411) do
 
   create_table "ips", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "address"
@@ -53,7 +53,6 @@ ActiveRecord::Schema.define(version: 2018_08_31_103946) do
     t.bigint "invited_by_id"
     t.integer "invitations_count", default: 0
     t.bigint "organisation_id"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["invitations_count"], name: "index_users_on_invitations_count"
