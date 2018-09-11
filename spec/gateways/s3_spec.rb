8271f570bb70069ea0218fdfbe9f4f6fc9ab57f0
Tom Taylor <tommotaylor@gmail.com>
2018-09-11 17:05:03 +0100

    Remove unnecessary stubs from specs

--- spec/gateways/s3_spec.rb
+++ spec/gateways/s3_spec.rb
@@ -5,23 +5,6 @@ describe Gateways::S3 do
 
   subject { described_class.new(bucket: bucket, key: key) }
 
-  before do
-    ENV['AWS_CONTAINER_CREDENTIALS_RELATIVE_URI'] = '/stubUri'
-
-    stub_request(:get, 'http://169.254.170.2/stubUri').to_return(body: {
-      'AccessKeyId': 'ACCESS_KEY_ID',
-      'Expiration': (Time.now + 60).iso8601,
-      'RoleArn': 'TASK_ROLE_ARN',
-      'SecretAccessKey': 'SECRET_ACCESS_KEY',
-      'Token': 'SECURITY_TOKEN_STRING'
-    }.to_json)
-
-    stub_request(:put, "https://s3.eu-west-1.amazonaws.com/#{bucket}/#{key}") \
-      .with({
-        body: data.to_json
-      })
-  end
-
   it 'uploads the data' do
     expect(subject.upload(data: data)).to eq({})
   end
