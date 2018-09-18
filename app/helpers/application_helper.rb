53a48f512349855150bd33353175669be12d6427
Tom Taylor <tommotaylor@gmail.com>
2018-09-18 18:34:36 +0100

    WIP

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def field_error(resource, key)
     resource&.errors&.include?(key.to_sym) ? "govuk-form-group--error" : ""
   end
+
+  def active?(path)
+    if current_controller == path
+    end
+  end
 end
