a90c6a3d1d318b0b93e9991e6b7baa5228d1c204
Emile Swarts <emile@madetech.com>
2018-09-19 17:04:04 +0100

    Display the location of an IP on the IP index page (#137)
    Also align the "Add IP Address" button to the right to be consistent
    with the button on the team page.
    
    Seed data for IP addresses and locations.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,18 @@
 organisation = Organisation.create(name: "Parks & Recreation Dept")
 user = organisation.users.create(email: "test@gov.uk", password: "password", name: "Steve")
 user.confirm
+
+location_1 = Location.create!(
+  address: 'Momentum Centre, London',
+  postcode: 'SE10SX',
+  organisation_id: organisation.id
+)
+
+location_2 = Location.create!(
+  address: '136 Southwark Street, London',
+  postcode: 'E33EH',
+  organisation_id: organisation.id
+)
+
+Ip.create!(address: '127.3.3.1', location: location_1)
+Ip.create!(address: '193.1.1.9', location: location_2)
