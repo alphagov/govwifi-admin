25a958cb1f9200716152254ceeebd24a113f26c1
Tom Taylor <tommotaylor@gmail.com>
2018-09-19 09:11:45 +0100

    Sub-navigation styling (#136)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def field_error(resource, key)
     resource&.errors&.include?(key.to_sym) ? "govuk-form-group--error" : ""
   end
+
+  def active_tab(path)
+    request.path == path ? "active" : ""
+  end
 end
