e8ef92fd7d65cf25273a9ab044ccd8e671022f5a
George Schena <40758489+georgeschena@users.noreply.github.com>
2018-09-25 12:21:14 +0100

    Redirect user after making support request (#156)
    * WIP: Support email confirmation test
    
    * WIP: implement mock
    
    * Complete unit test
    
    * Test passes
    
    * Redirect to root path on submit
    
    * Request support flash message and redirect bug fix
    
    * Clean up code
    
    * Split spec with two expectations into one
    
    * Change string path to variable & ignore DS_Store
    
    * Delete DS_Store
    
    * Add space after require
    
    * Add redirect test
    
    * Remove brackets to maintain consistency
    
    * Remove trailing whitespace

---
+++ spec/features/contact_us_submit_spec.rb
@@ -0,0 +1,39 @@
+require 'features/support/sign_up_helpers'
+require 'support/notifications_service'
+require 'support/send_help_email_use_case_spy'
+require 'support/send_help_email_use_case'
+
+describe 'contact us page' do
+  include_examples 'notifications service'
+  include_examples 'send help email use case spy'
+
+  let(:user) { create(:user, :confirmed, :with_organisation) }
+
+  context 'with support ticket details filled in' do
+    before do
+      sign_in_user user
+      visit help_index_path
+      fill_in 'contact_number', with: '11111111112'
+      fill_in 'subject', with: 'Subject'
+      fill_in 'details', with: 'Details'
+    end
+
+    it 'submitting request explains a support request has been submitted' do
+      click_on 'Submit'
+      expect(page).to have_content('Your support request has been submitted')
+    end
+
+    it 'redirects to the home page' do
+      click_on 'Submit'
+      expect(page.current_path).to eq(root_path)
+    end
+
+    it 'submitting request sends an email to support' do
+      expect {
+        click_on 'Submit'
+      }.to change {
+        SendHelpEmailSpy.support_emails_sent_count
+      }.by(1)
+    end
+  end
+end
---
+++ spec/support/send_help_email_use_case_spy.rb
@@ -0,0 +1,23 @@
+# rubocop:disable Style/ClassVars
+class SendHelpEmailSpy
+  @@support_emails_count = 0
+
+  class << self
+    def support_emails_sent_count
+      @@support_emails_count
+    end
+
+    def clear!
+      @@support_emails_count = 0
+    end
+  end
+
+  # rubocop:disable Lint/UnusedMethodArgument
+  def execute(email:, sender_email:, name:, organisation:, details:, phone:, subject:, template_id:)
+    @@support_emails_count += 1
+
+    {}
+  end
+  # rubocop:enable Lint/UnusedMethodArgument
+end
+# rubocop:enable Style/ClassVars
