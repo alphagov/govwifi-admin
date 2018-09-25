59a048066ec9789270d0d076527036bf910fa6a5
George Schena <40758489+georgeschena@users.noreply.github.com>
2018-09-25 17:00:31 +0100

    Link new user to add IP's and locations (#158)
    Add test to inform new users to add IP's and locations.
    Add conditional to display guidance message when no IP's and locations are saved.
    Rearrange instructions on home page in logical order.

--- spec/features/guidance_after_sign_in_spec.rb
+++ spec/features/guidance_after_sign_in_spec.rb
@@ -3,44 +3,52 @@ require 'support/notifications_service'
 
 describe 'guidance after sign in' do
   let(:user) { create(:user, :confirmed, :with_organisation) }
-  let!(:location) { create(:location, organisation: user.organisation) }
+  before { sign_in_user user }
 
-  before do
-    sign_in_user user
-    visit '/'
+  context 'without locations' do
+    before { visit root_path }
+
+    it 'displays message to inform user to add IPs and locations' do
+      expect(page).to have_content 'RADIUS secret keys will be generated'
+    end
   end
 
-  it 'shows me the landing guidance' do
-    expect(page).to have_content 'Get GovWifi'
-    expect(page).to have_content 'Getting help'
-  end
+  context 'with locations' do
+    let!(:location) { create(:location, organisation: user.organisation) }
+    before { visit root_path }
 
-  it 'displays radius connection details' do
-    expect(page).to have_content(location.radius_secret_key)
-  end
-
-  context 'with radius IPs in env-vars' do
-    let(:radius_ip_1) { '111.111.111.111' }
-    let(:radius_ip_2) { '121.121.121.121' }
-    let(:radius_ip_3) { '131.131.131.131' }
-    let(:radius_ip_4) { '141.141.141.141' }
-
-    before do
-      ENV['LONDON_RADIUS_IPS'] = "#{radius_ip_1},#{radius_ip_2}"
-      ENV['DUBLIN_RADIUS_IPS'] = "#{radius_ip_3},#{radius_ip_4}"
+    it 'shows me the landing guidance' do
+      expect(page).to have_content 'Get GovWifi'
+      expect(page).to have_content 'Getting help'
     end
 
-    it 'displays RADIUS settings' do
-      expect(page).to have_content('RADIUS')
-      expect(page).to have_content('London')
-      expect(page).to have_content('Dublin')
+    it 'displays radius connection details' do
+      expect(page).to have_content(location.radius_secret_key)
     end
 
-    it 'displays the correct IPs' do
-      expect(page).to have_content(radius_ip_1)
-      expect(page).to have_content(radius_ip_2)
-      expect(page).to have_content(radius_ip_3)
-      expect(page).to have_content(radius_ip_4)
+    context 'with radius IPs in env-vars' do
+      let(:radius_ip_1) { '111.111.111.111' }
+      let(:radius_ip_2) { '121.121.121.121' }
+      let(:radius_ip_3) { '131.131.131.131' }
+      let(:radius_ip_4) { '141.141.141.141' }
+
+      before do
+        ENV['LONDON_RADIUS_IPS'] = "#{radius_ip_1},#{radius_ip_2}"
+        ENV['DUBLIN_RADIUS_IPS'] = "#{radius_ip_3},#{radius_ip_4}"
+      end
+
+      it 'displays RADIUS settings' do
+        expect(page).to have_content('RADIUS')
+        expect(page).to have_content('London')
+        expect(page).to have_content('Dublin')
+      end
+
+      it 'displays the correct IPs' do
+        expect(page).to have_content(radius_ip_1)
+        expect(page).to have_content(radius_ip_2)
+        expect(page).to have_content(radius_ip_3)
+        expect(page).to have_content(radius_ip_4)
+      end
     end
   end
 end
