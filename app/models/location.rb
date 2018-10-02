0193f0c67f7a2bbd3797d2bda522cc778aff8feb
George Schena <george@madetech.com>
2018-10-02 17:09:44 +0100

    Add validation method for location address

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,11 +2,20 @@ class Location < ApplicationRecord
   belongs_to :organisation
   has_many :ips
 
+  validates :address, presence: true
+
   before_create :set_radius_secret_key
 
 private
 
   def set_radius_secret_key
     self.radius_secret_key = RadiusSecretKeyGenerator.new.execute
+  end
+
+  def location_address_must_be_valid
+    checker = UseCases::Administrator::CheckIfValidLocationAddress.new
+    valid_location = checker.execute(self.address)[:success]
+    message = 'field cannot be empty'
+    errors.add(:address, message) unless valid_location
   end
 end
