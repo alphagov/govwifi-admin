15eda983574f7fb6d828b2645880185ff9782ee4
Renny Fadoju <rennyvbirds@yahoo.com>
2018-10-04 09:55:47 +0100

    Remove unused code

--- app/models/location.rb
+++ app/models/location.rb
@@ -11,11 +11,5 @@ private
   def set_radius_secret_key
     self.radius_secret_key = RadiusSecretKeyGenerator.new.execute
   end
-
-  def location_address_must_be_valid
-    checker = UseCases::Administrator::CheckIfValidLocationAddress.new
-    valid_location = checker.execute(self.address)[:success]
-    message = 'field cannot be empty'
-    errors.add(:address, message) unless valid_location
-  end
+  
 end
