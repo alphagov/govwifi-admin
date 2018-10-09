aee7d20700d93d5a23d75f9cd2cacd8f192b6e5f
Omorinsola Renny Fadoju <32823756+Renny4Real@users.noreply.github.com>
2018-10-09 16:19:58 +0100

    Revert "Display a list of signed up organisations (#176)"
    This reverts commit 8558981a962086e1ce8b74425f3cd63fbbea0e26.

--- db/migrate/20181009090135_add_admin_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddAdminToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :admin, :bool, default: false
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-require 'faker'
-
-admin_organisation = Organisation.create(name: "Super Admins")
-admin_user = admin_organisation.users.create(email: "admin@gov.uk", password: "password", name: "Steve", admin: true)
-admin_user.confirm
 
 organisation = Organisation.create(name: "Parks & Recreation Dept")
 user = organisation.users.create(email: "test@gov.uk", password: "password", name: "Steve")
@@ -24,5 +19,3 @@ location_2 = Location.create!(
 
 Ip.create!(address: '127.3.3.1', location: location_1)
 Ip.create!(address: '193.1.1.9', location: location_2)
-
-5.times { Organisation.create(name: Faker::Company.name) }
