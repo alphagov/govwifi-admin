f6d1f4da4acc7066c9720a708424d5ba8d5de532
George Schena <george@madetech.com>
2018-10-10 11:51:31 +0100

    Add test for if an MoU is signed

---
+++ spec/features/check_if_signed_mou_spec.rb
@@ -0,0 +1,23 @@
+describe 'when a user signs in it asks the users to go and sign their MoU' do
+  include_examples 'confirmation use case spy'
+  include_examples 'notifications service'
+
+  let!(:user) { create(:user, :confirmed, :with_organisation) }
+
+  before do
+    sign_in_user user
+    visit root_path
+  end
+
+  context 'user doesnt have a signed MoU' do
+    it 'shows the user a prompt to sign their MoU form' do
+      expect(page).to have_content("Please sign your Memorandum of Understanding (MoU)")
+    end
+
+    context 'user has signed their MoU form' do
+      it 'does not show a prompt' do
+        expect(page).to have_content("Get GovWifi Access in your Organisation")
+      end
+    end
+  end
+end
