a8c291c9f15ba179bec0d86b80ecfd2285560a99
George Schena <george@madetech.com>
2018-10-10 16:55:19 +0100

    Add tests for filtering logs

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -3,5 +3,7 @@ class LogsController < ApplicationController
     @logs = UseCases::Administrator::GetAuthRequestsForUsername.new(
       authentication_logs_gateway: Gateways::LoggingApi.new
     ).execute(username: params[:username])
+
+    # @raw_logs.filter{|log| log is from an ip that is in the current_organisation.ips array}
   end
 end
--- spec/features/logging/view_auth_requests_for_a_username_spec.rb
+++ spec/features/logging/view_auth_requests_for_a_username_spec.rb
@@ -2,8 +2,11 @@ require 'features/support/sign_up_helpers'
 
 describe "View authentication requests for a username" do
   context "with results" do
-    let(:user) { create(:user, :confirmed, :with_organisation) }
     let(:username) { "AAAAAA" }
+    let(:organisation) { create(:organisation) }
+    let(:admin_user) { create(:user, :confirmed, organisation_id: organisation.id) }
+    let(:location) { create(:location, organisation_id: organisation.id) }
+    let(:ip) { create(:ip, location_id: location.id, address: "1.1.1.1" )}
     let(:logs) do
       [
         {
@@ -11,7 +14,7 @@ describe "View authentication requests for a username" do
           "building_identifier" => "",
           "id" => 1,
           "mac" => "",
-          "siteIP" => "",
+          "siteIP" => "1.1.1.1",
           "start" => "2018-10-01 18:18:09 +0000",
           "stop" => nil,
           "success" => true,
@@ -22,7 +25,18 @@ describe "View authentication requests for a username" do
           "building_identifier" => "",
           "id" => 1,
           "mac" => "",
-          "siteIP" => "",
+          "siteIP" => "1.1.1.1",
+          "start" => "2018-10-01 18:18:09 +0000",
+          "stop" => nil,
+          "success" => true,
+          "username" => username
+        },
+        {
+          "ap" => "",
+          "building_identifier" => "",
+          "id" => 1,
+          "mac" => "",
+          "siteIP" => "2.2.2.2",
           "start" => "2018-10-01 18:18:09 +0000",
           "stop" => nil,
           "success" => true,
@@ -42,7 +56,7 @@ describe "View authentication requests for a username" do
           })
         .to_return(status: 200, body: logs.to_json, headers: {})
 
-      sign_in_user user
+      sign_in_user admin_user
       visit search_logs_path
       fill_in "username", with: username
       click_on "Submit"
@@ -51,13 +65,21 @@ describe "View authentication requests for a username" do
     it "displays the authentication requests" do
       expect(page).to have_content("Displaying logs for #{username}")
     end
+
+    it "displays the logs of the ips owned by the organisation location" do
+      expect(page).to have_content("1.1.1.1")
+    end
+
+    it "does not display the logs of the ip the organisation does not own" do
+      expect(page).to_not have_content("2.2.2.2")
+    end
   end
 
   context "without results" do
     let(:user) { create(:user, :confirmed, :with_organisation) }
 
     before do
-      sign_in_user user
+      sign_in_user admin_user
       visit search_logs_path
       fill_in "username", with: ""
       click_on "Submit"
