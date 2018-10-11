c25104d835fbab2e64e72395fae44f247631d9f5
Tom Taylor <tommotaylor@gmail.com>
2018-10-11 11:58:09 +0100

    Improve logs page views

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,7 +1,23 @@
 class LogsController < ApplicationController
   def index
+    redirect_to :search_logs, alert: generate_notice if invalid_username?
+
     @logs = UseCases::Administrator::GetAuthRequestsForUsername.new(
       authentication_logs_gateway: Gateways::LoggingApi.new
     ).execute(username: params[:username])
+  end
+
+private
+
+  def generate_notice
+    "Username must be 6 characters in length."
+  end
+
+  def invalid_username?
+    username.nil? || username.empty? || username.length != 6
+  end
+
+  def username
+    @username ||= params[:username]
   end
 end
--- spec/features/logging/view_auth_requests_for_a_username_spec.rb
+++ spec/features/logging/view_auth_requests_for_a_username_spec.rb
@@ -3,7 +3,6 @@ require 'features/support/sign_up_helpers'
 describe "View authentication requests for a username" do
   context "with results" do
     let(:user) { create(:user, :confirmed, :with_organisation) }
-    let(:username) { "AAAAAA" }
     let(:logs) do
       [
         {
@@ -25,7 +24,7 @@ describe "View authentication requests for a username" do
           "siteIP" => "",
           "start" => "2018-10-01 18:18:09 +0000",
           "stop" => nil,
-          "success" => true,
+          "success" => false,
           "username" => username
         }
       ]
@@ -48,18 +47,56 @@ describe "View authentication requests for a username" do
       click_on "Submit"
     end
 
-    it "displays the authentication requests" do
-      expect(page).to have_content("Displaying logs for #{username}")
+    context "when username is correct" do
+      let(:username) { "AAAAAA" }
+
+      it "displays the authentication requests" do
+        expect(page).to have_content("Displaying logs for #{username}")
+        expect(page).to have_content("successful")
+        expect(page).to have_content("failed")
+        expect(page).to_not have_content("+0000")
+      end
+    end
+
+    context "when username is too short" do
+      let(:username) { "1" }
+
+      it "displays the search page with an error" do
+        expect(page).to have_content("There is a problem")
+        expect(page).to have_content("Username must be 6 characters in length")
+        expect(page).to have_content("Find authentication requests for a username")
+      end
+    end
+
+    context "when username is too long" do
+      let(:username) { "12345678" }
+
+      it "displays the search page with an error" do
+        expect(page).to have_content("There is a problem")
+        expect(page).to have_content("Username must be 6 characters in length")
+        expect(page).to have_content("Find authentication requests for a username")
+      end
     end
   end
 
   context "without results" do
     let(:user) { create(:user, :confirmed, :with_organisation) }
+    let(:username) { "random" }
 
     before do
+      endpoint = ENV['LOGGING_API_SEARCH_ENDPOINT'] + username
+      stub_request(:get, endpoint)
+        .with(headers: {
+            'Accept' => '*/*',
+            'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'Host' => 'govwifi-logging-api.gov.uk',
+            'User-Agent' => 'Ruby'
+          })
+        .to_return(status: 200, body: [].to_json, headers: {})
+
       sign_in_user user
       visit search_logs_path
-      fill_in "username", with: ""
+      fill_in "username", with: username
       click_on "Submit"
     end
 
