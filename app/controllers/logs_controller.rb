9a31bf58f6c31ab0a3308dcb9c3b20c48f28fccc
Tom Taylor <tommotaylor@gmail.com>
2018-10-11 14:38:57 +0100

    Error handling for searching logs (#187)
    * Improve logs page views
    
    * Sort results by most recent

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,7 +1,29 @@
 class LogsController < ApplicationController
   def index
-    @logs = UseCases::Administrator::GetAuthRequestsForUsername.new(
+    redirect_to :search_logs, alert: alert if invalid_username?
+
+    results = UseCases::Administrator::GetAuthRequestsForUsername.new(
       authentication_logs_gateway: Gateways::LoggingApi.new
     ).execute(username: params[:username])
+
+    @logs = sort_results_by_most_recent(results)
+  end
+
+private
+
+  def alert
+    "Username must be 6 characters in length."
+  end
+
+  def invalid_username?
+    username.nil? || username.empty? || username.length != 6
+  end
+
+  def username
+    @username ||= params[:username]
+  end
+
+  def sort_results_by_most_recent(results)
+    results.sort_by { |log| log["start"] }.reverse
   end
 end
