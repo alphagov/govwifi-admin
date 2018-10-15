03266a13ef43d5f385af96ee39ecf8fc48a68bf0
Kyle <kjdchapman@gmail.com>
2018-10-15 16:23:25 +0100

    Refactor support email spec
    - Rename spec
    - Remove unused requires
    - Switch from shared_example to shared_context, renaming file and
    descriptor accordingly
    - Bring mock and context-defining into one file to help readability

--- spec/support/with_send_help_email_mocked.rb
+++ spec/support/with_send_help_email_mocked.rb
@@ -1,3 +1,15 @@
+shared_context 'with SendHelpEmail mocked' do
+  before do
+    allow(UseCases::Administrator::SendHelpEmail)
+      .to receive(:new)
+      .and_return(SendHelpEmailSpy.new)
+  end
+
+  after do
+    SendHelpEmailSpy.clear!
+  end
+end
+
 # rubocop:disable Style/ClassVars
 class SendHelpEmailSpy
   @@support_emails_count = 0
