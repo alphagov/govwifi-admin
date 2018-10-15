9b1984730de0ded17bd7eee6087a589d5ce2e417
Kyle <kjdchapman@gmail.com>
2018-10-15 12:04:17 +0100

    Show location postcode in list of IPs, if available (#196)

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,18 +1,47 @@
 describe Location do
+  it { is_expected.to validate_presence_of(:address) }
+
   context 'associations' do
-    it { should belong_to(:organisation) }
-    it { should have_many(:ips) }
+    it { is_expected.to belong_to(:organisation) }
+    it { is_expected.to have_many(:ips) }
   end
 
-  describe "#save" do
-    let(:organisation) { create(:organisation) }
+  describe '#save' do
     subject { build(:location, organisation: organisation) }
-    before do
-      subject.save
-    end
+
+    let(:organisation) { create(:organisation) }
+
+    before { subject.save }
 
     it 'sets the radius_secret_key' do
       expect(subject.radius_secret_key).to be_present
+    end
+  end
+
+  describe '#full_address' do
+    before do
+      subject.address = address
+      subject.postcode = postcode
+    end
+
+    context 'with address' do
+      let(:address) { '121 Fictional Street' }
+
+      context 'and postcode' do
+        let(:postcode) { 'FI5 S67' }
+
+        it 'combines the two' do
+          expect(subject.full_address).to eq('121 Fictional Street, FI5 S67')
+        end
+      end
+
+      context 'but a blank postcode' do
+        let(:postcode) { '' }
+
+        it 'only returns address' do
+          expect(subject.full_address).to eq('121 Fictional Street')
+        end
+      end
     end
   end
 end
