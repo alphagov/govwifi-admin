95e99028ef33f56e8a93b6782e7ca644b6b53a31
Emile Swarts <emile@madetech.com>
2018-10-15 15:18:03 +0100

    Extract log search query validator (#205)
    This will be used to ensure that search queries are valid for viewing
    logs.

---
+++ lib/use_cases/administrator/validate_log_search_query.rb
@@ -0,0 +1,23 @@
+module UseCases
+  module Administrator
+    class ValidateLogSearchQuery
+      def execute(params)
+        { success: success?(params) }
+      end
+
+    private
+
+      def success?(params)
+        valid_username?(params.fetch(:username)) || valid_ip?(params.fetch(:ip))
+      end
+
+      def valid_username?(username)
+        username.present? && username.length > 5
+      end
+
+      def valid_ip?(ip)
+        UseCases::Administrator::CheckIfValidIp.new.execute(ip).fetch(:success)
+      end
+    end
+  end
+end
