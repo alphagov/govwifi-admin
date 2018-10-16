44f7d361f8e1e4c39e7925e44fa65501ebe88a01
Emile Swarts <emile@madetech.com>
2018-10-16 17:23:00 +0100

    We now require a service email on organisation (#227)
    Add this to the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,11 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 require 'faker'
 
-admin_organisation = Organisation.create(name: "Super Admins")
+admin_organisation = Organisation.create(name: "Super Admins", service_email: 'it@gds.com')
 admin_user = admin_organisation.users.create(email: "admin@gov.uk", password: "password", name: "Steve", admin: true)
 admin_user.confirm
 
-organisation = Organisation.create(name: "Parks & Recreation Dept")
+organisation = Organisation.create(name: "Parks & Recreation Dept", service_email: 'it@parks.com')
 user = organisation.users.create(email: "test@gov.uk", password: "password", name: "Steve")
 user.confirm
 
@@ -25,6 +25,6 @@ location_2 = Location.create!(
 Ip.create!(address: '127.3.3.1', location: location_1)
 Ip.create!(address: '193.1.1.9', location: location_2)
 
-5.times { Organisation.create(name: Faker::Company.name) }
+5.times { Organisation.create(name: Faker::Company.name, service_email: Faker::Company.email) }
 
 MouTemplate.create!
