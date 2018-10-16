ecc57ffbc9f11b8349d3ce450a98876d696cfd92
Jakub Naliwajek <jakub@madetech.com>
2018-10-16 15:49:16 +0100

    Redirect admin based on type of the MOU upload

--- app/controllers/admins/mou_controller.rb
+++ app/controllers/admins/mou_controller.rb
@@ -5,19 +5,23 @@ class Admins::MouController < ApplicationController
     @mou = AdminConfig.mou
   end
 
-  def update
+  def create
+    params[:id].present? ? attach_to_organisation : attach_to_template
+  end
+
+private
+
+  def attach_to_organisation
     Organisation.find(params[:id]).signed_mou.attach(params[:signed_mou])
     flash[:notice] = "MOU uploaded successfully."
     redirect_to organisations_path
   end
 
-  def create
+  def attach_to_template
     AdminConfig.mou.unsigned_document.attach(params[:unsigned_document])
     flash[:notice] = "MOU template uploaded successfully."
     redirect_to admins_mou_index_path
   end
-
-private
 
   def authorise_admin
     redirect_to root_path unless current_user.admin?
