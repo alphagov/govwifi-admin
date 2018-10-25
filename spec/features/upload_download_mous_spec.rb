3d81392ae1bc8cb7b9a878df88393551fa434621
George Schena <george@madetech.com>
2018-10-25 16:10:35 +0100

    Remove depreciated test

--- spec/features/upload_download_mous_spec.rb
+++ spec/features/upload_download_mous_spec.rb
@@ -25,9 +25,6 @@ describe 'the upload and download of MOUs' do
 
         expect(page).to have_content("MOU uploaded successfully.")
 
-        click_on 'Download your signed MOU'
-
-        expect(page.body).to eq("12334567 signed mou with content\n")
       end
     end
 
