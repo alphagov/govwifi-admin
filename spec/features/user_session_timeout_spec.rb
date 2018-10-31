033d9a56d3b5b99479493dbe6a275cad7919408d
Renny Fadoju <rennyvbirds@yahoo.com>
2018-10-31 17:03:15 +0000

    Edit test for timeout session

--- spec/features/user_session_timeout_spec.rb
+++ spec/features/user_session_timeout_spec.rb
@@ -2,28 +2,27 @@ require 'features/support/sign_up_helpers'
 require 'features/support/not_signed_in'
 require 'timecop'
 
-describe 'user sessions timeout ', focus: true do
+describe 'user sessions timeout' do
   after { Timecop.return }
 
   let(:user) { create(:user, :confirmed, :with_organisation) }
-  context 'when logged out' do
+
+  context 'when user has not signed in' do
     before { visit root_path }
 
     it_behaves_like 'not signed in'
   end
 
-  context 'when user is signed in' do
-    context 'and they are inactive for 60 minutes' do
-      before do 
-        sign_in_user user
-        visit root_path
-        Timecop.freeze(Time.now + (60 * 60))
-      end
-    end
+  context 'when user has signed in' do
+    before { sign_in_user user }
 
-    it 'will log the user out' do
+    context 'and they have been inactive for 60 minutes' do
+      before do
+        Timecop.freeze(Time.now + (60 * 60))
+        visit root_path
+      end
+
       it_behaves_like 'not signed in'
     end
-    
   end
 end
