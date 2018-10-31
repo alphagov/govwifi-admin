57fae0879e438b7173ba703242f5d240b4dc2036
Renny Fadoju <rennyvbirds@yahoo.com>
2018-10-31 15:47:30 +0000

    Add timeoutable symbol to devise method in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   accepts_nested_attributes_for :organisation
 
   devise :invitable, :confirmable, :database_authenticatable, :registerable, :recoverable,
-    :rememberable, :trackable, :validatable
+    :rememberable, :trackable, :timeoutable, :validatable
 
   validates :name, presence: true, on: :update
 
