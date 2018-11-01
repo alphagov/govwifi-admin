d3fc5b13ff4015effb226a305efd8f1c7023fa00
Renny Fadoju <rennyvbirds@yahoo.com>
2018-11-01 15:56:40 +0000

    Edit descriptions in test blocks

--- spec/features/user_session_timeout_spec.rb
+++ spec/features/user_session_timeout_spec.rb
@@ -2,7 +2,7 @@ require 'features/support/sign_up_helpers'
 require 'features/support/not_signed_in'
 require 'timecop'
 
-describe 'with devise timeoutable enabled' do
+describe 'logout users after specific period of inactivity' do
 
   context 'when user has not signed in' do
     before { visit root_path }
@@ -10,26 +10,24 @@ describe 'with devise timeoutable enabled' do
     it_behaves_like 'not signed in'
   end
 
-  describe 'user sessions timeout' do
-    context 'when user has signed in' do
-      let(:user) { create(:user, :confirmed, :with_organisation) }
+  context 'when user has signed in' do
+    let(:user) { create(:user, :confirmed, :with_organisation) }
 
-      before do
+    before do
+      sign_in_user user
+      visit root_path
+    end
+
+    context 'and they have been inactive for 60 minutes' do
+      before { Timecop.travel(Time.now + (60 * 60)) { visit root_path } }
+
+      it_behaves_like 'not signed in'
+
+      it 'and they navigate to a page' do
         sign_in_user user
-        visit root_path
-      end
 
-      context 'and they have been inactive for 60 minutes' do
-        before { Timecop.travel(Time.now + (60 * 60)) { visit root_path } }
-
-        it_behaves_like 'not signed in'
-
-        it 'and they navigate to a page' do
-          sign_in_user user
-
-          visit team_members_path
-          expect(page).to have_content('Invite team member')
-        end
+        visit team_members_path
+        expect(page).to have_content('Invite team member')
       end
     end
   end
