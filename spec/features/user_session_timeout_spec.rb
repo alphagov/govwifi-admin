cdf2e71a0c3e11cdc1161b56abc38c0ccb38a2f3
Renny Fadoju <rennyvbirds@yahoo.com>
2018-11-01 16:07:21 +0000

    Edit spec layout and language

--- spec/features/user_session_timeout_spec.rb
+++ spec/features/user_session_timeout_spec.rb
@@ -3,25 +3,22 @@ require 'timecop'
 
 describe 'logout users after specific period of inactivity' do
 
-  context 'when user has signed in' do
+  context 'when a signed in user has been inactive for an hour' do
     let(:user) { create(:user, :confirmed, :with_organisation) }
 
     before do
       sign_in_user user
       visit root_path
+      Timecop.travel(Time.now + (60 * 60)) { visit root_path }
     end
 
-    context 'and they have been inactive for 60 minutes' do
-      before { Timecop.travel(Time.now + (60 * 60)) { visit root_path } }
+    it_behaves_like 'not signed in'
 
-      it_behaves_like 'not signed in'
-
-      it 'and they navigate to a page' do
-        sign_in_user user
-
-        visit team_members_path
-        expect(page).to have_content('Invite team member')
-      end
+    it 'they navigate to a page after signing in again' do
+      sign_in_user user
+      visit team_members_path
+      
+      expect(page).to have_content('Invite team member')
     end
   end
 end
