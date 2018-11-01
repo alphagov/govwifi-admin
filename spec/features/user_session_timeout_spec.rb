15e9e0a7be0315b7ffffde8139158b8269f10480
Renny Fadoju <rennyvbirds@yahoo.com>
2018-11-01 16:38:58 +0000

    Use helpers to describe time passage

--- spec/features/user_session_timeout_spec.rb
+++ spec/features/user_session_timeout_spec.rb
@@ -8,7 +8,7 @@ describe 'logout users after specific period of inactivity' do
     before do
       sign_in_user user
       visit root_path
-      Timecop.travel(Time.now + (59 * 60)) { visit root_path }
+      Timecop.travel(Time.now + (59.minutes)) { visit root_path }
     end
 
     it 'and they navigate to a page' do
@@ -22,7 +22,7 @@ describe 'logout users after specific period of inactivity' do
     before do
       sign_in_user user
       visit root_path
-      Timecop.travel(Time.now + (60 * 60)) { visit root_path }
+      Timecop.travel(Time.now + (1.hour)) { visit root_path }
     end
 
     it_behaves_like 'not signed in'
