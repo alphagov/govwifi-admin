69f3a3b6d2b37d91b32c9497da70537936ef19a6
George Schena <george@madetech.com>
2018-11-01 20:24:22 +0000

    WIP: Unlock user account using devise

--- app/mailers/authentication_mailer.rb
+++ app/mailers/authentication_mailer.rb
@@ -33,22 +33,24 @@ class AuthenticationMailer < ::Devise::Mailer
     )
   end
 
-  def unlock_instructions(record, token, opts = {})
-    confirmation_instructions(record, record.confirmation_token, opts) unless record.confirmed?
+  def unlock_instructions(record, token, opts={})
+    @token = token
+    devise_mail(record, :unlock_instructions, opts)
 
-    reset_link = edit_password_url(record, unlock_instructions: token)
-    template_id = GOV_NOTIFY_CONFIG['reset_password_email']['template_id']
+    # reset_link = edit_password_url(record, unlock_instructions: token)
+    # template_id = GOV_NOTIFY_CONFIG['reset_password_email']['template_id']
 
-    pp reset_link
+    # # pp reset_link
 
-    UseCases::Administrator::SendResetPasswordEmail.new(
-      notifications_gateway: EmailGateway.new
-    ).execute(
-      email: record.email,
-      reset_url: reset_link,
-      template_id: template_id
-    )
+    # UseCases::Administrator::SendResetPasswordEmail.new(
+    #   notifications_gateway: EmailGateway.new
+    # ).execute(
+    #   email: record.email,
+    #   reset_url: reset_link,
+    #   template_id: template_id
+    # )
   end
+
 
   def invitation_instructions(record, token, _opts = {})
     invite_link = accept_invitation_url(record, invitation_token: token)
