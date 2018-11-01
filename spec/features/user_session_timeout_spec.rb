b718f45227403a399350c5e6415ef3faa1773f94
Renny Fadoju <rennyvbirds@yahoo.com>
2018-11-01 13:02:47 +0000

    Reorganise layout of spec

--- spec/features/user_session_timeout_spec.rb
+++ spec/features/user_session_timeout_spec.rb
@@ -2,7 +2,7 @@ require 'features/support/sign_up_helpers'
 require 'features/support/not_signed_in'
 require 'timecop'
 
-describe 'user sessions timeout' do
+describe 'with devise timeoutable enabled' do
   after { Timecop.return }
 
   context 'when user has not signed in' do
@@ -11,21 +11,23 @@ describe 'user sessions timeout' do
     it_behaves_like 'not signed in'
   end
 
-  context 'when user has signed in' do
-    let(:user) { create(:user, :confirmed, :with_organisation) }
+  describe 'user sessions timeout' do
+    context 'when user has signed in' do
+      let(:user) { create(:user, :confirmed, :with_organisation) }
 
-    before do
-      sign_in_user user
-      visit root_path
-    end
-
-    context 'and they have been inactive for 60 minutes' do
       before do
-        Timecop.freeze(Time.now + (60 * 60))
+        sign_in_user user
         visit root_path
       end
 
-      it_behaves_like 'not signed in'
+      context 'and they have been inactive for 60 minutes' do
+        before do
+          Timecop.travel(Time.now + (60 * 60))
+          visit root_path
+        end
+
+        it_behaves_like 'not signed in'
+      end
     end
   end
 end
