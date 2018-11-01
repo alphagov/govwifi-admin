0662a8430c1cfa08a58659e3a5ed3cc3c3d369a7
Renny Fadoju <rennyvbirds@yahoo.com>
2018-11-01 16:13:17 +0000

    Add test for edgecase

--- spec/features/user_session_timeout_spec.rb
+++ spec/features/user_session_timeout_spec.rb
@@ -2,10 +2,24 @@ require 'features/support/sign_up_helpers'
 require 'timecop'
 
 describe 'logout users after specific period of inactivity' do
+  let(:user) { create(:user, :confirmed, :with_organisation) }
+
+  context 'when a signed in user has been inactive for 59 minutes' do
+    before do
+      sign_in_user user
+      visit root_path
+      Timecop.travel(Time.now + (59 * 60)) { visit root_path }
+    end
+
+    it 'and they navigate to a page' do
+      visit team_members_path
+
+      expect(page).to have_content('Invite team member')
+    end
+
+  end
 
   context 'when a signed in user has been inactive for an hour' do
-    let(:user) { create(:user, :confirmed, :with_organisation) }
-
     before do
       sign_in_user user
       visit root_path
@@ -14,10 +28,10 @@ describe 'logout users after specific period of inactivity' do
 
     it_behaves_like 'not signed in'
 
-    it 'they navigate to a page after signing in again' do
+    it 'and they navigate to a page after signing in again' do
       sign_in_user user
       visit team_members_path
-      
+
       expect(page).to have_content('Invite team member')
     end
   end
