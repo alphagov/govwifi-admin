b1b5583d19f05b6ec90d8f795cd7c13c38b31796
George Schena <george@madetech.com>
2018-11-01 16:42:21 +0000

    Add password reset email when locked out of account

--- spec/features/authentication/lock_user_account_spec.rb
+++ spec/features/authentication/lock_user_account_spec.rb
@@ -1,13 +1,5 @@
-require 'features/support/not_signed_in'
-require 'features/support/sign_up_helpers'
-require 'support/notifications_service'
-require 'support/confirmation_use_case'
-
 describe "Locking a users account after too many failed login attempts" do
-  include_examples 'confirmation use case spy'
   include_examples 'notifications service'
-
-  after { Timecop.return }
 
   let(:correct_password) { 'password' }
   let(:incorrect_password) { 'incorrectpassword' }
@@ -31,39 +23,19 @@ describe "Locking a users account after too many failed login attempts" do
         fill_in 'Password', with: incorrect_password
         click_on 'Continue'
       end
-      expect(page).to have_content 'Your account is locked.'
-    end
-  end
-
-  context "after the user locks their accont" do
-    it "unlocks the users account after one hour" do
-      10.times do
-        fill_in 'Email', with: user.email
-        fill_in 'Password', with: incorrect_password
-        click_on 'Continue'
+        expect(page).to have_content 'Your account has been locked. Reset your password'
       end
 
-      Timecop.freeze(Time.now + 61.minutes)
-
-      fill_in 'Email', with: user.email
-      fill_in 'Password', with: correct_password
-      click_on 'Continue'
-      expect(page).to have_content 'Signed in successfully.'
-    end
-
-    it "won't unlock their account if its under one hour" do
-      10.times do
-        fill_in 'Email', with: user.email
-        fill_in 'Password', with: incorrect_password
-        click_on 'Continue'
-      end
-
-      Timecop.freeze(Time.now + 59.minutes)
-
-      fill_in 'Email', with: user.email
-      fill_in 'Password', with: correct_password
-      click_on 'Continue'
-      expect(page).to have_content 'Your account is locked.'
+    it "locks the users account after ten failed attempts and sends an email" do
+      allow(UseCases::Administrator::SendResetPasswordEmail).to receive(:new).and_return(ResetPasswordUseCaseSpy.new)
+      expect {
+        10.times do
+          fill_in 'Email', with: user.email
+          fill_in 'Password', with: incorrect_password
+          click_on 'Continue'
+        end
+      }.to change { ResetPasswordUseCaseSpy.reset_count }.by(1)
+      expect(page).to have_content 'Your account has been locked. Reset your password'
     end
   end
 end
