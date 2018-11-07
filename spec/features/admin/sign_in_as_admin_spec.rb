8a3ab04a90a7e1deebaa4770198175a059ab9e5f
Renny Fadoju <rennyvbirds@yahoo.com>
2018-11-07 14:05:02 +0000

    Remove unnecessary tests and refactor spec

--- spec/features/admin/sign_in_as_admin_spec.rb
+++ spec/features/admin/sign_in_as_admin_spec.rb
@@ -6,19 +6,13 @@ describe 'signing as an admin user' do
 
   context 'when visiting the home page' do
     before do
-      5.times { create(:organisation) }
+      create(:organisation, name: "TestMe & Company")
       sign_in_user user
       visit root_path
     end
 
-    it 'redirects to the organisations page' do
-      expect(page.current_path).to eq(organisations_path)
-    end
-
     it 'shows list of signed organisations on the home page' do
-      Organisation.all.each do |organisation|
-        expect(page).to have_content(organisation.name)
-      end
+      expect(page).to have_content("TestMe & Company")
     end
   end
 end
