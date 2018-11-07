3ab6337301255916329a29dc1f593fb0b5d7b59a
Omorinsola Renny Fadoju <32823756+Renny4Real@users.noreply.github.com>
2018-11-07 14:40:54 +0000

    Make the root page for super admins the list of organisations (#286)

---
+++ spec/features/admin/sign_in_as_admin_spec.rb
@@ -0,0 +1,18 @@
+require 'features/support/sign_up_helpers'
+require 'features/support/not_signed_in'
+
+describe 'signing as an admin user' do
+  let!(:user) { create(:user, :confirmed, admin: true) }
+
+  context 'when visiting the home page' do
+    before do
+      create(:organisation, name: "TestMe & Company")
+      sign_in_user user
+      visit root_path
+    end
+
+    it 'shows list of signed organisations on the home page' do
+      expect(page).to have_content("TestMe & Company")
+    end
+  end
+end
