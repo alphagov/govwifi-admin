9af233530140b88f7843963651dc1f72f12f9674
Renny Fadoju <rennyvbirds@yahoo.com>
2018-11-07 16:55:42 +0000

    [WIP]: Untested commits Done to grasp an idea of the behaviour of branch

--- app/controllers/admin/organisations_controller.rb
+++ app/controllers/admin/organisations_controller.rb
@@ -1,5 +1,27 @@
-class Admin::OrganisationsController < AdminController
+class OrganisationsController < ApplicationController
+  helper_method :sort_column, :sort_direction
+  before_action :index, :authorise_admin
+
   def index
-    @organisations = Organisation.order(:name).all
+    @organisations = Organisation.order("#{sort_column} #{sort_direction}").all
   end
+
+private
+
+  def authorise_admin
+    redirect_to root_path unless current_user.admin?
+  end
+
+  def sortable_columns
+    ["name", "created_at"]
+  end
+
+  def sort_column
+    sortable_columns.include?(params[:column]) ? params[:column] : "name"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+
 end
