637db7c2f7db17798206ce2a438faa2fcb1122d8
Kyle <kjdchapman@gmail.com>
2018-11-07 15:57:49 +0000

    Extend spec to cover single/multiple orgs

--- spec/features/admin/view_organisation_list_spec.rb
+++ spec/features/admin/view_organisation_list_spec.rb
@@ -1,27 +1,57 @@
 require 'features/support/not_signed_in'
 require 'features/support/sign_up_helpers'
 
-describe 'view list of signed up organisations' do
-  context 'when logged out' do
-    before { visit organisations_path }
+describe 'view list of signed up organisations', focus: true do
+  before do
+    login_as user
+    visit organisations_path
+  end
 
+  context 'when not logged in' do
+    let(:user) { nil }
     it_behaves_like 'not signed in'
   end
 
-  context "when logged in" do
-    before do
-      3.times { create(:organisation) }
+  context 'when logged in as a normal user' do
+    let(:user) { create(:user, :confirmed) }
+
+    it 'redirects me to the landing guidance' do
+      expect(page).to have_content 'Get GovWifi'
+      expect(page).to have_content 'Getting help'
+    end
+  end
+
+  context 'when logged in as an admin' do
+    let(:user) { create(:user, :confirmed, admin: true) }
+
+    context 'and an organisation exists' do
+      before do
+        create(
+          :organisation,
+          name: "Bob's Burgers",
+          created_at: '1 Feb 2014'
+        )
+        visit organisations_path
+      end
+
+      it 'shows their name' do
+        expect(page).to have_content "Bob's Burgers"
+      end
+
+      it 'shows when they signed up' do
+        expect(page).to have_content('1 Feb 2014')
+      end
     end
 
-    let!(:user) { create(:user, :confirmed, admin: true) }
-
-    context 'when admin navigates to the list page' do
-      it 'renders the names of the signed up organisations' do
-        sign_in_user user
+    context 'and three organisations exist' do
+      before do
+        3.times { create(:organisation) }
         visit organisations_path
+      end
 
-        Organisation.all.each do |organisation|
-          expect(page).to have_content(organisation.name)
+      it 'shows all three organisations' do
+        Organisation.all.each do |org|
+          expect(page).to have_content(org.name)
         end
       end
     end
