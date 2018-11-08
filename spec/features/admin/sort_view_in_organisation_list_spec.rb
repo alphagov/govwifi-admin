fb8543f9ce91f198129e275c1155b2dd6cc37b8d
Renny Fadoju <rennyvbirds@yahoo.com>
2018-11-08 17:00:33 +0000

    Add spec for "created at" sorting

--- spec/features/admin/sort_view_in_organisation_list_spec.rb
+++ spec/features/admin/sort_view_in_organisation_list_spec.rb
@@ -1,4 +1,4 @@
-describe 'sorting the columns in the organisation list', focus: true  do
+describe 'sorting the columns in the organisation list', focus: true do
   context 'when admin views the names of organisations' do
     let!(:organisation_1) { create(:organisation, name: "ABC", created_at: Date.yesterday) }
     let!(:organisation_2) { create(:organisation, name: "DEF", created_at: 1.month.ago) }
@@ -6,24 +6,31 @@ describe 'sorting the columns in the organisation list', focus: true  do
 
     let!(:user) { create(:user, :confirmed, admin: true) }
 
-    it 'can sort the organisations by names in descending order' do
+    before do
       sign_in_user user
       visit root_path
-
+    end
+    
+    it 'can sort the organisations by names in descending order' do
       click_link 'Name'
 
       expect(page.body).to match(/XYZ.*DEF.*ABC/m)
     end
 
-    context 'and interested in the account creation date' do
-      it 'can sort the dates from newest to oldest' do
-        sign_in_user user
-        visit root_path
-
+    context 'can toggle the order of the dates' do
+      it 'can sort the dates from oldest to newest' do
         click_link 'Created At'
+
+        expect(page.body).to match(/DEF.*ABC.*XYZ/m)
+      end
+
+      it 'can sort the dates from oldest to newest' do
+        2.times{ click_link 'Created At' }
 
         expect(page.body).to match(/XYZ.*ABC.*DEF/m)
       end
+
     end
+
   end
 end
