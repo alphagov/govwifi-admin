ecb1249af17a660f1dcbb1464c8f26147eeecb21
Renny Fadoju <rennyvbirds@yahoo.com>
2018-11-09 13:29:33 +0000

    Refactor test set up

--- spec/features/admin/sort_view_in_organisation_list_spec.rb
+++ spec/features/admin/sort_view_in_organisation_list_spec.rb
@@ -1,12 +1,12 @@
 describe 'sorting the columns in the organisation list' do
   context 'when admin views the names of organisations' do
-    let!(:organisation_1) { create(:organisation, name: "ABC", created_at: Date.yesterday) }
-    let!(:organisation_2) { create(:organisation, name: "DEF", created_at: 1.month.ago) }
-    let!(:organisation_3) { create(:organisation, name: "XYZ", created_at: 2.years.from_now) }
-
     let!(:user) { create(:user, :confirmed, admin: true) }
 
     before do
+      create(:organisation, name: "ABC", created_at: Date.yesterday)
+      create(:organisation, name: "DEF", created_at: 1.month.ago)
+      create(:organisation, name: "XYZ", created_at: 2.years.from_now)
+
       sign_in_user user
       visit root_path
     end
