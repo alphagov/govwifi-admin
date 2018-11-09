721a5dd402f4b0483135a04fef136a222c465b2f
Renny Fadoju <rennyvbirds@yahoo.com>
2018-11-09 10:46:49 +0000

    Remove focus from test

--- spec/features/admin/sort_view_in_organisation_list_spec.rb
+++ spec/features/admin/sort_view_in_organisation_list_spec.rb
@@ -1,4 +1,4 @@
-describe 'sorting the columns in the organisation list', focus: true do
+describe 'sorting the columns in the organisation list' do
   context 'when admin views the names of organisations' do
     let!(:organisation_1) { create(:organisation, name: "ABC", created_at: Date.yesterday) }
     let!(:organisation_2) { create(:organisation, name: "DEF", created_at: 1.month.ago) }
