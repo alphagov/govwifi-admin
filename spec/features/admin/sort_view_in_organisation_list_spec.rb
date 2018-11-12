406aca05a82ba145d321c7948e1a91155bebb003
Renny Fadoju <rennyvbirds@yahoo.com>
2018-11-12 17:04:14 +0000

    Add spec for sort by MoU

--- spec/features/admin/sort_view_in_organisation_list_spec.rb
+++ spec/features/admin/sort_view_in_organisation_list_spec.rb
@@ -1,13 +1,18 @@
 require 'features/support/sign_up_helpers'
 
-describe 'sorting the columns in the organisation list' do
+describe 'sorting the columns in the organisation list', focus: true do
   context 'when admin views the table with organisation data' do
     let(:user) { create(:user, :confirmed, admin: true) }
 
     before do
       create(:organisation, name: "Department of Apple Cakes", created_at: Date.yesterday)
-      create(:organisation, name: "Department of Silly Hats", created_at: 1.month.ago)
-      create(:organisation, name: "Department of Xylophones", created_at: 2.years.from_now)
+
+      create(:organisation, name: "Department of Silly Hats", created_at: 1.month.ago).signed_mou.attach(
+        io: File.open(Rails.root + "spec/fixtures/mou.pdf"), filename: "mou.pdf"
+      )
+      create(:organisation, name: "Department of Xylophones", created_at: 2.years.from_now).signed_mou.attach(
+        io: File.open(Rails.root + "spec/fixtures/mou.pdf"), filename: "mou.pdf"
+      )
 
       sign_in_user user
       visit root_path
@@ -36,6 +41,18 @@ describe 'sorting the columns in the organisation list' do
         2.times { click_link 'Created on' }
 
         expect(page.body).to match(/Xylophones.*Apple Cakes.*Silly Hats/m)
+      end
+    end
+
+    context 'and wants to sort by MoU uploads' do
+      before do
+        Organisation.find_by(name: "Department of Silly Hats").signed_mou_attachment.update(created_at: 3.years.from_now)
+      end
+
+      it 'sorts the organistions from oldest to newest when MoU signed is clicked once' do
+        click_link 'MoU Signed'
+
+        expect(page.body).to match(/Apple Cakes.*Xylophones.*Silly Hats/m)
       end
     end
   end
