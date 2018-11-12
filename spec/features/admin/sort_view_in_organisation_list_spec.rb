6ffbe954fd6f67bca9ed22a921f7e16921a01d29
Renny Fadoju <rennyvbirds@yahoo.com>
2018-11-12 09:50:06 +0000

    Edit organisation names to be more realistic

--- spec/features/admin/sort_view_in_organisation_list_spec.rb
+++ spec/features/admin/sort_view_in_organisation_list_spec.rb
@@ -3,9 +3,9 @@ describe 'sorting the columns in the organisation list' do
     let!(:user) { create(:user, :confirmed, admin: true) }
 
     before do
-      create(:organisation, name: "ABC", created_at: Date.yesterday)
-      create(:organisation, name: "DEF", created_at: 1.month.ago)
-      create(:organisation, name: "XYZ", created_at: 2.years.from_now)
+      create(:organisation, name: "Department of Apple Cakes", created_at: Date.yesterday)
+      create(:organisation, name: "Department of Silly Hats", created_at: 1.month.ago)
+      create(:organisation, name: "Department of Xylophones", created_at: 2.years.from_now)
 
       sign_in_user user
       visit root_path
@@ -14,20 +14,20 @@ describe 'sorting the columns in the organisation list' do
     it 'can sort the organisations by names in descending order' do
       click_link 'Name'
 
-      expect(page.body).to match(/XYZ.*DEF.*ABC/m)
+      expect(page.body).to match(/Xylophones.*Silly Hats.*Apple Cakes/m)
     end
 
     context 'can toggle the order of the dates' do
       it 'can sort the dates from oldest to newest' do
         click_link 'Created on'
 
-        expect(page.body).to match(/DEF.*ABC.*XYZ/m)
+        expect(page.body).to match(/Silly Hats.*Apple Cakes.*Xylophones/m)
       end
 
       it 'can sort the dates from oldest to newest' do
         2.times { click_link 'Created on' }
 
-        expect(page.body).to match(/XYZ.*ABC.*DEF/m)
+        expect(page.body).to match(/Xylophones.*Apple Cakes.*Silly Hats/m)
       end
     end
   end
