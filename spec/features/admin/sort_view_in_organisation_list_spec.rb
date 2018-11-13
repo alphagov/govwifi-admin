a5db80909e280356bde71459cb73ff1dc3292413
Renny Fadoju <rennyvbirds@yahoo.com>
2018-11-13 10:05:18 +0000

    Refactor test for MoU sorting

--- spec/features/admin/sort_view_in_organisation_list_spec.rb
+++ spec/features/admin/sort_view_in_organisation_list_spec.rb
@@ -1,18 +1,13 @@
 require 'features/support/sign_up_helpers'
 
-describe 'sorting the columns in the organisation list' do
+describe 'sorting the columns in the organisation list', focus: true do
   context 'when admin views the table with organisation data' do
     let(:user) { create(:user, :confirmed, admin: true) }
 
     before do
+      create(:organisation, name: "Department of Silly Hats", created_at: 1.month.ago)
       create(:organisation, name: "Department of Apple Cakes", created_at: Date.yesterday)
-
-      create(:organisation, name: "Department of Silly Hats", created_at: 1.month.ago).signed_mou.attach(
-        io: File.open(Rails.root + "spec/fixtures/mou.pdf"), filename: "mou.pdf"
-      )
-      create(:organisation, name: "Department of Xylophones", created_at: 2.years.from_now).signed_mou.attach(
-        io: File.open(Rails.root + "spec/fixtures/mou.pdf"), filename: "mou.pdf"
-      )
+      create(:organisation, name: "Department of Xylophones", created_at: 2.years.from_now)
 
       sign_in_user user
       visit root_path
@@ -45,20 +40,30 @@ describe 'sorting the columns in the organisation list' do
     end
 
     context 'and wants to sort by MoU uploads' do
-      before do
-        Organisation.find_by(name: "Department of Silly Hats").signed_mou_attachment.update(created_at: 3.years.from_now)
+      before do 
+        create(:organisation, name: "Has No MoU Company")
+
+        create(:organisation, name: "Has New MoU Company").signed_mou.attach(
+          io: File.open(Rails.root + "spec/fixtures/mou.pdf"), filename: "mou.pdf"
+        )
+
+        create(:organisation, name: "Has Old MoU Company").signed_mou.attach(
+          io: File.open(Rails.root + "spec/fixtures/mou.pdf"), filename: "mou.pdf"
+        )
+
+        Organisation.find_by(name: "Has New MoU Company").signed_mou_attachment.update(created_at: 3.years.from_now)
       end
 
       it 'sorts the organistions from oldest to newest when MoU signed is clicked once' do
         click_link 'MoU Signed'
 
-        expect(page.body).to match(/Apple Cakes.*Xylophones.*Silly Hats/m)
+        expect(page.body).to match(/No MoU.*Old MoU.*New MoU/m)
       end
 
       it 'sorts the organistions from newest to oldest when MoU signed is clicked twice' do
         2.times { click_link 'MoU Signed' }
 
-        expect(page.body).to match(/Silly Hats.*Xylophones.*Apple Cakes/m)
+        expect(page.body).to match(/New MoU.*Old MoU.*No MoU/m)
       end
     end
   end
