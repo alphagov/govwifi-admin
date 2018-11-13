cc57c211a37d0d7200f0836332938378409619cb
Kyle <kjdchapman@gmail.com>
2018-11-13 16:57:22 +0000

    Display permissions for team members (#303)
    Notably, this PR includes no way to edit those permissions, but the display 
    does accurately reflect the state of a User's Permission model.

--- spec/features/team/view_team_members_spec.rb
+++ spec/features/team/view_team_members_spec.rb
@@ -17,16 +17,26 @@ describe 'View team members of my organisation' do
       ENV['DUBLIN_RADIUS_IPS'] = "1.1.1.1,2.2.2.2"
     end
 
-    context 'when user is the only in the organisation' do
-      it 'renders only the user' do
+    context 'as the only user in my organisation' do
+      before do
         sign_in_user user
         visit team_members_path
+      end
 
+      it 'shows my email' do
         expect(page).to have_content('me@example.gov.uk')
+      end
+
+      it 'shows my permissions' do
+        expect(page).to have_content('View logs')
+        expect(page).to have_content('View team members')
+        expect(page).to have_content('View locations and IPs')
+        expect(page).to have_content('Add and remove team members')
+        expect(page).to have_content('Add and remove locations and IPs')
       end
     end
 
-    context 'when there are many users in the organisation' do
+    context 'when there are many users in my organisation' do
       before do
         create(:user, :confirmed, email: 'friend@example.gov.uk', organisation: organisation)
       end
@@ -40,7 +50,7 @@ describe 'View team members of my organisation' do
       end
     end
 
-    context 'when there are users outside of the organisation' do
+    context 'when there are users outside of my organisation' do
       before do
         other_organisation = create(:organisation, name: 'Org 2')
         create(:user, :confirmed, email: 'stranger@example.gov.uk', organisation: other_organisation)
