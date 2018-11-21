e5fb2a9b1504b06c796fa4bf524867fe5e1d794e
Tom Taylor <tommotaylor@gmail.com>
2018-11-21 16:08:48 +0000

    Make organisation name validaation case insensitive

--- spec/models/organisation_spec.rb
+++ spec/models/organisation_spec.rb
@@ -3,6 +3,13 @@ describe Organisation do
     it { should have_many(:users) }
     it { should have_many(:locations) }
     it { should validate_presence_of(:name) }
-    it { should validate_uniqueness_of(:name) }
+    it { should validate_uniqueness_of(:name).case_insensitive }
+
+    it "requires a unique name regardless of case" do
+      organisation = Organisation.create(name: "Parks & Rec Dept")
+      expect {
+        Organisation.create(name: "parks & rec dept")
+      }.to_not change { Organisation.count }
+    end
   end
 end
