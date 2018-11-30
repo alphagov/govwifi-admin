bc5715b2d322d3ccc723c145cf87bc2f844cfa8c
Renny Fadoju <rennyvbirds@yahoo.com>
2018-11-30 12:25:59 +0000

    Redefine active tab method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,13 @@ module ApplicationHelper
     resource&.errors&.include?(key.to_sym) ? "govuk-form-group--error" : ""
   end
 
-  def active_tab(path)
-    request.path == path ? "active" : ""
+  def active_tab(identifier)
+    if (request.path == root_path) && (request.path == identifier)
+      return "active"
+    elsif request.path.include?(identifier) && identifier != root_path
+      return "active"
+    else
+      ""
+    end
   end
 end
