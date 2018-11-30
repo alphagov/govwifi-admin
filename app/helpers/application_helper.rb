49ec2b57d747512a06d790cc1908c017721dbac2
Omorinsola Renny Fadoju <32823756+Renny4Real@users.noreply.github.com>
2018-11-30 13:23:56 +0000

    Fix Sub Navigation Underlines (#344)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,13 @@ module ApplicationHelper
     resource&.errors&.include?(key.to_sym) ? "govuk-form-group--error" : ""
   end
 
-  def active_tab(path)
-    request.path == path ? "active" : ""
+  def active_tab(identifier)
+    if (request.path == root_path) && (request.path == identifier)
+      "active"
+    elsif request.path.include?(identifier) && identifier != root_path
+      "active"
+    else
+      ""
+    end
   end
 end
