f75c1bb165ff385178c848a2c997296c44127120
Renny Fadoju <rennyvbirds@yahoo.com>
2018-12-06 15:29:18 +0000

    Refactor scenario for bug better

--- spec/features/invite_existing_user_spec.rb
+++ spec/features/invite_existing_user_spec.rb
@@ -6,12 +6,12 @@ require 'support/notifications_service'
 require 'support/confirmation_use_case_spy'
 require 'support/confirmation_use_case'
 
-describe 'inviting a user', focus: true do
+describe 'inviting a user that has already signed up', focus: true do
   let(:user) { create(:user) }
   let(:invited_user) { create(:user) }
-  let(:second_invited_user) { create(:user, :unconfirmed)  }
+  let(:second_invited_user_email) { 'notconfirmedyet@gov.uk' }
 
-  context 'when invited user has already created an account' do
+  context 'when invited user has been confirmed before invite' do
     include_examples 'invite use case spy'
     include_examples 'notifications service'
 
@@ -31,13 +31,13 @@ describe 'inviting a user', focus: true do
       expect(page).to have_content("Email is already invited, or already registered with another organisation")
     end
 
-    context 'when invited existing user tries to log into their account again' do
+    context 'when invited existing user logs into their account afterwards' do
       before do
         login_as(invited_user, scope: :user)
         visit root_path
       end
 
-      it 'signs the invited user in' do
+      it 'signs in the invited user' do
         expect(page).to have_content 'Sign out'
       end
 
@@ -47,26 +47,38 @@ describe 'inviting a user', focus: true do
     end
   end
 
-  context 'when invited user has already signed up and NOT yet confirmed their account' do
+  context 'when invited user has already signed up but NOT yet confirmed their account' do
     include_examples 'confirmation use case spy'
     include_examples 'invite use case spy'
     include_examples 'notifications service'
 
-
-    before do
-      sign_in_user user
-      visit root_path
-      click_on "Team members"
-      click_on "Invite team member"
-      fill_in "Email", with: second_invited_user.email
+    context 'and based on what is expected of the user sign up path'do
+      it 'sends the confirmation link' do
+        expect {
+          sign_up_for_account(email: second_invited_user_email)
+        }.to change { ConfirmationUseCaseSpy.confirmations_count }.by(1)
+        expect(URI(confirmation_email_link).scheme).to eq("https")
+      end
     end
 
-    it "tells the user that the email has already been taken" do
-      expect {
-        click_on "Send invitation email"
-      }.to change { User.count }.by(0)
-      expect(InviteUseCaseSpy.invite_count).to eq(0)
-      expect(page).to have_content("Email is already invited, or already registered with another organisation")
+    context 'and another confirmed user invites them' do
+      before do
+        sign_up_for_account(email: second_invited_user_email)
+        sign_in_user user
+        visit root_path
+        click_on "Team members"
+        click_on "Invite team member"
+        fill_in "Email", with: second_invited_user_email
+      end
+
+      it "tells the user that the email has already been taken" do
+        expect {
+          click_on "Send invitation email"
+        }.to change { User.count }.by(0)
+        expect(InviteUseCaseSpy.invite_count).to eq(0)
+        expect(page).to have_content("Email is already invited, or already registered with another organisation")
+      end
+
     end
   end
 end
