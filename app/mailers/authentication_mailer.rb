9507c5e0990a893600e16c374b09cbf05047712f
Renny Fadoju <rennyvbirds@yahoo.com>
2018-12-14 12:57:52 +0000

    Remove empty line

--- app/mailers/authentication_mailer.rb
+++ app/mailers/authentication_mailer.rb
@@ -18,7 +18,6 @@ class AuthenticationMailer < ::Devise::Mailer
 
   # Overrides https://github.com/plataformatec/devise/blob/master/app/mailers/devise/mailer.rb#L12
   def reset_password_instructions(record, token, _opts = {})
-
     reset_link = edit_password_url(record, reset_password_token: token)
     template_id = GOV_NOTIFY_CONFIG['reset_password_email']['template_id']
 
