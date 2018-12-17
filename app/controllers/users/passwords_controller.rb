404929b73eddadf3181da2e3d98dcd6b0dc72ae5
Omorinsola Renny Fadoju <32823756+Renny4Real@users.noreply.github.com>
2018-12-17 11:14:37 +0000

    Block unconfirmed users from resetting passwords (#367)
    Redirect the unconfirmed user to confirm their account first before resetting their password.

---
+++ app/controllers/users/passwords_controller.rb
@@ -0,0 +1,13 @@
+class Users::PasswordsController < Devise::PasswordsController
+  append_before_action :validate_user_is_confirmed, only: :create
+
+protected
+
+  def validate_user_is_confirmed
+    user = User.find_by(email: params[:user][:email])
+    unless user.nil? || user.confirmed?
+      set_flash_message(:alert, :unconfirmed)
+      redirect_to new_user_confirmation_path
+    end
+  end
+end
--- spec/features/authentication/reset_password_spec.rb
+++ spec/features/authentication/reset_password_spec.rb
@@ -32,20 +32,28 @@ describe "Resetting a password" do
   end
 
   context "when user is not yet confirmed" do
+    include_examples 'reset password use case spy'
     include_examples 'confirmation use case spy'
     include_examples 'notifications service'
 
     let(:user) { create(:user, :unconfirmed) }
 
-    it "sends the confirmation instructions instead of reset password" do
+    it "tells them to confirm their account first" do
+      visit new_user_password_path
+      fill_in "user_email", with: user.email
+      click_on "Send me reset password instructions"
+
+      expect(page).to have_content("Resend confirmation instructions")
+      expect(page.current_path).to eq(new_user_confirmation_path)
+    end
+
+    it 'does not send a reset password link' do
       visit new_user_password_path
       fill_in "user_email", with: user.email
 
       expect {
         click_on "Send me reset password instructions"
-      }.to change { ConfirmationUseCaseSpy.confirmations_count }.by(1)
-
-      expect(page).to have_content("You will receive an email with instructions")
+      }.to change { ResetPasswordUseCaseSpy.reset_count }.by(0)
     end
   end
 
