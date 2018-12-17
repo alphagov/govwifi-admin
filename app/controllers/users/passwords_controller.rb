e68e8a335ca257a609d1058074708b754646f88a
Renny Fadoju <rennyvbirds@yahoo.com>
2018-12-17 11:06:50 +0000

    Refactor code & devise message

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,13 +1,11 @@
 class Users::PasswordsController < Devise::PasswordsController
   append_before_action :validate_user_is_confirmed, only: :create
-  append_before_action :assert_reset_token_passed, only: :edit
 
 protected
 
   def validate_user_is_confirmed
-    current_user_details = params[:user]
-    user_email = current_user_details[:email]
-    unless User.find_by(email: user_email).nil? || User.find_by(email: user_email).confirmed?
+    user = User.find_by(email: params[:user][:email])
+    unless user.nil? || user.confirmed?
       set_flash_message(:alert, :unconfirmed)
       redirect_to new_user_confirmation_path
     end
