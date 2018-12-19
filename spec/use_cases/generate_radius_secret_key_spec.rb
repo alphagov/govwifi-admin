bcc56b86b9379e3ec97bb4b74be36797297702c8
Kyle <kjdchapman@gmail.com>
2018-12-19 11:59:34 +0000

    Remove some noise in spec folders

--- spec/use_cases/generate_radius_secret_key_spec.rb
+++ spec/use_cases/generate_radius_secret_key_spec.rb
@@ -1,4 +1,4 @@
-describe RadiusSecretKeyGenerator do
+describe UseCases::Administrator::GenerateRadiusSecretKey do
   it 'has a length of 20 characters' do
     expect(subject.execute.length).to eq(20)
   end
