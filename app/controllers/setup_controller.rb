332930b2406b01730785543d15a1c0b4c45db58f
Renny Fadoju <rennyvbirds@yahoo.com>
2019-01-03 16:11:38 +0000

    Add SetupController

---
+++ app/controllers/setup_controller.rb
@@ -0,0 +1,16 @@
+class SetupController < ApplicationController
+  def index
+    @ips = current_organisation.ips
+    @locations = current_organisation.locations
+    @team_members = current_organisation.users
+    @london_radius_ips = radius_ips[:london]
+    @dublin_radius_ips = radius_ips[:dublin]
+  end
+
+  private
+
+  def radius_ips
+    view_radius = UseCases::Organisation::ViewRadiusIPAddresses.new(organisation_id: current_organisation.id)
+    view_radius.execute
+  end
+end
