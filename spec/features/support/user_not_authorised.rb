ebcc0c77c06aa9dd48513aaf9baf3950bcbc16df
Renny Fadoju <rennyvbirds@yahoo.com>
2019-01-07 14:48:35 +0000

    Add comment to describe expectation of path

--- spec/features/support/user_not_authorised.rb
+++ spec/features/support/user_not_authorised.rb
@@ -1,7 +1,6 @@
 shared_examples 'user not authorised' do
-  context 'note that the users set up for testing have no inital IPs' do
-    it 'redirects to root page' do
-      expect(page).to have_current_path(setup_instructions_path)
-    end
+  it 'redirects to setting up' do
+    # this relies on the assumption that users created for the purpose of testing have no IPs, which routes them differently from root.
+    expect(page).to have_current_path(setup_instructions_path)
   end
 end
