0d8c1eb905c9aaf708d5c706f28083961f8ead0e
Kyle <kjdchapman@gmail.com>
2019-01-11 12:13:59 +0000

    Make controller more readable, modular

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,29 +1,46 @@
 class LogsController < ApplicationController
   def index
-    use_case = UseCases::Administrator::GetAuthRequests.new(
+    if location_outside_organisation?
+      redirect_to(location_new_logs_search_path) && return
+    end
+
+    if valid_search_params.blank?
+      redirect_to(new_logs_search_path) && return
+    end
+
+    @location_name = Location.find_by(id: params[:location])&.address
+
+    @logs = get_auth_requests
+      .execute(ips: ip_params, username: params[:username])
+      .fetch(:results)
+  end
+
+private
+
+  def location_outside_organisation?
+    return unless params[:location].present?
+
+    location = Location.find(params[:location])
+    location.organisation != current_organisation
+  end
+
+  def valid_search_params
+    params.keys & %w[ip username location]
+  end
+
+  def get_auth_requests
+    UseCases::Administrator::GetAuthRequests.new(
       authentication_logs_gateway: Gateways::Sessions.new(
         ips: current_organisation.ips.map(&:address)
       )
     )
+  end
 
-    if params[:ip].present?
-      logs = use_case.execute(ips: [params[:ip]])
-    elsif params[:username].present?
-      logs = use_case.execute(username: params[:username])
-    elsif params[:location].present?
-      # TODO: don't allow locations outside the org
-      location = Location.find(params[:location])
-
-      if location.organisation == current_organisation
-        @location_name = location.address
-        logs = use_case.execute(ips: location.ips.map(&:address))
-      else
-        redirect_to location_new_logs_search_path and return
-      end
+  def ip_params
+    if params[:location].present?
+      Location.find(params[:location]).ips.map(&:address)
     else
-      redirect_to new_logs_search_path and return
+      params[:ip] ? [params[:ip]] : nil
     end
-
-    @logs = logs.fetch(:results)
   end
 end
