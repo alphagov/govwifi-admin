423312d8bea375564b3aa4f3430f219a792f8500
Renny Fadoju <rennyvbirds@yahoo.com>
2019-01-15 10:33:44 +0000

    Refactor code that calls get auth usecase for consisitency

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,22 +4,17 @@ class HomeController < ApplicationController
 
     redirect_to setup_instructions_path unless current_organisation.ips.present?
 
-    connections = UseCases::Administrator::GetAuthRequests.new(
-      authentication_logs_gateway: Gateways::Sessions.new(
-        ips: current_organisation.ips.map(&:address)
-      )
-    ).execute(
-      username: nil,
-      ips: @ips
-    )
-
     @ips = current_organisation.ips
     @locations = current_organisation.locations
     @team_members = current_organisation.users
     @london_radius_ips = radius_ips[:london]
     @dublin_radius_ips = radius_ips[:dublin]
 
-    @connections = connections.fetch(:connection_count)
+    @connections = get_auth_requests.execute(
+      ips: @ips.map(&:address),
+      username: nil
+    ).fetch(:connection_count)
+
   end
 
 private
@@ -27,5 +22,13 @@ private
   def radius_ips
     view_radius = UseCases::Organisation::ViewRadiusIPAddresses.new(organisation_id: current_organisation.id)
     view_radius.execute
+  end
+
+  def get_auth_requests
+    UseCases::Administrator::GetAuthRequests.new(
+      authentication_logs_gateway: Gateways::Sessions.new(
+        ips: current_organisation.ips.map(&:address)
+      )
+    )
   end
 end
