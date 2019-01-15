f7ed4d8ceaf9469565625a9ade073c84060b951e
Renny Fadoju <rennyvbirds@yahoo.com>
2019-01-15 16:50:23 +0000

    Rearrange setup of tests

--- spec/features/overview/view_activity_section_spec.rb
+++ spec/features/overview/view_activity_section_spec.rb
@@ -1,31 +1,45 @@
 describe 'viewing the activity of GovWifi in an organisation' do
-  context 'within the last 24 hours' do
-    let(:ip) { '1.2.3.4' }
-    let(:username_1) { 'AAAAAA' }
-    let(:username_2) { 'BBBBBB' }
-    let(:organisation) { create(:organisation) }
-    let(:admin_user) { create(:user, organisation_id: organisation.id) }
-    let(:location) { create(:location, organisation_id: organisation.id) }
+  let(:username_1) { 'AAAAAA' }
+  let(:username_2) { 'BBBBBB' }
+  let(:ip) { '1.2.3.4' }
+  let(:admin_user) { create(:user, organisation: organisation) }
+  let(:organisation) { create(:organisation, :with_locations) }
+  let(:location) { organisation.locations.first }
 
-    before do
-      create(:ip, location_id: location.id, address: ip)
-      sign_in_user admin_user
-      visit root_path
-    end
+  context 'within the past 24 hours' do
+    context 'with no successful connections' do
+      before do
+        Session.create!(start: 3.hours.ago, success: false, username: username_1, siteIP: ip)
+        Session.create!(start: 5.hours.ago, success: false, username: username_1, siteIP: ip)
 
-    it 'displays a message if there are no successful connections' do
-      within('div#user-statistics') do
-        expect(page).to have_content("There have been no connections")
+        create(:ip, location: location, address: ip)
+
+        sign_in_user admin_user
+        visit root_path
+      end
+
+      it 'displays a message if there are no successful connections' do
+        within('div#user-statistics') do
+          expect(page).to have_content("No connections")
+        end
       end
     end
 
-    it 'displays the number of successful connections' do
-      Session.create(start: 3.hours.ago, success: true, username: username_1, siteIP: ip)
-      Session.create(start: 5.hours.ago, success: true, username: username_2, siteIP: ip)
-      Session.create(start: 10.hours.ago, success: false, username: username_1, siteIP: ip)
+    context 'with at least one successful connection' do
+      before do
+        Session.create!(start: 3.hours.ago, success: false, username: username_1, siteIP: ip)
+        Session.create!(start: 5.hours.ago, success: true, username: username_2, siteIP: ip)
 
-      within('div#user-statistics') do
-        expect(page).to have_content("There have been 2 connections")
+        create(:ip, location: location, address: ip)
+
+        sign_in_user admin_user
+        visit root_path
+      end
+
+      it 'displays the number of successful connections' do
+        within('div#user-statistics') do
+          expect(page).to have_content("1 connection")
+        end
       end
     end
   end
