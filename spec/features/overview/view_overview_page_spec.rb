b70e52835b51b7c62fd7269b53a1b14efad4612f
Renny Fadoju <rennyvbirds@yahoo.com>
2019-01-16 14:51:27 +0000

    Add tests for activity section on the overview page

--- spec/features/overview/view_overview_page_spec.rb
+++ spec/features/overview/view_overview_page_spec.rb
@@ -43,56 +43,102 @@ describe 'viewing the overview page' do
         create(:ip, address: ip_one, location: location_one)
         create(:ip, address: ip_two, location: location_one)
         create(:ip, address: ip_three, location: location_two)
-
-        sign_in_user user
-        visit root_path
       end
 
-      it 'shows an Overview' do
-        within('div#overview') do
-          expect(page).to have_content("Overview")
-          expect(page).to have_link("Team Members")
-          expect(page).to have_link("Locations")
-          expect(page).to have_link("IPs")
+      context 'within overview section' do
+        before do
+          sign_in_user user
+          visit root_path
+        end
+
+        it 'shows an overview of the current installation' do
+          within('div#overview') do
+            expect(page).to have_link("Team Members")
+            expect(page).to have_link("Locations")
+            expect(page).to have_link("IPs")
+          end
+        end
+
+        it 'displays the number of Team members' do
+          within('div#team-members') do
+            expect(page).to have_content(1)
+            expect(page).to have_content("Team Members")
+          end
+        end
+
+        it 'redirects to the team members page when Team Members is clicked on' do
+          click_link 'Team Members'
+
+          expect(page.current_path).to eq(team_members_path)
+        end
+
+        it 'displays the number of Locations' do
+          within('div#locations') do
+            expect(page).to have_content(2)
+            expect(page).to have_content("Locations")
+          end
+        end
+
+        it 'redirects to the IPs page when Locations is clicked on' do
+          click_link 'Locations'
+
+          expect(page.current_path).to eq(ips_path)
+        end
+
+        it 'displays the number of IPs added' do
+          within('div#ips') do
+            expect(page).to have_content(3)
+            expect(page).to have_content("IPs")
+          end
+        end
+
+        it 'redirects to the IPs page when IPs is clicked on' do
+          within('div#ips') do
+            click_link 'IPs'
+          end
+          expect(page.current_path).to eq(ips_path)
         end
       end
 
-      it 'redirects to the team members page when Team Members is clicked on' do
-        click_link 'Team Members'
+      context 'within activity section' do
+        let(:username_1) { 'AAAAAA' }
+        let(:username_2) { 'BBBBBB' }
+        let(:username_3) { 'CCCCCC' }
 
-        expect(page.current_path).to eq(team_members_path)
-      end
+        context 'with no successful connections' do
+          before do
+            Session.create!(start: 3.hours.ago, success: false, username: username_1, siteIP: ip_one)
+            Session.create!(start: 5.hours.ago, success: false, username: username_1, siteIP: ip_one)
 
-      it 'describes the number of Team members' do
-        within('div#team-members') do
-          expect(page).to have_content(1)
-          expect(page).to have_content("Team Members")
+            sign_in_user user
+            visit root_path
+          end
+
+          it 'displays a message if there are no successful connections' do
+            within('div#user-statistics') do
+              expect(page).to have_content("No connections")
+            end
+          end
         end
-      end
 
-      it 'redirects to the IPs page when Locations is clicked on' do
-        click_link 'Locations'
+        context 'with successful connections' do
+          before do
+            Session.create!(start: 1.hour.ago, success: true, username: username_1, siteIP: ip_one)
+            Session.create!(start: 6.hours.ago, success: true, username: username_1, siteIP: ip_two)
+            Session.create!(start: 9.hours.ago, success: true, username: username_1, siteIP: ip_three)
+            Session.create!(start: 12.hours.ago, success: true, username: username_2, siteIP: ip_one)
+            Session.create!(start: 1.day.ago, success: true, username: username_2, siteIP: ip_two)
+            Session.create!(start: 2.weeks.ago, success: true, username: username_3, siteIP: ip_three)
 
-        expect(page.current_path).to eq(ips_path)
-      end
+            sign_in_user user
+            visit root_path
+          end
 
-      it 'describes the number of Locations added' do
-        within('div#locations') do
-          expect(page).to have_content(2)
-          expect(page).to have_content("Locations")
-        end
-      end
-
-      it 'redirects to the IPs page when IPs is clicked on' do
-        click_link 'nav-ips'
-
-        expect(page.current_path).to eq(ips_path)
-      end
-
-      it 'describes the number of IPs added' do
-        within('div#ips') do
-          expect(page).to have_content(3)
-          expect(page).to have_content("IPs")
+          it 'displays the number of unique connections within 24 hours' do
+            within('div#user-statistics') do
+              expect(page).to have_content("2 connections")
+            end
+          end
         end
       end
     end
