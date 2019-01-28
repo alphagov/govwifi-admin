57f11ff2f5beb2791faa9ce80ea47edd8785beaa
Emile Swarts <emile@madetech.com>
2019-01-28 15:31:31 +0000

    Ensure IP whitelist is updated when deleting IPs (#429)
    We publish an IP whitelist to S3 whenever we create one, do the same
    when we delete them

--- spec/requests/ips/delete_spec.rb
+++ spec/requests/ips/delete_spec.rb
@@ -6,6 +6,8 @@ describe "DELETE /ips/:id", type: :request do
   before do
     https!
     login_as(user, scope: :user)
+    stub_request(:get, 'http://169.254.169.254/latest/meta-data/iam/security-credentials/')
+    stub_request(:put, /s3.eu-west-2/)
   end
 
   context "when the user owns the IP" do
@@ -13,6 +15,11 @@ describe "DELETE /ips/:id", type: :request do
       expect {
         delete ip_path(ip)
       }.to change { Ip.count }.by(-1)
+    end
+
+    it "Publishes the new list of IPs" do
+      expect_any_instance_of(Facades::Ips::Publish).to receive(:execute)
+      delete ip_path(ip)
     end
   end
 
