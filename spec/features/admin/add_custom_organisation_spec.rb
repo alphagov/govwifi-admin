55234afd6f9553886d58d0c270a9f64c14fe1947
George Schena <george@madetech.com>
2019-02-07 14:19:51 +0000

    failing test

--- spec/features/admin/add_custom_organisation_spec.rb
+++ spec/features/admin/add_custom_organisation_spec.rb
@@ -14,7 +14,18 @@ describe 'adding a custom organisation name', focus: true do
     it 'will redirect you to organiastions page once a custom org is added' do
       fill_in 'custom_organisations[name]', with: 'Custom Org name'
       click_on 'Confirm'
-      expect(page.body).to have_content('Successfully added a custom organisation')
+      expect(page).to have_content('Successfully added a custom organisation')
+    end
+
+    context 'signout and find custom org' do
+      it 'will redirect you to organiastions page once a custom org is added' do
+        fill_in 'custom_organisations[name]', with: 'Custom Org name'
+        click_on 'Confirm'
+        sign_out
+        visit confirmation_email_link
+        select 'Custom Org name', from: 'Organisation name'
+        expect(page).to have_content('Custom Org name')
+      end
     end
   end
 end
