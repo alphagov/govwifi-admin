177c1a2427631abed4331ae9deaf5ff4860f809a
George Schena <george@madetech.com>
2019-02-07 11:47:51 +0000

    remove file

--- spec/features/admin/add_custom_organisation_as_super_admin_spec.rb
+++
@@ -1,9 +0,0 @@
-describe 'adding a custom organisation' do
-  let!(:admin_user) { create(:user, super_admin: true) }
-
-  context 'when visiting the organisations page' do
-    before do
-      sign_in_user admin_user
-    end
-  end
-end
