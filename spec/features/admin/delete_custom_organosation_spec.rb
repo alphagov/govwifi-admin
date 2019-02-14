c721cfb36745501dd866fffb42d4211a7d1c1c2f
George Schena <george@madetech.com>
2019-02-14 17:41:23 +0000

    Can delete custom orgs

--- spec/features/admin/delete_custom_organosation_spec.rb
+++ spec/features/admin/delete_custom_organosation_spec.rb
@@ -7,15 +7,30 @@ describe 'adding a custom organisation name' do
 
   context 'when visiting the custom organisations page' do
     before do
-      sign_in_user admin_user
       CustomOrganisationName.create(name: 'Custom Org1')
+      CustomOrganisationName.create(name: 'Custom Org2')
+      CustomOrganisationName.create(name: 'Custom Org3')
+      CustomOrganisationName.create(name: 'Custom Org4')
+      CustomOrganisationName.create(name: 'Custom Org5')
+      sign_in_user admin_user
       visit admin_custom_organisations_path
     end
 
     it 'allows the user to delete a custom organisation' do
-      click_link 'Remove'
+      within("tr#data-id-1") do
+        click_link 'Remove'
+      end
       click_on 'Yes, remove this organisation'
-      expect(page).to have_content("Successfully removed Custom Org1")
+      click_on 'Allow organisation'
+      expect(page).to_not have_content("Custom Org1")
+    end
+  end
+
+  context 'changes the number of custom orgs in the register' do
+    before do
+      CustomOrganisationName.create(name: 'Custom Org1')
+      sign_in_user admin_user
+      visit admin_custom_organisations_path
     end
 
     it 'will remove the custom org from the register' do
