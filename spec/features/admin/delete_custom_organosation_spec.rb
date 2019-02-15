a2939950533cb6b849d2c69b9ff1240df74ef415
George Schena <george@madetech.com>
2019-02-15 10:30:53 +0000

    Bad test

--- spec/features/admin/delete_custom_organosation_spec.rb
+++ spec/features/admin/delete_custom_organosation_spec.rb
@@ -1,34 +1,32 @@
 require 'support/confirmation_use_case'
 
-describe 'adding a custom organisation name' do
+describe 'deleting a custom organisation name' do
   include_examples 'confirmation use case spy'
 
   let!(:admin_user) { create(:user, super_admin: true) }
 
   context 'when visiting the custom organisations page' do
     before do
-      CustomOrganisationName.create(name: 'Custom Org1')
-      CustomOrganisationName.create(name: 'Custom Org2')
-      CustomOrganisationName.create(name: 'Custom Org3')
-      CustomOrganisationName.create(name: 'Custom Org4')
-      CustomOrganisationName.create(name: 'Custom Org5')
       sign_in_user admin_user
       visit admin_custom_organisations_path
+      CustomOrganisationName.create(name: 'DummyOrg1')
+      CustomOrganisationName.create(name: 'DummyOrg2')
     end
 
     it 'allows the user to delete a custom organisation' do
       within("tr#data-id-1") do
         click_link 'Remove'
       end
+
       click_on 'Yes, remove this organisation'
       click_on 'Allow organisation'
-      expect(page).to_not have_content("Custom Org1")
+      expect(page).to_not have_content("DummyOrg1")
     end
   end
 
   context 'changes the number of custom orgs in the register' do
     before do
-      CustomOrganisationName.create(name: 'Custom Org1')
+      CustomOrganisationName.create(name: 'DummyOrg2')
       sign_in_user admin_user
       visit admin_custom_organisations_path
     end
