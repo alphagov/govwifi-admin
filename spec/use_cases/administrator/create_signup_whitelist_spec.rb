d6f7d80f9b74575f2a7d2de4c5ba7fc1f0f97773
Emile <emile.swarts123@gmail.com>
2019-02-18 14:08:42 +0000

    Use case to generate a signup whitelist
    Given a list of domains, generate the PERL regular expression that will
    be used to make sure someone is allowed to sign up to the admin or user
    signup API.
    
    A list of:
    foo.com, bar.gov.uk
    
    Will result in a regex like:
    ^.*@(foo\.com|bar\.gov\.uk)$

---
+++ spec/use_cases/administrator/create_signup_whitelist_spec.rb
@@ -0,0 +1,31 @@
+describe UseCases::Administrator::CreateSignupWhitelist do
+  let(:result) { subject.execute(authorised_domains) }
+
+  context 'given no domains' do
+    let(:authorised_domains) { [] }
+
+    it 'creates no whitelist' do
+      expect(result).to eq('^$')
+    end
+  end
+
+  context 'given one domain' do
+    let(:authorised_domains) { %w(gov.uk) }
+
+    it 'escapes the fullstops' do
+      expect(result).to include('\.')
+    end
+
+    it 'creates a whitelist with one entry' do
+      expect(result).to eq('^.*@(gov\.uk)$')
+    end
+  end
+
+  context 'given multiple domains' do
+    let(:authorised_domains) { %w(gov.uk police.uk some.domain.org.uk) }
+
+    it 'creates a whitelist with multiple entries' do
+      expect(result).to eq('^.*@(gov\.uk|police\.uk|some\.domain\.org\.uk)$')
+    end
+  end
+end
