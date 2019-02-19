afe7059575889563bd73ee3fe4d07db0b63ad3f5
Emile Swarts <emile@madetech.com>
2019-02-19 11:05:21 +0000

    Use case for publishing the signup whitelist to S3 (#483)

---
+++ spec/use_cases/administrator/publish_signup_whitelist_spec.rb
@@ -0,0 +1,22 @@
+class DummyWhitelistPresenter
+  def execute(payload)
+    payload.join('*')
+  end
+end
+
+describe UseCases::Administrator::PublishSignupWhitelist do
+  let(:source_gateway) { double(fetch_domains: %i(gov.org.uk nhs.uk)) }
+  let(:s3_gateway) { double }
+  let(:formatted_s3_payload) { 'gov.org.uk*nhs.uk' }
+  let(:presenter) { DummyWhitelistPresenter.new }
+
+  it 'publishes the email domain whitelist' do
+    expect(s3_gateway).to receive(:upload).with(data: formatted_s3_payload)
+
+    described_class.new(
+      destination_gateway: s3_gateway,
+      source_gateway: source_gateway,
+      presenter: presenter
+    ).execute
+  end
+end
