ec8ad597f3328c3dcf2c54fdb4b551f9b0681641
Emile Swarts <emile@madetech.com>
2019-02-19 16:22:39 +0000

    Validate shape of the data being whitelisted (#486)
    There is a danger that malformed data entered here could produce a
    corrupt whitelist regular expression.
    
    Validate that only domains / subdomains are allowed to be saved.

--- app/models/authorised_email_domain.rb
+++ app/models/authorised_email_domain.rb
@@ -1,3 +1,4 @@
 class AuthorisedEmailDomain < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates_format_of :name, with: /\A[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/, on: :create
 end
