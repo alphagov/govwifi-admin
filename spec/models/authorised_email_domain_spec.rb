2fac04fedee3aab0ed1878c0c7dcb49e928b9d32
Emile <emile.swarts123@gmail.com>
2019-02-19 15:56:01 +0000

    Validate shape of the data being whitelisted
    There is a danger that malformed data entered here could produce a
    corrupt whitelist regular expression.
    
    Validate that only domains / subdomains are allowed to be saved.

--- spec/models/authorised_email_domain_spec.rb
+++ spec/models/authorised_email_domain_spec.rb
@@ -2,5 +2,27 @@ describe AuthorisedEmailDomain do
   context 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:name).case_insensitive }
+
+    it 'validates the domain is in the correct shape' do
+      invalid_examples = %i(
+        justarandomstring
+        i_contain_an_at_sign@gov.uk
+        999999
+      )
+
+      invalid_examples.each do |invalid_example|
+        expect(described_class.new(name: invalid_example).valid?).to eq(false)
+      end
+
+      valid_examples = %i(
+        foo.com
+        nhs.net
+        123abc.foo.com
+      )
+
+      valid_examples.each do |valid_example|
+        expect(described_class.new(name: valid_example).valid?).to eq(true)
+      end
+    end
   end
 end
