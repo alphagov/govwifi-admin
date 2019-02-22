9b107689c2e6badaeba68b3253c616126cfcc4fa
Renny Fadoju <rennyvbirds@yahoo.com>
2019-02-22 10:51:01 +0000

    Refactor delete spec

--- spec/requests/email_domain/delete_spec.rb
+++ spec/requests/email_domain/delete_spec.rb
@@ -1,36 +1,33 @@
 describe "DELETE /authorised_email_domains/:id", type: :request do
-  let(:admin_user) { create(:user, super_admin: true) }
-  let(:normal_user) { create(:user, super_admin: false) }
-  let!(:email_domain_1) { create(:authorised_email_domain, name: 'some.domain.org.uk') }
-  let!(:email_domain_2) { create(:authorised_email_domain, name: 'police.uk') }
+  let!(:email_domain) { create(:authorised_email_domain, name: 'some.domain.org.uk') }
 
   context "when the user is a super admin" do
     before do
       https!
-      login_as(admin_user, scope: :user)
+      sign_in_user(create(:user, super_admin: true))
     end
 
     it "deletes the email domain" do
       expect {
-        delete admin_authorised_email_domain_path(email_domain_1)
+        delete admin_authorised_email_domain_path(email_domain)
       }.to change { AuthorisedEmailDomain.count }.by(-1)
     end
 
     it 'publishes the authorised domains to S3' do
       expect_any_instance_of(Gateways::S3).to receive(:upload)
-      delete admin_authorised_email_domain_path(email_domain_1)
+      delete admin_authorised_email_domain_path(email_domain)
     end
   end
 
   context "when the user is not super admin" do
     before do
       https!
-      login_as(normal_user, scope: :user)
+      sign_in_user(create(:user, super_admin: false))
     end
 
     it "does not delete the email domain" do
       expect {
-        delete admin_authorised_email_domain_path(email_domain_2)
+        delete admin_authorised_email_domain_path(email_domain)
       }.to change { AuthorisedEmailDomain.count }.by(0)
     end
   end
