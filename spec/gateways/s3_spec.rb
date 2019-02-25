b878c5d8801eb284050262ed21f6cf0211144f85
Omorinsola Renny Fadoju <32823756+Renny4Real@users.noreply.github.com>
2019-02-25 13:57:50 +0000

    Add read method to s3 gateway (#489)

--- spec/gateways/s3_spec.rb
+++ spec/gateways/s3_spec.rb
@@ -8,4 +8,22 @@ describe Gateways::S3 do
   it 'uploads the data' do
     expect(subject.upload(data: data)).to eq({})
   end
+
+  context 'given we read from the S3 file' do
+    before do
+      Rails.application.config.s3_aws_config = {
+        stub_responses: {
+          get_object: ->(context) {
+            if context.params.fetch(:bucket) == bucket && context.params.fetch(:key) == key
+              { body: 'some data' }
+            end
+          }
+        }
+      }
+    end
+
+    it 'returns the contents' do
+      expect(subject.read).to eq('some data')
+    end
+  end
 end
