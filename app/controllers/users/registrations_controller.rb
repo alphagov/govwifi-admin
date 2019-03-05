fa660fc575aedb3acf4414a95b4d438645521a92
Omorinsola Renny Fadoju <32823756+Renny4Real@users.noreply.github.com>
2019-03-05 10:56:49 +0000

    Stop passing the user email as a query string to the pending URL (#508)

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,8 +6,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
 protected
 
   # The path used after sign up for inactive accounts.
-  def after_inactive_sign_up_path_for(resource)
-    users_confirmations_pending_path(email: resource.email)
+  def after_inactive_sign_up_path_for(_resource)
+    users_confirmations_pending_path
   end
 
   def validate_email_on_whitelist
