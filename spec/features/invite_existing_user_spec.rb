6679976d32a85ed103dd16b43e262dcf11a84814
George Schena <george@madetech.com>
2019-03-07 12:17:21 +0000

    working example

--- spec/features/invite_existing_user_spec.rb
+++ spec/features/invite_existing_user_spec.rb
@@ -21,7 +21,7 @@ describe 'inviting a user that has already signed up' do
         click_on "Send invitation email"
       }.to change { User.count }.by(0)
       expect(InviteUseCaseSpy.invite_count).to eq(0)
-      expect(page).to have_content("Email is already invited, or already registered with another organisation")
+      expect(page).to have_content("Email is already associated with an account. If you can't sign in, reset your password")
     end
 
     context 'when betty signs into their account after the confirmed user tried to invite them' do
@@ -67,7 +67,7 @@ describe 'inviting a user that has already signed up' do
 
       it "does not send an invitation to an existing user" do
         expect(InviteUseCaseSpy.invite_count).to eq(0)
-        expect(page).to have_content("Email is already invited, or already registered with another organisation")
+        expect(page).to have_content("Email is already associated with an account. If you can't sign in, reset your password")
       end
 
       context 'when claire visits the confirmation link after the confirmed user tried to invite them' do
