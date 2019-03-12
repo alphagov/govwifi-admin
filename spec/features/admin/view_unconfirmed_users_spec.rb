5901260f2b4905d16fa8c96c2295887be831c38d
George Schena <george@madetech.com>
2019-03-12 14:42:59 +0000

    refac

--- spec/features/admin/view_unconfirmed_users_spec.rb
+++ spec/features/admin/view_unconfirmed_users_spec.rb
@@ -3,15 +3,11 @@ require 'support/notifications_service'
 describe 'viewing all unconfirmed users' do
   include_examples 'notifications service'
 
-  let!(:admin_user) { create(:user, super_admin: true) }
+  let(:admin_user) { create(:user, super_admin: true) }
 
-  let!(:organisation) { create(:organisation) }
-
-  let!(:unconfirmed_user1) { create(:user, confirmed_at: nil, email: "unconfirmed_user1@gov.uk") }
-  let!(:unconfirmed_user2) { create(:user, confirmed_at: nil, email: "unconfirmed_user2@gov.uk") }
-
-  let!(:confirmed_user1) { create(:user, confirmed_at: Time.now, email: "person1@gov.uk", organisation: organisation) }
-  let!(:confirmed_user2) { create(:user, confirmed_at: Time.now, email: "person2@gov.uk", organisation: organisation) }
+  let!(:unconfirmed_user_1) { create(:user, confirmed_at: nil) }
+  let!(:unconfirmed_user_2) { create(:user, confirmed_at: nil) }
+  let!(:confirmed_user) { create(:user) }
 
   context 'when visiting the manage users page' do
     before do
@@ -24,11 +20,9 @@ describe 'viewing all unconfirmed users' do
     end
 
     it 'lists only the unconfirmed users' do
-      expect(page).to have_content("unconfirmed_user1@gov.uk")
-      expect(page).to have_content("unconfirmed_user2@gov.uk")
-
-      expect(page).to_not have_content("person1@gov.uk")
-      expect(page).to_not have_content("person2@gov.uk")
+      expect(page).to have_content(unconfirmed_user_1.email)
+      expect(page).to have_content(unconfirmed_user_2.email)
+      expect(page).to_not have_content(confirmed_user.email)
     end
   end
 end
