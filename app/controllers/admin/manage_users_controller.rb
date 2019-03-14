ac97ba31fe6c19324d56649758c8a0f1ddf826a9
George Schena <george@madetech.com>
2019-03-14 12:36:39 +0000

    manage team page

--- app/controllers/admin/manage_users_controller.rb
+++ app/controllers/admin/manage_users_controller.rb
@@ -1,5 +1,22 @@
 class Admin::ManageUsersController < AdminController
+  helper_method :sort_column, :sort_direction
+
   def index
-    @all_unconfirmed_users = User.where(confirmed_at: nil)
+    unconfirmed_user = User.where(confirmed_at: nil)
+    @all_unconfirmed_users = unconfirmed_user.order("#{sort_column} #{sort_direction}")
+  end
+
+  private
+
+  def sortable_columns
+    %w[email created_at]
+  end
+
+  def sort_column
+    sortable_columns.include?(params[:sort]) ? params[:sort] : "created_at"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
