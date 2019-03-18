db9782292e9aa60e5d0547d69b284fe873d2960d
Renny Fadoju <rennyvbirds@yahoo.com>
2019-03-18 11:08:06 +0000

    Reword test descriptions

--- spec/features/invite_existing_user_spec.rb
+++ spec/features/invite_existing_user_spec.rb
@@ -2,12 +2,12 @@ require 'support/invite_use_case'
 require 'support/notifications_service'
 require 'support/confirmation_use_case'
 
-describe 'inviting a user that has already signed up' do
+describe 'inviting a user that has signed up' do
   include_examples 'invite use case spy'
 
   let(:confirmed_user) { create(:user) }
 
-  context 'when betty has a confirmed account before being invited by a confirmed user' do
+  context 'when user is already a confirmed user' do
     let(:betty) { create(:user) }
 
     before do
@@ -16,7 +16,7 @@ describe 'inviting a user that has already signed up' do
       fill_in "Email", with: betty.email
     end
 
-    it "does not send an invitation to an existing user" do
+    it "does not send an invitation" do
       expect {
         click_on "Send invitation email"
       }.to change { User.count }.by(0)
@@ -24,13 +24,13 @@ describe 'inviting a user that has already signed up' do
       expect(page).to have_content("Email is already associated with an account. If you can't sign in, reset your password")
     end
 
-    context 'when betty signs into their account after the confirmed user tried to invite them' do
+    context 'and user logs into their account afterwards' do
       before do
         login_as(betty, scope: :user)
         visit root_path
       end
 
-      it 'allows betty to sign in successfully' do
+      it 'allows the user to sign in successfully' do
         expect(page).to have_content 'Sign out'
       end
 
@@ -38,40 +38,40 @@ describe 'inviting a user that has already signed up' do
         expect(page).to_not have_content('An error occurred')
       end
 
-      it 'should not change their default permissions from signing up' do
+      it 'does not change their default permissions' do
         expect(betty.permission.can_manage_team?).to eq(true)
         expect(betty.permission.can_manage_locations?).to eq(true)
       end
     end
   end
 
-  context 'when claire has already signed up but has NOT yet confirmed their account' do
+  context 'when user is an unconfirmed user' do
     include_examples 'confirmation use case spy'
 
-    let(:claires_email) { 'notconfirmedyet@gov.uk' }
+    let(:unconfirmed_email) { 'notconfirmedyet@gov.uk' }
 
-    before { sign_up_for_account(email: claires_email) }
+    before { sign_up_for_account(email: unconfirmed_email) }
 
-    it 'sends a confirmation link to claire' do
+    it 'sends a confirmation link' do
       expect(ConfirmationUseCaseSpy.confirmations_count).to eq(1)
       expect(URI(confirmation_email_link).scheme).to eq("https")
     end
 
-    context 'and a confirmed user invites them' do
+    context 'and is invited by another user' do
       before do
         sign_in_user confirmed_user
         visit new_user_invitation_path
-        fill_in "Email", with: claires_email
+        fill_in "Email", with: unconfirmed_email
         click_on "Send invitation email"
       end
 
-      it "does not send an invitation to an existing user" do
+      it "does not send an invitation" do
         expect(InviteUseCaseSpy.invite_count).to eq(0)
         expect(page).to have_content("Email is already associated with an account. If you can't sign in, reset your password")
       end
 
-      context 'when claire visits the confirmation link after the confirmed user tried to invite them' do
-        let(:claire) { User.find_by(email: claires_email) }
+      context 'when user uses the confirmation link after invitation attempt' do
+        let(:claire) { User.find_by(email: unconfirmed_email) }
 
         before { visit confirmation_email_link }
 
@@ -80,12 +80,12 @@ describe 'inviting a user that has already signed up' do
           expect(page).to have_content("Create your account")
         end
 
-        it 'should not change the default permissions from signing up' do
+        it 'does not change the default permissions from signing up' do
           expect(claire.permission.can_manage_team?).to eq(true)
           expect(claire.permission.can_manage_locations?).to eq(true)
         end
 
-        it 'signs them in successfully after they have completed creating their account' do
+        it 'signs them in successfully' do
           update_user_details(name: 'claire', organisation_name: 'Org 2')
           expect(page).to have_content 'Sign out'
         end
