4696c2e7339409b6698a0cd6216a40fe9da66460
Tom Taylor <tommotaylor@gmail.com>
2019-03-22 10:45:38 +0000

    Improve spec

--- spec/features/download_poster_spec.rb
+++ spec/features/download_poster_spec.rb
@@ -6,7 +6,12 @@ describe 'Downloading the GovWifi poster', type: :feature do
     visit root_path
   end
 
+  it 'displays the download poster link' do
+    expect(page).to have_link('Download a poster to advertise GovWifi is available in your building(s)')
+  end
+
   it 'allows the user to click on link and download the poster' do
-    expect(page).to have_content("Download a poster to advertise GovWifi is available in your building(s)")
+    click_on 'Download a poster to advertise GovWifi is available in your building(s)'
+    expect(page.response_headers['Content-Type']).to eq('image/png')
   end
 end
