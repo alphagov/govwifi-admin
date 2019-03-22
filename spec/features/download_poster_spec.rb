aa7a067a6d685ffa20ee1fb2a8ba976855903a5c
Tom Taylor <tommotaylor@gmail.com>
2019-03-22 10:39:59 +0000

    Fix linting errors

--- spec/features/download_poster_spec.rb
+++ spec/features/download_poster_spec.rb
@@ -1,14 +1,12 @@
-describe 'Allowing a user to download the GovWifi poster' do
+describe 'Downloading the GovWifi poster', type: :feature do
   let(:user) { create(:user) }
 
-  context 'shows the download poster link on the home page' do
-    before do
-      sign_in_user user
-      visit root_path
-    end
+  before do
+    sign_in_user user
+    visit root_path
+  end
 
-    it 'allows the user to click on link and download the poster' do
-      expect(page).to have_content("Download a poster to advertise GovWifi is available in your building(s)")
-    end
+  it 'allows the user to click on link and download the poster' do
+    expect(page).to have_content("Download a poster to advertise GovWifi is available in your building(s)")
   end
 end
