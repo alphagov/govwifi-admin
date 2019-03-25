9d1a6c76ae7ee22e34cd4e6ef1f77703d0728c73
George Schena <george@madetech.com>
2019-03-25 11:19:31 +0000

    Refactor spec

--- spec/features/super_admin/view_logs_spec.rb
+++ spec/features/super_admin/view_logs_spec.rb
@@ -1,20 +1,26 @@
 describe "View authentication requests for a username", type: :feature do
-  context "with super admin privileages" do
-    let(:username) { "Larry" }
+  let(:username) { "Larry" }
 
-    let!(:super_admin) { create(:user, super_admin: true) }
-    let(:super_admin_location) { create(:location, organisation: super_admin.organisation) }
-    let!(:super_admin_ip) { create(:ip, location: super_admin_location) }
+  let(:super_admin) { create(:user, super_admin: true) }
+  let(:super_admin_location) { create(:location, organisation: super_admin.organisation) }
+  let(:super_admin_ip) { create(:ip, location: super_admin_location) }
 
-    let(:organisation) { create(:organisation) }
-    let(:location) { create(:location, organisation: organisation) }
-    let!(:ip) { create(:ip, location: location) }
+  let(:organisation) { create(:organisation) }
+  let(:location) { create(:location, organisation: organisation) }
+  let(:ip) { create(:ip, location: location) }
 
-    let(:organisation_2) { create(:organisation) }
-    let(:location_2) { create(:location, organisation: organisation_2) }
-    let!(:ip_2) { create(:ip, location: location_2) }
+  let(:organisation_2) { create(:organisation) }
+  let(:location_2) { create(:location, organisation: organisation_2) }
+  let(:ip_2) { create(:ip, location: location_2) }
 
+  before do
+    sign_in_user super_admin
+    visit new_logs_search_path
+    choose "Username"
+    click_on "Go to search"
+  end
 
+  context "when there are results from multiple organisations and users" do
     before do
       Session.create!(
         start: 3.days.ago,
@@ -45,18 +51,50 @@ describe "View authentication requests for a username", type: :feature do
         building_identifier: ""
       )
 
-      sign_in_user super_admin
-      visit new_logs_search_path
-      choose "Username"
-      click_on "Go to search"
+      Session.create!(
+        start: 3.days.ago,
+        username: "user2",
+        mac: "",
+        ap: "",
+        siteIP: ip_2.address,
+        success: true,
+        building_identifier: ""
+      )
+
       fill_in "Username", with: username
       click_on "Show logs"
     end
 
-    context "when username is correct" do
-      it "displays three results" do
-        expect(page).to have_content("Found 3 results for \"#{username}\"")
-      end
+    it "displays all results for the searched user across mutliple organisations" do
+      expect(page).to have_content("Found 3 results for \"#{username}\"")
+    end
+
+    it "does not display results for other users" do
+      expect(page).not_to have_content("user2")
+    end
+  end
+
+  context "when no results exist" do
+    before do
+      fill_in "Username", with: username
+      click_on "Show logs"
+    end
+
+    it "displays the no results message" do
+      expect(page).to have_content("\"#{username}\" isn't reaching the GovWifi service")
+    end
+  end
+
+  context "when username is incorrect" do
+    let(:username) { "1" }
+
+    before do
+      fill_in "Username", with: username
+      click_on "Show logs"
+    end
+
+    it "prompts the user for a valid username" do
+      expect(page).to have_content("Enter a valid username")
     end
   end
 end
