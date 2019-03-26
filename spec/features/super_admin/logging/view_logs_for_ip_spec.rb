85fe31dfe3c0b0f5b85729c75cbb388a0de6c69c
George Schena <george@madetech.com>
2019-03-26 11:34:58 +0000

    pr changes

--- spec/features/super_admin/logging/view_logs_for_ip_spec.rb
+++ spec/features/super_admin/logging/view_logs_for_ip_spec.rb
@@ -1,31 +1,39 @@
 describe 'View authentication requests for an IP', type: :feature do
-  let(:other_user) { create(:user, organisation: other_user_organisation) }
-  let(:other_user_organisation) { create(:organisation) }
-  let(:other_user_location) { create(:location, organisation: other_user.organisation) }
+  let(:user) { create(:user, organisation: user_organisation) }
+  let(:user_organisation) { create(:organisation) }
+  let(:user_location) { create(:location, organisation: user.organisation) }
+
+  let(:user_2) { create(:user, organisation: user_2_organisation) }
+  let(:user_2_organisation) { create(:organisation) }
+  let(:user_2_location) { create(:location, organisation: user_2.organisation) }
 
   let(:super_admin) { create(:user, super_admin: true) }
   let(:super_admin_organisation) { create(:organisation) }
   let(:super_admin_location) { create(:location, organisation: super_admin.organisation) }
 
-  let(:ip) { '1.2.3.4' }
+  let(:ip_1) { create(:ip, location_id: user_location.id, address: '1.2.3.4') }
+  let(:ip_2) { create(:ip, location_id: user_2_location.id, address: '1.2.3.5') }
 
   before do
     Session.create!(
       start: 3.days.ago,
-      siteIP: ip,
+      siteIP: ip_1.address,
     )
 
     Session.create!(
       start: 3.days.ago,
-      siteIP: ip,
+      siteIP: ip_1.address,
     )
 
     Session.create!(
       start: 3.days.ago,
-      siteIP: ip,
+      siteIP: ip_2.address,
     )
 
-    create(:ip, location_id: super_admin_location.id, address: ip)
+    Session.create!(
+      start: 3.days.ago,
+      siteIP: ip_2.address,
+    )
 
     sign_in_user super_admin
   end
@@ -37,26 +45,26 @@ describe 'View authentication requests for an IP', type: :feature do
       click_on "Show logs"
     end
 
-    context "with a correct IP" do
-      let(:search_string) { ip }
+    context "with a correct IP from another organisation" do
+      let(:search_string) { ip_1.address }
 
-      it 'displays all results for the searched IP' do
-        expect(page).to have_content("Found 3 results for \"#{ip}\"")
+      it 'displays all results for that searched IP' do
+        expect(page).to have_content("Found 2 results for \"#{ip_1.address}\"")
       end
     end
 
     context "when entering an IP address that belongs to another organisation" do
-      let(:search_string) { ip }
+      let(:search_string) { ip_2.address }
 
       before do
-        sign_in_user other_user
+        sign_in_user user
         visit ip_new_logs_search_path
         fill_in "IP address", with: search_string
         click_on "Show logs"
       end
 
       it "does not display any logs" do
-        expect(page).not_to have_content("Found 3 results for \"#{ip}\"")
+        expect(page).not_to have_content("Found 2 results for \"#{ip_2.address}\"")
       end
     end
 
