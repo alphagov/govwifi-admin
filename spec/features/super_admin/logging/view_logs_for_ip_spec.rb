b3d6945914de266d60ef739f06e1436781d465be
George Schena <george@madetech.com>
2019-03-26 12:33:57 +0000

    DONE

--- spec/features/super_admin/logging/view_logs_for_ip_spec.rb
+++ spec/features/super_admin/logging/view_logs_for_ip_spec.rb
@@ -1,11 +1,11 @@
 describe 'View authentication requests for an IP', type: :feature do
   let(:user) { create(:user) }
   let(:user_location) { create(:location, organisation: user.organisation) }
-  let(:ip_1) { create(:ip, location_id: user_location.id, address: '1.2.3.4') }
+  let(:ip_1) { create(:ip, location: user_location, address: '1.2.3.4') }
 
   let(:user_2) { create(:user) }
   let(:user_2_location) { create(:location, organisation: user_2.organisation) }
-  let(:ip_2) { create(:ip, location_id: user_2_location.id, address: '1.2.3.5') }
+  let(:ip_2) { create(:ip, location: user_2_location, address: '1.2.3.5') }
 
   let(:super_admin) { create(:user, super_admin: true) }
 
