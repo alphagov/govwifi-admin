5670f732fcadeb6d2d9b5a2466d2125f2b08473c
Renny Fadoju <rennyvbirds@yahoo.com>
2019-03-29 10:40:51 +0000

    Refactor tests

--- spec/models/organisation_spec.rb
+++ spec/models/organisation_spec.rb
@@ -42,42 +42,32 @@ describe Organisation do
     end
 
     context 'when my organisation is not in the GovUk register' do
+      let(:organisation) { build(:organisation, name: 'Some invalid organisation name') }
+
       it 'is not valid' do
-        expect { create(:organisation, name: 'Some invalid organisation name') }.to raise_error ActiveRecord::RecordInvalid
+        expect(organisation).not_to be_valid
+      end
+
+      it 'explains why it is invalid' do
+        organisation.valid?
+        expect(organisation.errors.full_messages).to eq([
+          "#{organisation.name} isn't a whitelisted organisation"
+        ])
       end
     end
 
-    context 'when adding an organisation name at account confirmation' do
-      let(:organisation) { build(:organisation, name: organisation_name) }
+    context 'when organisation name is left blank' do
+      let(:organisation) { build(:organisation, name: '') }
 
-      context 'when organisation name is left blank' do
-        let(:organisation_name) { '' }
-
-        it 'is invalid' do
-          expect(organisation).not_to be_valid
-        end
-
-        it 'explains why it is invalid' do
-          organisation.valid?
-          expect(organisation.errors.full_messages).to eq([
-            "Name can't be blank"
-          ])
-        end
+      it 'is invalid' do
+        expect(organisation).not_to be_valid
       end
 
-      context 'when organisation name is not in register' do
-        let(:organisation_name) { 'Some Name' }
-
-        it 'is invalid' do
-          expect(organisation).not_to be_valid
-        end
-
-        it 'calls this method' do
-          organisation.valid?
-          expect(organisation.errors.full_messages).to eq([
-            "#{organisation.name} isn't a whitelisted organisation"
-          ])
-        end
+      it 'explains why it is invalid' do
+        organisation.valid?
+        expect(organisation.errors.full_messages).to eq([
+          "Name can't be blank"
+        ])
       end
     end
   end
