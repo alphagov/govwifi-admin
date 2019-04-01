a560544f4bd45134aed3942dbb38bda3ab95b821
Anthony King <anthony@madetech.com>
2019-04-01 12:03:47 +0100

    add a request spec to ensure the organisation_id parameter is not respected when sending requests as a normal user

---
+++ spec/requests/users/invitations/create_spec.rb
@@ -0,0 +1,35 @@
+require 'support/invite_use_case'
+require 'support/notifications_service'
+
+describe "POST /users/invitation", type: :request do
+  let(:user) { create(:user) }
+  let(:organisation) { user.organisation }
+
+  before do
+    https!
+    login_as(user, scope: :user)
+  end
+
+  include_examples 'notifications service'
+  include_examples 'invite use case spy'
+
+  context 'with tampered organisation_id parameter' do
+    let(:email) { 'barry@gov.uk' }
+    let(:other_organisation) { create(:organisation) }
+
+    before do
+      post user_invitation_path, params: { user: {
+        email: email,
+        organisation_id: other_organisation.id
+      } }
+    end
+
+    it 'invites a user' do
+      expect(InviteUseCaseSpy.invite_count).to eq(1)
+    end
+
+    it 'ignores provided organisation_id' do
+      expect(User.find_by(email: email).organisation_id).to eq(organisation.id)
+    end
+  end
+end
