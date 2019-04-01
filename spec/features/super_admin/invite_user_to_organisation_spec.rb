752835659a1f41ff0c1c53e7ae986865d8252285
Anthony King <anthony@madetech.com>
2019-04-01 11:44:34 +0100

    ensure we get error messages and that we can still send invitations after a failure

--- spec/features/super_admin/invite_user_to_organisation_spec.rb
+++ spec/features/super_admin/invite_user_to_organisation_spec.rb
@@ -4,15 +4,18 @@ require 'support/notifications_service'
 describe "Inviting a team member as a super admin", type: :feature do
   let(:organisation) { create(:organisation, name: "Gov Org 3") }
   let(:super_admin) { create(:user, super_admin: true) }
+  let(:email) { 'barry@gov.uk' }
 
   before do
     sign_in_user super_admin
     visit admin_organisation_path(organisation)
     click_on 'Add team member'
-    fill_in 'Email address', with: 'barry@gov.uk'
+    fill_in 'Email address', with: email
   end
 
   include_examples 'notifications service'
+  include_examples 'invite use case spy'
+
 
   it "will take the user to the organisation when they click 'back to organisation'" do
     click_on 'Back to organisation'
@@ -36,5 +39,44 @@ describe "Inviting a team member as a super admin", type: :feature do
   it "will redirect the user to the organisation page on success" do
     click_on 'Send invitation email'
     expect(page).to have_current_path(admin_organisation_path(organisation))
+  end
+
+  context "without an email address" do
+    let(:email) { "" }
+
+    before do
+      click_on 'Send invitation email'
+    end
+
+    it "does not send an invite" do
+      expect(InviteUseCaseSpy.invite_count).to eq(0)
+    end
+
+    it "displays the correct error message" do
+      expect(page).to have_content("Email can't be blank")
+    end
+
+    context "when retying with a valid email address" do
+      let(:email_second_attempt) { "barry@gov.uk" }
+
+      before do
+        click_on 'Send invitation email'
+        fill_in 'Email address', with: email_second_attempt
+        click_on 'Send invitation email'
+      end
+
+      it "sends an invite" do
+        expect(InviteUseCaseSpy.invite_count).to eq(1)
+      end
+
+      it "adds the invited user to the correct organisation" do
+        user = User.find_by(email: 'barry@gov.uk')
+        expect(user.organisation).to eq(organisation)
+      end
+
+      it "will redirect the user to the organisation page on success" do
+        expect(page).to have_current_path(admin_organisation_path(organisation))
+      end
+    end
   end
 end
