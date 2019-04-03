16b16a2c12cbd9295b56a12f91a05a2e13eab48b
George Schena <george@madetech.com>
2019-04-03 14:45:35 +0100

    changes

--- spec/features/super_admin/view_map_of_locations_for_govwifi_spec.rb
+++ spec/features/super_admin/view_map_of_locations_for_govwifi_spec.rb
@@ -6,5 +6,9 @@ describe 'Signing in as a super admin', type: :feature do
       sign_in_user user
       visit admin_govwifi_map_index_path
     end
+
+    it 'shows the title of the page' do
+      expect(page).to have_content('GovWifi Map of Locations')
+    end
   end
 end
