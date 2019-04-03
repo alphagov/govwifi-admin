4fee0bbad1a1ad9ed7057e16916c0139f7667618
Renny Fadoju <rennyvbirds@yahoo.com>
2019-04-03 16:47:55 +0100

    lint location_spec file

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -2,64 +2,65 @@ describe Location do
   it { is_expected.to validate_presence_of(:address) }
   it { is_expected.to validate_presence_of(:postcode) }
 
-  context 'associations' do
+  context 'with associations' do
     it { is_expected.to belong_to(:organisation) }
     it { is_expected.to have_many(:ips) }
   end
 
   describe '#save' do
-    subject { build(:location, organisation: organisation) }
+    subject(:location) { build(:location, organisation: organisation) }
 
     let(:organisation) { create(:organisation) }
 
-    before { subject.save }
+    before { location.save }
 
     it 'sets the radius_secret_key' do
-      expect(subject.radius_secret_key).to be_present
+      expect(location.radius_secret_key).to be_present
     end
   end
 
   describe '#full_address' do
+    subject(:location) { described_class.new }
+
     before do
-      subject.address = address
-      subject.postcode = postcode
+      location.address = address
+      location.postcode = postcode
     end
 
-    context 'with address' do
+    context 'with address and postcode' do
       let(:address) { '121 Fictional Street' }
+      let(:postcode) { 'FI5 S67' }
 
-      context 'and postcode' do
-        let(:postcode) { 'FI5 S67' }
-
-        it 'combines the two' do
-          expect(subject.full_address).to eq('121 Fictional Street, FI5 S67')
-        end
+      it 'combines the two' do
+        expect(location.full_address).to eq('121 Fictional Street, FI5 S67')
       end
     end
   end
 
   describe 'Entering a postcode' do
-    context 'which is in the incorrect format' do
+    subject(:location) { described_class.new }
+
+    context 'when in the incorrect format' do
       it 'errors as the postcode does not match the correct format' do
-        subject.postcode = 'WHATEVER POSTCODE'
-        expect(subject).not_to be_valid
+        location.postcode = 'WHATEVER POSTCODE'
+        expect(location).not_to be_valid
       end
 
       it 'errors as the postcode is empty' do
-        subject.postcode = ''
-        expect(subject).not_to be_valid
+        location.postcode = ''
+        expect(location).not_to be_valid
       end
 
       it 'errors as the postcode is nil' do
-        subject.postcode = nil
-        expect(subject).not_to be_valid
+        location.postcode = nil
+        expect(location).not_to be_valid
       end
     end
   end
 
   describe 'saving invalid IPs with mix of hash and strong parameters' do
-    it 'does not save invalid IPs' do
-      location = described_class.create(
+    let(:location) {
+      described_class.create(
         address: '6-8 HEMMING ST',
         postcode: '',
         organisation_id: create(:organisation).id,
@@ -68,8 +69,13 @@ describe Location do
           "1" => ActionController::Parameters.new(address: 'wrong')
         ).permit!
       )
+    }
 
+    it 'does not save invalid IPs' do
       expect(location.save).to be_falsey
+    end
+
+    it 'is invalid' do
       expect(location).not_to be_valid
     end
   end
