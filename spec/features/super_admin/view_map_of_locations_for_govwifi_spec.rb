6bc4c4afcff289f4e5f04667c1757c0d5d9869d6
George Schena <george@madetech.com>
2019-04-03 14:39:47 +0100

    remove dependencies

--- spec/features/super_admin/view_map_of_locations_for_govwifi_spec.rb
+++ spec/features/super_admin/view_map_of_locations_for_govwifi_spec.rb
@@ -6,9 +6,5 @@ describe 'Signing in as a super admin', type: :feature do
       sign_in_user user
       visit admin_govwifi_map_index_path
     end
-
-    it 'shows list of signed organisations on the home page' do
-      expect(page).to have_content('GovWifi Map of Locations')
-    end
   end
 end
