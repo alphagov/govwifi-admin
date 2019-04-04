c3e845237e0a546b1422dcd99428b762a9ba50b6
George Schena <george@madetech.com>
2019-04-04 13:09:47 +0100

    more tests

--- spec/gateways/coordinates_spec.rb
+++ spec/gateways/coordinates_spec.rb
@@ -1,17 +1,45 @@
 describe Gateways::Coordinates do
   subject(:postcode_coordinates_gateway) { described_class.new(postcode: postcode) }
 
-  before do
-    stub_request(:get, "http://api.postcodes.io/postcodes/HA72BL").
-    to_return(status: 200, body: File.read("#{Rails.root}/spec/fixtures/postcode_conversion_payload.json"))
-  end
-
-  context 'when converting a valid postcode to long and lat' do
+  context 'when given a valid postcode' do
     let(:postcode) { 'HA72BL' }
 
-    it 'Converts a given postcode to long and latitude' do
+    before do
+      stub_request(:get, "http://api.postcodes.io/postcodes/HA72BL").
+        to_return(status: 200,
+          body: {
+            status: 200,
+            result: {
+              longitude: -0.316963,
+              latitude: 51.604163
+            }
+          }.to_json)
+    end
+
+    it 'Converts the postcode to long and latitude' do
       result = postcode_coordinates_gateway.fetch_coordinates
       expect(result[:coordinates]).to eq([-0.316963, 51.604163])
+    end
+  end
+
+  context 'when given an invalid postcode' do
+    let(:postcode) { 'not_a_valid_postcode' }
+
+    before do
+      stub_request(:get, "http://api.postcodes.io/postcodes/not_a_valid_postcode").
+        to_return(status: 404,
+          body: {
+            status: 404,
+            result: {
+              longitude: -0.316963,
+              latitude: 51.604163
+          }
+        }.to_json)
+    end
+
+    it 'will error' do
+      result = postcode_coordinates_gateway.fetch_coordinates
+      expect(result[:error]).to eq("Invalid postcode")
     end
   end
 end
