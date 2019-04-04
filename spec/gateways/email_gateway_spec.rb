39fbf72f286892a2a8d55c27b67fdd231426c004
Renny Fadoju <rennyvbirds@yahoo.com>
2019-04-04 16:33:01 +0100

    lint email gateway spec file

--- spec/gateways/email_gateway_spec.rb
+++ spec/gateways/email_gateway_spec.rb
@@ -1,14 +1,16 @@
 require 'support/notifications_service'
 
 describe EmailGateway do
+  subject(:email_gateway) { described_class.new }
+
+  let(:notification) { instance_spy(Notifications::Client, send_email: nil) }
+
   before do
     ENV['NOTIFY_API_KEY'] = 'dummy_key-00000000-0000-0000-0000-000000000000-00000000-0000-0000-0000-000000000000'
-
-    expect_any_instance_of(Notifications::Client).to \
-      receive(:send_email).with(notifications_payload)
+    allow(Notifications::Client).to receive(:new).and_return(notification)
   end
 
-  context 'confirmation emails' do
+  context 'with confirmation emails' do
     let(:email) { 'test@example.com' }
     let(:confirmation_url) { 'http://example.com/confirm?token=123' }
     let(:template_id) { 1 }
@@ -22,13 +24,17 @@ describe EmailGateway do
       }
     end
 
-    it 'calls notifications' do
-      subject.send(
+    before do
+      email_gateway.send(
         email: email,
         template_id: template_id,
         locals: { confirmation_url: confirmation_url },
         reference: reference
       )
+    end
+
+    it 'calls notifications' do
+      expect(notification).to have_received(:send_email).with(notifications_payload)
     end
   end
 end
