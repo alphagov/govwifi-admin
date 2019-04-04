b06973795b4306515c28022747a1c65b4851a835
Anthony King <anthony@madetech.com>
2019-04-04 10:56:58 +0100

    be permissive around failing command

--- ci/tasks/scripts/with-docker.sh
+++ ci/tasks/scripts/with-docker.sh
@@ -3,7 +3,7 @@
 source /docker-helpers.sh
 start_docker
 
-ls -lsa /mysql-image
+ls -lsa mysql-image || echo "failed to find mysql-image"
 
 docker load -i mysql-image/image
 docker load -i nginx-image/image
