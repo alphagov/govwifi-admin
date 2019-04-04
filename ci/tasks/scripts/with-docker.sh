4027d68c36abbed67d78763a74b12cf4916bd66b
Anthony King <anthony@madetech.com>
2019-04-04 14:03:42 +0100

    load docker images in parallel

--- ci/tasks/scripts/with-docker.sh
+++ ci/tasks/scripts/with-docker.sh
@@ -3,9 +3,9 @@
 source /docker-helpers.sh
 start_docker
 
-ls -lsa govwifi-admin-prebuilt-cached || echo "failed to find cached prebuilt"
+docker load -i mysql-image/image.tar &
+docker load -i nginx-image/image.tar &
+docker load -i ruby-image/image.tar &
+[[ -f "govwifi-admin-prebuilt/image.tar" ]] && docker load -i "govwifi-admin-prebuilt/image.tar" &
 
-docker load -i mysql-image/image.tar
-docker load -i nginx-image/image.tar
-docker load -i ruby-image/image.tar
-[[ -f "govwifi-admin-prebuilt/image.tar" ]] && docker load -i "govwifi-admin-prebuilt/image.tar"
+wait
