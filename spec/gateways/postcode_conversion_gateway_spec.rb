df89f1e7d5a222d6bd1b69faad2b5678f3422842
George Schena <george@madetech.com>
2019-04-04 11:33:11 +0100

    gateway changes

--- spec/gateways/postcode_conversion_gateway_spec.rb
+++ spec/gateways/postcode_conversion_gateway_spec.rb
@@ -1,13 +1,21 @@
 describe Gateways::PostcodeConversionGateway do
-  subject(:postcode_conversion) { described_class.new }
+  subject(:postcode_coordinates_gateway) { described_class.new(postcode: postcode) }
 
-  it 'Converts a given postcode to long and latitude' do
-    result = postcode_conversion.convert_postcode_to_long_and_lat('HA7 2BL')
-    expect(result).to eq([-0.316963, 51.604163])
+  context 'when converting a valid postcode to long and lat' do
+    let(:postcode) { 'HA72BL' }
+
+    it 'Converts a given postcode to long and latitude' do
+      result = postcode_coordinates_gateway.fetch_coordinates
+      expect(result).to eq([-0.316963, 51.604163])
+    end
   end
 
-  it 'will error with an invalid postcode' do
-    result = postcode_conversion.convert_postcode_to_long_and_lat('not a valid postcode')
-    expect(result).to eq('Invalid postcode')
+  context 'when converting an invalid postcode to long and lat' do
+    let(:postcode) { 'not a valid postcode' }
+
+    it 'will error with an invalid postcode' do
+      result = postcode_coordinates_gateway.fetch_coordinates
+      expect(result).to eq('Invalid postcode')
+    end
   end
 end
