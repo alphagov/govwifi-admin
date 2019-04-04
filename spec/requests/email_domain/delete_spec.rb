a9ce93e356e0f7be673d7d87551008c5635fd650
Renny Fadoju <rennyvbirds@yahoo.com>
2019-04-04 12:30:07 +0100

    lint email domain delete request spec

--- spec/requests/email_domain/delete_spec.rb
+++ spec/requests/email_domain/delete_spec.rb
@@ -1,5 +1,11 @@
 describe "DELETE /authorised_email_domains/:id", type: :request do
   let!(:email_domain) { create(:authorised_email_domain, name: 'some.domain.org.uk') }
+  let(:gateway_instance) { instance_double(Gateways::S3) }
+
+  before do
+    allow(Gateways::S3).to receive(:new).and_return(gateway_instance)
+    allow(gateway_instance).to receive(:write)
+  end
 
   context "when the user is a super admin" do
     before do
@@ -13,9 +19,9 @@ describe "DELETE /authorised_email_domains/:id", type: :request do
       }.to change(AuthorisedEmailDomain, :count).by(-1)
     end
 
-    it 'publishes the authorised domains to S3' do
-      expect_any_instance_of(Gateways::S3).to receive(:write)
+    it 'publishes the new list of authorised domains to S3' do
       delete admin_authorised_email_domain_path(email_domain)
+      expect(gateway_instance).to have_received(:write)
     end
   end
 
