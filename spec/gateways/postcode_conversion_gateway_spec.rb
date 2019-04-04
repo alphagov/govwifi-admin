45aadcfa443f139190696d015ffc11e53873b906
George Schena <george@madetech.com>
2019-04-04 10:01:25 +0100

    negative test case

--- spec/gateways/postcode_conversion_gateway_spec.rb
+++ spec/gateways/postcode_conversion_gateway_spec.rb
@@ -5,4 +5,9 @@ describe Gateways::PostcodeConversionGateway do
     result = postcode_conversion.convert_postcode_to_long_and_lat('HA7 2BL')
     expect(result).to eq([-0.316963, 51.604163])
   end
+
+  it 'will error with an invalid postcode' do
+    result = postcode_conversion.convert_postcode_to_long_and_lat('not a valid postcode')
+    expect(result).to eq('Invalid postcode')
+  end
 end
