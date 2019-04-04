890cce2b2e7b5186fb8beaa9b0562c36328efef0
Anthony King <anthony@madetech.com>
2019-04-04 11:07:19 +0100

    fix file reference

--- ci/tasks/scripts/with-docker.sh
+++ ci/tasks/scripts/with-docker.sh
@@ -5,6 +5,6 @@ start_docker
 
 ls -lsa mysql-image || echo "failed to find mysql-image"
 
-docker load -i mysql-image/image
-docker load -i nginx-image/image
-docker load -i ruby-image/image
+docker load -i mysql-image/image.tar
+docker load -i nginx-image/image.tar
+docker load -i ruby-image/image.tar
