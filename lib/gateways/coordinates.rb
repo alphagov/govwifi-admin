f1d3ef7394ebcc2429933722bbf63f83416780c3
George Schena <george@madetech.com>
2019-04-05 12:49:00 +0100

    gateway takes array of postcodes

--- lib/gateways/coordinates.rb
+++ lib/gateways/coordinates.rb
@@ -1,24 +1,31 @@
 module Gateways
   class Coordinates
-    def initialize(postcode:)
-      @postcode = postcode
+    def initialize(postcodes: [] )
+      @postcodes = postcodes
     end
 
     def fetch_coordinates
-      response = HTTParty.get("http://api.postcodes.io/postcodes/#{postcode}")
+      response = HTTParty.post("http://api.postcodes.io/postcodes")
+
       result = JSON.parse(response.body)
 
       if result['status'] == 404
         return { success: false, coordinates: [], error: "Invalid postcode" }
       else
-        longitude = result['result']['longitude']
-        latitude = result['result']['latitude']
-        { success: true, coordinates: [longitude, latitude], error: nil }
+        coordinates = []
+
+        result["result"].each do | o |
+          longitude = o["result"]["longitude"]
+          latitude = o["result"]["latitude"]
+          coordinates << [longitude, latitude]
+        end
+
+        { success: true, coordinates: coordinates, error: nil }
       end
     end
 
   private
 
-    attr_reader :postcode
+    attr_reader :postcodes
   end
 end
