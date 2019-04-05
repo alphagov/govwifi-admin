9a7eeb28013cabb73f50cf3abf9f36ea5ef2b810
George Schena <george@madetech.com>
2019-04-05 14:20:05 +0100

    more stuff

--- spec/gateways/coordinates_spec.rb
+++ spec/gateways/coordinates_spec.rb
@@ -1,4 +1,4 @@
-describe Gateways::Coordinates, focus: true do
+describe Gateways::Coordinates do
   subject(:postcode_coordinates_gateway) { described_class.new(postcodes: postcode) }
 
   context 'when given a valid postcode' do
@@ -43,24 +43,24 @@ describe Gateways::Coordinates, focus: true do
     end
   end
 
-  context 'when given an invalid postcode' do
-    let(:postcode) { 'not_a_valid_postcode' }
+  # context 'when given an invalid postcode' do
+  #   let(:postcode) { 'not_a_valid_postcode' }
 
-    before do
-      stub_request(:get, "http://api.postcodes.io/postcodes/not_a_valid_postcode").
-        to_return(status: 404,
-          body: {
-            status: 404,
-            result: {
-              longitude: -0.316963,
-              latitude: 51.604163
-          }
-        }.to_json)
-    end
+  #   before do
+  #     stub_request(:get, "http://api.postcodes.io/postcodes/not_a_valid_postcode").
+  #       to_return(status: 404,
+  #         body: {
+  #           status: 404,
+  #           result: {
+  #             longitude: -0.316963,
+  #             latitude: 51.604163
+  #         }
+  #       }.to_json)
+  #   end
 
-    it 'will error' do
-      result = postcode_coordinates_gateway.fetch_coordinates
-      expect(result[:error]).to eq("Invalid postcode")
-    end
-  end
+  #   it 'will error' do
+  #     result = postcode_coordinates_gateway.fetch_coordinates
+  #     expect(result[:error]).to eq("Invalid postcode")
+  #   end
+  # end
 end
