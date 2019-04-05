7c3b38548168015ee55a9eaf766d5604021f4225
Renny Fadoju <rennyvbirds@yahoo.com>
2019-04-05 10:09:40 +0100

    lint route 53 gateway spec

--- spec/gateways/route53_spec.rb
+++ spec/gateways/route53_spec.rb
@@ -1,9 +1,11 @@
 describe Gateways::Route53 do
-  it '.list_health_checks' do
-    expect(subject.list_health_checks.health_checks).to be_empty
+  subject(:route_gateway) { described_class.new }
+
+  it 'calls the .list_health_checks method' do
+    expect(route_gateway.list_health_checks.health_checks).to be_empty
   end
 
-  it '.get_health_check_status' do
-    expect(subject.get_health_check_status(health_check_id: 'abc').health_check_observations).to be_empty
+  it 'calls the .get_health_check_status method' do
+    expect(route_gateway.get_health_check_status(health_check_id: 'abc').health_check_observations).to be_empty
   end
 end
