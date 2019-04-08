cbb34782285777cea9fcd02204c85b0b28269fb7
George Schena <george@madetech.com>
2019-04-08 10:56:13 +0100

    plots on google maps

--- app/controllers/admin/govwifi_map_controller.rb
+++ app/controllers/admin/govwifi_map_controller.rb
@@ -1,17 +1,8 @@
 class Admin::GovwifiMapController < AdminController
   def index
     @postcodes = Location.pluck(:postcode)
+
     @coordinates = convert_postcodes
-
-    pp convert_postcodes
-
-    # get all postcodes
-
-    # send postcodes to gateway and save coordinates
-
-    # combine coordinates into array of the correct shape
-
-    # pass array of coordinates to view
   end
 
   def convert_postcodes
