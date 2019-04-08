9870bd34167e30677c8b7c6d20c5f47dbc95f0e5
George Schena <george@madetech.com>
2019-04-08 17:39:51 +0100

    batches

--- lib/gateways/coordinates.rb
+++ lib/gateways/coordinates.rb
@@ -5,23 +5,28 @@ module Gateways
     end
 
     def fetch_coordinates
-      response = HTTParty.post("http://api.postcodes.io/postcodes", body: { postcodes: postcodes })
+      combined_coordinates = []
 
-      result = JSON.parse(response.body)
+      postcodes.each_slice(100) do |batch|
+        response = HTTParty.post("http://api.postcodes.io/postcodes", body: { postcodes: batch })
+        result = JSON.parse(response.body)
+        combined_coordinates << result
+        final_coordinates = combined_coordinates.first
 
-      if result['result'].length < 2
-        return { success: false, coordinates: [], error: "Invalid postcode" }
-      else
-        coordinates = []
-        result["result"].each do |o|
-          next if o["result"].nil?
+        if final_coordinates['result'].length < 2
+          return { success: false, coordinates: [], error: "Invalid postcode" }
+        else
+          coordinates = []
+          final_coordinates["result"].each do |o|
+            next if o["result"].nil?
 
-          longitude = o["result"]["longitude"]
-          latitude = o["result"]["latitude"]
-          coordinates << [latitude, longitude]
+            longitude = o["result"]["longitude"]
+            latitude = o["result"]["latitude"]
+            coordinates << [latitude, longitude]
+          end
+
+          return { success: false, coordinates: coordinates, error: nil }
         end
-
-        return { success: false, coordinates: coordinates, error: nil }
       end
     end
 
