9e5001a136e376824b03261b75f3aa30f2495b04
Anthony King <anthonydking@gmail.com>
2019-04-09 09:44:19 +0100

    Run PRs in Concourse (#610)
    * initial drop of concourse
    
    * update to utilise task file
    
    * update concourse configs
    
    * update execution permissions of scripts
    
    * add support for preloading docker images
    
    * fix inputs not being a list
    
    * fix image reference, add in debug code
    
    * be permissive around failing command
    
    * fix file reference
    
    * finish preloading images, add support for prebuilding our images
    
    * ensure the image is registered in compose
    
    * save the image using the repository name
    
    * restructure building and pipelines to use images
    
    * fix docker compose up command
    
    no combining no-start and detached mode
    
    * no building on concourse, only prebuilding
    
    * add support for caching from the previous layer
    
    * cache the prebuilt image to speed up re-runs
    
    * move initial upping to prebuild command
    
    * debug on cached directory
    
    * fix missing flag
    
    * load docker images in parallel
    
    * quieten the layer loading
    
    * correctly wait on specific PIDs
    
    * actually use docker commands instead of sleeps
    
    * actually call load_layers
    
    * add a pipeline for running on PRs
    
    * set up ignores around the ci pipelines
    
    * set up pushing pipeline
    
    * default to production image
    
    concourse builder doesn't support build args yet, so default to what we need
    
    * add support for running migrations
    
    * fix missing task name
    
    * update reference to ruby image in use
    
    * restructure to support concourse 4.2.3
    
    * rename 'push' to 'deploy'
    
    * finish up PR pipeline
    
    * don't include dev compose in dev
    
    * use a ready made runner from the private ecr
    
    * don't set a default for 'BUNDLE_INSTALL_FLAGS'
    
    * update deploy pipeline with more things
    
    * remove deployment related changes
    
    This will be moved to another PR

---
+++ ci/tasks/scripts/pre-build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+./govwifi-admin/ci/tasks/scripts/with-docker.sh
+
+cd govwifi-admin || exit
+
+[[ -f "../govwifi-admin-prebuilt-cached/image.tar" ]] && docker load -qi "../govwifi-admin-prebuilt-cached/image.tar"
+
+
+make prebuild
+docker tag "$(docker-compose images -q app)" "govwifi-admin-app-prebuilt"
+docker save "govwifi-admin-app-prebuilt" -o "../govwifi-admin-prebuilt/image.tar"
+cp "../govwifi-admin-prebuilt/image.tar" "../govwifi-admin-prebuilt-cached/image.tar"
+
