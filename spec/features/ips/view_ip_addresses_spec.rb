d68435ef7d3601367f87336b54e1a82a352b641a
Tom Taylor <tommotaylor@gmail.com>
2019-04-09 12:50:09 +0100

    Simplify view ip addresses spec

--- spec/features/ips/view_ip_addresses_spec.rb
+++ spec/features/ips/view_ip_addresses_spec.rb
@@ -23,29 +23,24 @@ describe 'Viewing IP addresses', type: :feature do
   end
 
   context 'with IPs' do
-    let(:location_one) { create(:location, organisation: user.organisation) }
-    let(:location_two) { create(:location, organisation: user.organisation) }
-    let!(:ip_one) { create(:ip, location: location_one) }
-    let!(:ip_two) { create(:ip, location: location_two) }
+    let(:location) { create(:location, organisation: user.organisation) }
+    let!(:ip) { create(:ip, location: location) }
 
     before do
       sign_in_user user
       visit ips_path
     end
 
-    it 'shows the RADIUS secret keys' do
-      displayed_secret_keys = page.all("div#radius-secret-key").map(&:text)
-      expect(displayed_secret_keys).to match_array([location_one.radius_secret_key, location_two.radius_secret_key])
+    it 'shows the RADIUS secret key' do
+      expect(page).to have_content(location.radius_secret_key)
     end
 
-    it 'shows the IPs' do
-      displayed_ips = page.all("td#ip-address").map(&:text)
-      expect(displayed_ips).to match_array([ip_one.address, ip_two.address])
+    it 'shows the IP' do
+      expect(page).to have_content(ip.address)
     end
 
-    it 'shows the locations addresses' do
-      location_addresses = page.all("span#location-address").map(&:text)
-      expect(location_addresses).to match_array([location_one.full_address, location_two.full_address])
+    it 'shows the locations address' do
+      expect(page).to have_content(location.address)
     end
   end
 
