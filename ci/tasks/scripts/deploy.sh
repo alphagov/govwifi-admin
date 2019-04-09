e6b8e563f08273ef16b2dc8f68fcaae4cac6ccb4
Anthony King <anthony@madetech.com>
2019-04-09 10:22:18 +0100

    add deploy task

---
+++ ci/tasks/scripts/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source govwifi-admin/ci/tasks/scripts/aws-helpers.sh
+
+
+function deploy() {
+  local cluster_name, service_name, deploy_stage
+
+  deploy_stage="$(stage_name)"
+  cluster_name="${deploy_stage}-admin-cluster"
+  service_name="admin-${deploy_stage}"
+
+  ecs_deploy \
+    "${cluster_name}" \
+    "${service_name}"
+}
+
+deploy
