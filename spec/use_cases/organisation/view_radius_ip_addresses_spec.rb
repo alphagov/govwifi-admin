87293e9bc8c38a67d97672ce70c2a524f05e83d0
Tom Taylor <tommotaylor@gmail.com>
2019-04-09 14:01:45 +0100

    Lint remaining use cases (#633)

--- spec/use_cases/organisation/view_radius_ip_addresses_spec.rb
+++ spec/use_cases/organisation/view_radius_ip_addresses_spec.rb
@@ -1,80 +1,53 @@
 describe UseCases::Organisation::ViewRadiusIPAddresses do
-  subject { described_class.new(organisation_id: org_id) }
+  subject(:use_case) { described_class.new(organisation_id: org_id) }
 
-  context 'with RADIUS environment variables' do
-    before do
-      ENV['LONDON_RADIUS_IPS'] = "#{radius_ip_1},#{radius_ip_2}"
-      ENV['DUBLIN_RADIUS_IPS'] = "#{radius_ip_3},#{radius_ip_4}"
-    end
+  let(:radius_ip_1) { '111.111.111.111' }
+  let(:radius_ip_2) { '121.121.121.121' }
+  let(:radius_ip_3) { '131.131.131.131' }
+  let(:radius_ip_4) { '141.141.141.141' }
 
-    context 'as organisation 1' do
-      let(:org_id) { 1 }
+  before do
+    ENV['LONDON_RADIUS_IPS'] = "#{radius_ip_1},#{radius_ip_2}"
+    ENV['DUBLIN_RADIUS_IPS'] = "#{radius_ip_3},#{radius_ip_4}"
+  end
 
-      context 'example one' do
-        let(:radius_ip_1) { '111.111.111.111' }
-        let(:radius_ip_2) { '121.121.121.121' }
-        let(:radius_ip_3) { '131.131.131.131' }
-        let(:radius_ip_4) { '141.141.141.141' }
+  context 'with organisation one' do
+    let(:org_id) { 1 }
 
-        it 'returns those IPs in a hash' do
-          expect(subject.execute).to eq(
-            london: [radius_ip_1, radius_ip_2],
-            dublin: [radius_ip_3, radius_ip_4]
-          )
-        end
-      end
-
-      context 'example two' do
-        let(:radius_ip_1) { '151.151.151.151' }
-        let(:radius_ip_2) { '161.161.161.161' }
-        let(:radius_ip_3) { '171.171.171.171' }
-        let(:radius_ip_4) { '181.181.181.181' }
-
-        it 'returns those IPs in a hash' do
-          expect(subject.execute).to eq(
-            london: [radius_ip_1, radius_ip_2],
-            dublin: [radius_ip_3, radius_ip_4]
-          )
-        end
-      end
-
-      context 'including invalid Dublin IPs' do
-        let(:radius_ip_1) { '111.111.111.111' }
-        let(:radius_ip_2) { '121.121.121.121' }
-        let(:radius_ip_3) { '131.131.131.131' }
-        let(:radius_ip_4) { 'cat.dog.bear.pig' }
-
-        it 'blows up' do
-          expect { subject.execute }.to raise_error(IPAddr::InvalidAddressError)
-        end
-      end
-
-      context 'including invalid London IPs' do
-        let(:radius_ip_1) { '111.111.111.111' }
-        let(:radius_ip_2) { 'cat.dog.bear.pig' }
-        let(:radius_ip_3) { '131.131.131.131' }
-        let(:radius_ip_4) { '141.141.141.141' }
-
-        it 'blows up' do
-          expect { subject.execute }.to raise_error(IPAddr::InvalidAddressError)
-        end
-      end
-    end
-
-    context 'as organisation 2' do
-      let(:org_id) { 2 }
-
-      let(:radius_ip_1) { '111.111.111.111' }
-      let(:radius_ip_2) { '121.121.121.121' }
-      let(:radius_ip_3) { '131.131.131.131' }
-      let(:radius_ip_4) { '141.141.141.141' }
-
-      it 'returns those IPs in a differently ordered hash' do
-        expect(subject.execute).to eq(
-          london: [radius_ip_2, radius_ip_1],
-          dublin: [radius_ip_4, radius_ip_3]
+    context 'with valid IPs' do
+      it 'returns those IPs in a hash' do
+        expect(use_case.execute).to eq(
+          london: [radius_ip_1, radius_ip_2],
+          dublin: [radius_ip_3, radius_ip_4]
         )
       end
+    end
+
+    context 'with invalid Dublin IPs' do
+      let(:radius_ip_4) { 'cat.dog.bear.pig' }
+
+      it 'raises an error' do
+        expect { use_case.execute }.to raise_error(IPAddr::InvalidAddressError)
+      end
+    end
+
+    context 'with invalid London IPs' do
+      let(:radius_ip_2) { 'cat.dog.bear.pig' }
+
+      it 'raises an error' do
+        expect { use_case.execute }.to raise_error(IPAddr::InvalidAddressError)
+      end
+    end
+  end
+
+  context 'with a different organisation' do
+    let(:org_id) { 2 }
+
+    it 'returns those IPs in a differently ordered hash' do
+      expect(use_case.execute).to eq(
+        london: [radius_ip_2, radius_ip_1],
+        dublin: [radius_ip_4, radius_ip_3]
+      )
     end
   end
 
@@ -86,8 +59,8 @@ describe UseCases::Organisation::ViewRadiusIPAddresses do
       ENV.delete('DUBLIN_RADIUS_IPS')
     end
 
-    it 'blows up' do
-      expect { subject.execute }.to raise_error(IndexError)
+    it 'raises an error' do
+      expect { use_case.execute }.to raise_error(IndexError)
     end
   end
 end
