48ebf5b21c101003b7bc690753c5fac5234b4030
Tom Taylor <tommotaylor@gmail.com>
2019-04-11 15:42:14 +0100

    Implement new whitelist page (#644)

--- spec/features/super_admin/whitelists/whitelisting_process_spec.rb
+++ spec/features/super_admin/whitelists/whitelisting_process_spec.rb
@@ -4,7 +4,17 @@ describe 'Whitelisting an organisation', type: :feature do
     visit new_admin_whitelist_path
   end
 
-  it 'displays the new page' do
-    expect(page).to have_content('Whitelist an organisation')
+  it 'displays the start page' do
+    expect(page).to have_content('Give an organisation access to GovWifi')
+  end
+
+  it 'allows the user to see the list of whitelisted organisations' do
+    click_on "Organisations whitelist"
+    expect(page).to have_content("Custom Organisations that are already in our register")
+  end
+
+  it 'allows the user to see the list of whitelisted email domains' do
+    click_on "Users whitelist"
+    expect(page).to have_content("Email domains that are already whitelisted")
   end
 end
