4ace8d75b0076a7626b4c69c4d14461c0a4ab4d3
Tom Taylor <tommotaylor@gmail.com>
2019-04-12 10:48:46 +0100

    Bare steps for whitelisting flow

--- spec/features/super_admin/whitelists/whitelisting_process_spec.rb
+++ spec/features/super_admin/whitelists/whitelisting_process_spec.rb
@@ -17,4 +17,51 @@ describe 'Whitelisting an organisation', type: :feature do
     click_on "Users whitelist"
     expect(page).to have_content("Email domains that are already whitelisted")
   end
+
+  context "when stepping through the setup steps" do
+    it 'displays the second step' do
+      click_on 'Start'
+      expect(page).to have_content('Does the organisation need to create a GovWifi admin account?')
+    end
+
+    it 'displays the third step' do
+      click_on 'Start'
+      click_on 'Continue'
+      expect(page).to have_content("Is the organisation's name on the register?")
+    end
+
+    it 'displays the fourth step' do
+      click_on 'Start'
+      click_on 'Continue'
+      click_on 'Continue'
+      expect(page).to have_content('Add the organisation name to the register')
+    end
+
+    it 'displays the fifth step' do
+      click_on 'Start'
+      click_on 'Continue'
+      click_on 'Continue'
+      click_on 'Continue'
+      expect(page).to have_content("Add the organisation's email domain to the whitelist")
+    end
+
+    it 'displays the sixth step' do
+      click_on 'Start'
+      click_on 'Continue'
+      click_on 'Continue'
+      click_on 'Continue'
+      click_on 'Continue'
+      expect(page).to have_content("You've whitelisted the organisation to use GovWifi")
+    end
+
+    it 'displays the sixth step' do
+      click_on 'Start'
+      click_on 'Continue'
+      click_on 'Continue'
+      click_on 'Continue'
+      click_on 'Continue'
+      click_on 'Done'
+      expect(page).to have_content("Give an organisation access to GovWifi")
+    end
+  end
 end
