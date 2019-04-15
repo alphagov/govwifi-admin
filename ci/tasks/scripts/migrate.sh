d431c9afaecb03477a4436ad70217f24d8aa7e24
Anthony King <anthonydking@gmail.com>
2019-04-15 10:43:45 +0100

    Deploy with Concourse (#630)
    Flow is, on master branch:
    - Run tests + linting
    - On tests + lint success success:
       - push staging image
       - run staging migrations
       - deploy staging
       - on deploy staging success:
         - on manual input: 
           - push production image
           - run production migrations
           - deploy production
    
    Testing + linting is the same as the PR pipeline, and amounts to boilerplate around `make test lint`.
    
    Pushing the image utilises the concourse docker-image resource to build + push the ECR.
    
    Migrations are run using a variety of helper functions around the `aws-cli` tool to gather launch type + networking information (required due to Fargate usage).
    
    Deploys use the `aws-cli` tool to launch new task definitions

---
+++ ci/tasks/scripts/migrate.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ source govwifi-admin/ci/tasks/scripts/aws-helpers.sh
+
+function migrate() {
+  local migration_command="bundle exec rake db:migrate"
+  local docker_service_name="admin"
+  local cluster_name service_name task_definition docker_service_name deploy_stage
+
+  deploy_stage="$(stage_name)"
+  cluster_name="${deploy_stage}-admin-cluster"
+  service_name="admin-${deploy_stage}"
+  task_definition="admin-task-${deploy_stage}"
+
+  run_task_with_command \
+    "${cluster_name}" \
+    "${service_name}" \
+    "${task_definition}" \
+    "${docker_service_name}" \
+    "${migration_command}"
+}
+
+migrate
