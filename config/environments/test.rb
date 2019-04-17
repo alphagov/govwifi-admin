1a33e9b0e8ae7ab2698ad04d074dc6afa16851b3
Renny Fadoju <rennyvbirds@yahoo.com>
2019-04-17 11:17:16 +0100

    Don't force SSL in test environment?

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -3,6 +3,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.active_storage.service = :test
+  config.force_ssl = false
 
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
