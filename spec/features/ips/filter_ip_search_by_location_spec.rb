fe833d40f28fbff391008691fb9750b32d3f54ce
Renny Fadoju <rennyvbirds@yahoo.com>
2019-04-17 11:16:33 +0100

    Allow localhost and specific others

--- spec/features/ips/filter_ip_search_by_location_spec.rb
+++ spec/features/ips/filter_ip_search_by_location_spec.rb
@@ -6,11 +6,20 @@ describe 'filtering IP addresses by dynamic search' do
   let!(:location_three) { create(:location, organisation: organisation, address: 'Citrus Lane') }
 
   before do
+    # allowed_sites = [
+    #   "https://chromedriver.storage.googleapis.com",
+    #   "https://github.com/mozilla/geckodriver/releases",
+    #   "https://selenium-release.storage.googleapis.com",
+    #   "https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver"
+    # ]
+
+    WebMock.disable_net_connect!(allow_localhost: true, allow: ["app:55555", "selenium:4444", %r{github.com/mozilla/geckodriver/releases}])
+
     sign_in_user user
     visit ips_path
   end
 
-  context 'when user types in the search field' do
+  context 'when user types in the search field', js: true do
     it 'displays the location that closely matches the text field input' do
       fill_in 'filterInput', with: 'B'
       # page.execute_script(filterLocations())
