5e2ce91e1f15d0404360a4aabf82f42de89912d4
Renny Fadoju <rennyvbirds@yahoo.com>
2019-04-18 11:49:53 +0100

    Refactor test setup

--- spec/features/ips/filter_ips_by_location_spec.rb
+++ spec/features/ips/filter_ips_by_location_spec.rb
@@ -1,5 +1,7 @@
 describe 'filtering IP addresses by dynamic search', type: :feature do
 
+  Capybara.app_host = "http://app:3000"
+
   before do
     create(:location, organisation: organisation, address: 'Apple Street')
     create(:location, organisation: organisation, address: 'Banana Road')
@@ -11,49 +13,26 @@ describe 'filtering IP addresses by dynamic search', type: :feature do
   let!(:organisation) { create(:organisation) }
 
   let(:user) { create(:user, organisation: organisation) }
-  let(:filter_function_string) {
-    "(
-      function filterLocations(){
-        let filterValue = #{filter_value};
-        let table = document.getElementById('wrapper')
-        let location_name = table.querySelectorAll('table.govuk-table');
-        let ips = table.querySelectorAll('tbody#ips-table');
-
-        for(let i = 0;i < location_name.length;i++){
-          let text = location_name[i].getElementsByTagName('div')[0];
-
-          if(text.innerHTML.toUpperCase().indexOf(filterValue) > -1){
-            location_name[i].style.display = '';
-            ips[i].style.display = '';
-          } else {
-            location_name[i].style.display = 'none';
-            ips[i].style.display = 'none';
-          }
-        }
-      }
-    )"
-  }
-
-  it 'goes to the IPs page' do
-    expect(page).to have_current_path('/ips')
-  end
 
   context 'when user fills in the search bar', js: true do
-    Capybara.app_host = "http://app:3000"
 
     let(:filter_value) { 'b' }
 
-    before { page.driver.evaluate_script(filter_function_string) }
+    before { fill_in 'input#filterInput', with: filter_value }
 
-    it 'displays Banana Road since it has the letter b, in it' do
+    it 'goes to the IPs page' do
+      expect(page).to have_current_path('/ips')
+    end
+
+    it 'displays Banana Road as it has the letter b in it' do
       expect(page).to have_content('Banana Road')
     end
 
-    xit 'does not display Apple Street' do
+    it 'does not display Apple Street' do
       expect(page).not_to have_content('Apple Street')
     end
 
-    xit 'does not display Citrus Lane' do
+    it 'does not display Citrus Lane' do
       expect(page).not_to have_content('Citrus Lane')
     end
   end
