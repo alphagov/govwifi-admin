c100d4f5829fba39bb29c5d846e0b3aeca3d179d
Omorinsola Renny Fadoju <32823756+Renny4Real@users.noreply.github.com>
2019-04-30 15:11:43 +0100

    Update footer to match product page (#681)

--- spec/features/support/setup_instructions.rb
+++ spec/features/support/setup_instructions.rb
@@ -1,6 +1,6 @@
 shared_examples 'shows the setup instructions page' do
   it 'shows the user the setup instructions page' do
-    within("h2") do
+    within("#setup-header") do
       expect(page).to have_content("Get GovWifi access in your organisation")
     end
   end
