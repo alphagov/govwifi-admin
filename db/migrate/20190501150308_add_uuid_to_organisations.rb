3d51cb6ca37a39758321a129b4c5dda489ae827f
Emile <emile.swarts123@gmail.com>
2019-05-02 11:27:24 +0100

    Generate UUID in Ruby as mysql 5 doesn't allow functions as defaults

--- db/migrate/20190501150308_add_uuid_to_organisations.rb
+++ db/migrate/20190501150308_add_uuid_to_organisations.rb
@@ -1,5 +1,5 @@
 class AddUuidToOrganisations < ActiveRecord::Migration[5.2]
   def change
-    add_column :organisations, :uuid, :string, limit: 36, null: false, default: 'uuid()'
+    add_column :organisations, :uuid, :string, limit: 36, null: false
   end
 end
