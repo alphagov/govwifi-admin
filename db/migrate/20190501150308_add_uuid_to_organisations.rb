2e85bab86edfc1050c129037ef23efe63510f24e
Emile <emile.swarts123@gmail.com>
2019-05-02 10:21:29 +0100

    Add index to uuid on organisations table

--- db/migrate/20190501150308_add_uuid_to_organisations.rb
+++ db/migrate/20190501150308_add_uuid_to_organisations.rb
@@ -1,5 +1,6 @@
 class AddUuidToOrganisations < ActiveRecord::Migration[5.2]
   def change
     add_column :organisations, :uuid, :string, limit: 36, null: false, default: 'uuid()'
+    add_index :organisations, :uuid, unique: true
   end
 end
