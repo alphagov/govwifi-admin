72279f355670928043a1c862c606d2971266a268
Anthony King <anthony@madetech.com>
2019-05-08 14:58:28 +0100

    remove separate deploy pipeline
    The admin component is now deployed via the main deploy pipeline.

--- ci/tasks/scripts/deploy.sh
+++ ci/tasks/scripts/deploy.sh
@@ -2,14 +2,14 @@
 
 set -v -e -u -o pipefail
 
-source govwifi-admin/ci/tasks/scripts/aws-helpers.sh
+source deploy-tools/aws-helpers.sh
 
 function deploy() {
   local cluster_name service_name deploy_stage
 
   deploy_stage="$(stage_name)"
-  cluster_name="${deploy_stage}-admin-cluster"
-  service_name="admin-${deploy_stage}"
+  cluster_name="${deploy_stage}-${CLUSTER_NAME}-cluster"
+  service_name="${SERVICE_NAME}-${deploy_stage}"
 
   ecs_deploy \
     "${cluster_name}" \
