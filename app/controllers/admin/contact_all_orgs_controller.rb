49d80cb392b9233538ef26b3789aabc958fb5974
George Schena <george@madetech.com>
2019-05-10 16:18:42 +0100

    can download file

--- app/controllers/admin/contact_all_orgs_controller.rb
+++ app/controllers/admin/contact_all_orgs_controller.rb
@@ -1,10 +1,10 @@
 class Admin::ContactAllOrgsController < AdminController
   def index
-    @service_emails = Organisation.pluck(:service_email)
+    service_emails = Organisation.pluck(:service_email)
 
     respond_to do |format|
       format.html
-      format.csv { send_data @service_emails.to_csv, filename: "service_emails.csv" }
+      format.csv { send_data service_emails.to_csv, filename: "service_emails.csv" }
     end
   end
 end
--- spec/features/super_admin/download_all_service_emails_spec.rb
+++ spec/features/super_admin/download_all_service_emails_spec.rb
@@ -1,4 +1,4 @@
-describe 'Downloading all the service emails', type: :feature, focus: true do
+describe 'Downloading all the service emails', type: :feature do
   let(:user) { create(:user, :super_admin) }
 
   before do
@@ -8,5 +8,10 @@ describe 'Downloading all the service emails', type: :feature, focus: true do
 
   it 'will have the link to download all service emails' do
     expect(page).to have_content('Download all service emails')
+  end
+
+  it 'will download a csv file with all the service emails' do
+    click_on 'Download all service emails'
+    expect(page.response_headers['Content-Type']).to eq('text/csv')
   end
 end
