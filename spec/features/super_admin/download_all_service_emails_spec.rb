a85f7db3d7e6c5172af45b9e96b485428bdcfc7c
George Schena <40758489+georgeschena@users.noreply.github.com>
2019-05-13 13:57:57 +0100

    Download CSV file of all service emails (#709)

---
+++ spec/features/super_admin/download_all_service_emails_spec.rb
@@ -0,0 +1,17 @@
+describe 'Downloading all the service emails', type: :feature do
+  let(:user) { create(:user, :super_admin) }
+
+  before do
+    sign_in_user user
+    visit admin_organisations_path
+  end
+
+  it 'will have the link to download all service emails' do
+    expect(page).to have_content('Download all service emails')
+  end
+
+  it 'will download a csv file with all the service emails' do
+    click_on 'Download all service emails'
+    expect(page.response_headers['Content-Type']).to eq('text/csv')
+  end
+end
