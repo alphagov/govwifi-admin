90666197c047bb881cbc438bcf15d1c45f24ae5a
George Schena <george@madetech.com>
2019-05-14 12:12:15 +0100

    remove focus

--- spec/features/switch_organisation_spec.rb
+++ spec/features/switch_organisation_spec.rb
@@ -1,6 +1,6 @@
 require 'rack_session_access/capybara'
 
-describe 'Multiple organisations', type: :feature, focus: true do
+describe 'Multiple organisations', type: :feature do
   let(:organisation_1) { create(:organisation) }
   let(:organisation_2) { create(:organisation) }
   let(:user) { create(:user) }
