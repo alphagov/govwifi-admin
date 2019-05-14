f852d30098282778f8a23732a442bf58acac541b
George Schena <george@madetech.com>
2019-05-14 13:43:30 +0100

    inital high level test

---
+++ spec/features/add_organisation_as_existing_user_spec.rb
@@ -0,0 +1,9 @@
+require 'rack_session_access/capybara'
+
+describe 'Adding an organisation as exisiting user', type: :feature do
+  let(:organisation_1) { create(:organisation) }
+  let(:organisation_2) { create(:organisation) }
+  let(:user) { create(:user) }
+
+
+end
