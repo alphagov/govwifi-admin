260606dd56d23bfe8593169fa7621a604a735ea3
George Schena <george@madetech.com>
2019-05-20 10:27:51 +0100

    working

--- app/controllers/admin/locations_controller.rb
+++ app/controllers/admin/locations_controller.rb
@@ -1,5 +1,21 @@
 class Admin::LocationsController < AdminController
   def index
     @locations = Location.all
+    .order("#{sort_column} #{sort_direction}")
+  end
+
+private
+
+  def sortable_columns
+    %w[address]
+  end
+
+  def sort_column
+    sortable_columns.include?(params[:sort]) ? params[:sort] : "address"
+  end
+
+  def sort_direction
+    directions = %w[asc]
+    directions.include?(params[:direction]) ? params[:direction] : directions.first
   end
 end
--- spec/features/super_admin/search_locations_spec.rb
+++ spec/features/super_admin/search_locations_spec.rb
@@ -1,7 +1,7 @@
-describe 'View and search locations', type: :feature, focus: true do
+describe 'View and search locations', type: :feature do
   let(:user) { create(:user, :super_admin) }
   let(:organisation) { create(:organisation) }
-  let!(:location_1) { create(:location, address: '69 Garry Street, London', postcode: 'HA7 2BL', organisation: organisation) }
+  let(:location) { create(:location, address: '69 Garry Street, London', postcode: 'HA7 2BL', organisation: organisation) }
 
   before do
     sign_in_user user
@@ -13,9 +13,13 @@ describe 'View and search locations', type: :feature, focus: true do
     expect(page).to have_content("GovWifi locations")
   end
 
-  context 'view all the locations details' do
+  context 'with all the locations details' do
     it 'lists the full address of the location' do
       expect(page).to have_content("69 Garry Street, London, HA7 2BL")
+    end
+
+    it 'lists the organisation the location belongs to' do
+      expect(page).to have_content(organisation.name)
     end
   end
 end
