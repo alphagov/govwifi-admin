7fd3faf4dd3dfd53a3f85b7c92d52f4f70119ca0
George Schena <40758489+georgeschena@users.noreply.github.com>
2019-05-21 11:48:28 +0100

    Remove null false constraint migration (#740)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_21_092119) do
+ActiveRecord::Schema.define(version: 2019_05_21_104004) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -80,9 +80,9 @@ ActiveRecord::Schema.define(version: 2019_05_21_092119) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "organisation_id", null: false
-    t.string "invitation_token", null: false
+    t.string "invitation_token"
     t.bigint "user_id", null: false
-    t.integer "invited_by_id", null: false
+    t.integer "invited_by_id"
     t.datetime "confirmed_at"
     t.index ["organisation_id"], name: "index_memberships_on_organisation_id"
     t.index ["user_id"], name: "index_memberships_on_user_id"
