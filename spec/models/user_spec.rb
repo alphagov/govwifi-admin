461b3386b7cc784f6eb55196e6b06ace7525482a
George Schena <george@madetech.com>
2019-05-23 13:42:30 +0100

    testing user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,4 @@
-describe User do
+describe User, focus: true do
   it { is_expected.to have_many(:organisations).through(:memberships) }
   it { is_expected.to have_many(:memberships) }
   it { is_expected.to validate_presence_of(:name).on(:update) }
@@ -12,6 +12,15 @@ describe User do
 
     it 'receives default permission to manage locations' do
       expect(user).to be_can_manage_locations
+    end
+  end
+
+  context 'when checking if a membership is pending' do
+    let!(:organisation) { create(:organisation) }
+    subject(:user) { create(:user, organisations: [organisation]) }
+
+    it 'returns the confirmed_at attribute' do
+      expect(user.pending_membership_for?(organisation: organisation)).to eq('bob')
     end
   end
 end
