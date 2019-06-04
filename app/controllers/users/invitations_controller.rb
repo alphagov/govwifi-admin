7a9d68a72af4354659d39f998ad217a5c7d74aaf
Tom Taylor <tommotaylor@gmail.com>
2019-06-04 15:28:23 +0100

    Fix duplicate email sent on first invite (#766)

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -22,22 +22,29 @@ class Users::InvitationsController < Devise::InvitationsController
       return
     end
 
-    invite_user(organisation)
+    add_user_to_organisation(organisation)
 
     redirect_to(after_path(organisation), notice: "#{invited_user.email} has been invited to join #{organisation.name}")
   end
 
 private
 
-  def invite_user(organisation)
+  def add_user_to_organisation(organisation)
     membership = invited_user.memberships.find_or_create_by(invited_by_id: current_user.id, organisation: organisation)
     membership.update_attributes(can_manage_team: params[:can_manage_team], can_manage_locations: params[:can_manage_locations])
-
-    send_invitation_email(membership)
+    send_invite_email(membership) if user_has_confirmed_account?
   end
 
-  def send_invitation_email(membership)
-    AuthenticationMailer.membership_instructions(invited_user, membership.invitation_token, organisation: current_organisation).deliver_now
+  def send_invite_email(membership)
+    AuthenticationMailer.membership_instructions(
+      invited_user,
+      membership.invitation_token,
+      organisation: current_organisation
+    ).deliver_now
+  end
+
+  def user_has_confirmed_account?
+    invited_user.confirmed?
   end
 
   def after_path(organisation)
