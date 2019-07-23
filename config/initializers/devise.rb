8ea18b745c3daf28d3ae77ac273c80a9013be30b
Steve Laing <steve.laing@gmail.com>
2019-07-23 16:30:17 +0100

    Enforce 2FA every 24 hours

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -337,10 +337,10 @@ Devise.setup do |config|
   config.max_login_attempts = 3 # Maximum second factor attempts count.
   config.allowed_otp_drift_seconds = 30 # Allowed TOTP time drift between client and server.
   config.otp_length = 6 # TOTP code length
-  config.remember_otp_session_for_seconds = 30.days # Time before browser has to perform 2fA again. Default is 0.
+  config.remember_otp_session_for_seconds = 86400 # Time before browser has to perform 2fA again. Default is 0.
   config.otp_secret_encryption_key = ENV['OTP_SECRET_ENCRYPTION_KEY']
   config.second_factor_resource_id = 'id' # Field or method name used to set value for 2fA remember cookie
-  config.delete_cookie_on_logout = true # Delete cookie when user signs out, to force 2fA again on login
+  config.delete_cookie_on_logout = false # Delete cookie when user signs out, to force 2fA again on login
 end
 
 # rubocop:enable Metrics/BlockLength
