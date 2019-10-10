1a9508bcb3a4a0185e1a8e059b63ee611337b17b
Robin Mitra <robin.mitra@digital.cabinet-office.gov.uk>
2019-10-10 18:43:39 +0100

    Implement a warning on the overview screen if an organisation has not yet signed the MOU.

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -3,5 +3,6 @@ class OverviewController < ApplicationController
     @ips = current_organisation.ips
     @locations = current_organisation.locations
     @team_members = current_organisation.users
+    @current_org_signed_mou = current_organisation.signed_mou.attachment
   end
 end
