a19b3e9fb3aa434238fb4e475d19df43dcce687f
Stéphane Maniaci <stephane.maniaci@digital.cabinet-office.gov.uk>
2019-11-04 12:20:30 +0000

    fix: wrap user permissions with cancancan
    The two possible permissions are :administrate and :manage, in order
    of importance. I initially wanted to have it the other way around (i.e
    regular users get to `:manage` their organisation whilst we
    `:administrate` GovWifi in general) but it turns out CanCanCan
    reserves[1] the keyword to represent all permissions, so :manage would
    effectively override :administrate.
    
    [1]: https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities

---
+++ spec/models/ability_spec.rb
@@ -0,0 +1,40 @@
+describe Ability do
+  context 'when there is no user logged in' do
+    let(:ability) { described_class.new(nil) }
+
+    it 'cannot administrate' do
+      expect(ability.can?(:administrate, :all)).to be false
+    end
+
+    it 'cannot manage' do
+      expect(ability.can?(:manage, :all)).to be false
+    end
+  end
+
+  context 'when a user is logged in' do
+    let(:user) { create(:user) }
+    let(:ability) { described_class.new(user) }
+
+    context 'with a normal account' do
+      it 'can administrate' do
+        expect(ability.can?(:administrate, :all)).to be true
+      end
+
+      it 'cannot manage' do
+        expect(ability.can?(:manage, :all)).to be false
+      end
+    end
+
+    context 'with a superadmin account' do
+      let(:user) { create(:user, :super_admin) }
+
+      it 'can manage' do
+        expect(ability.can?(:manage, :all)).to be true
+      end
+
+      it 'can administrate' do
+        expect(ability.can?(:administrate, :all)).to be true
+      end
+    end
+  end
+end
