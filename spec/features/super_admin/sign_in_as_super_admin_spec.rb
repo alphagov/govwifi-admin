f37dd0b4ca3fd9f2af69316914e0b506718b5846
Stéphane Maniaci <stephane.maniaci@digital.cabinet-office.gov.uk>
2019-11-05 11:55:53 +0000

    fix: add a way for classic admins to access the new dashboard

--- spec/features/super_admin/sign_in_as_super_admin_spec.rb
+++ spec/features/super_admin/sign_in_as_super_admin_spec.rb
@@ -16,6 +16,12 @@ describe 'Signing in as a super admin', type: :feature do
       expect(page).to have_content 'Whitelist'
     end
 
+    it 'has a way to access the new dashboard' do
+      page.click_on 'Manage GovWifi'
+
+      expect(page).to have_content 'new dashboard'
+    end
+
     context 'when visiting a normal organisation' do
       before do
         user.organisations << organisation
