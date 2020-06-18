ea2ec7444cdb85bf0697b17849c9e59ecb6434d0
Robin Mitra <robin.mitra@digital.cabinet-office.gov.uk>
2020-06-18 19:13:20 +0100

    Improve JS password validation feedback and error handling.
    This now also presents a password score to users, which would make it much easier for users to know how much
    stronger a password needs to be, before being acceptable.

--- app/assets/javascripts/password_validation.js
+++ app/assets/javascripts/password_validation.js
@@ -1,24 +1,54 @@
-$("#user_password").on("ajax:success", (event, xhr) => {
-  const { score } = xhr;
-  const suggestions = xhr.suggestions.length
-    ? xhr.suggestions
-    : "Choose a password that is harder to guess";
+const getSuggestionHtml = suggestions => {
+  const headingHtml = `
+    <span class="govuk-error-message govuk-!-margin-bottom-1">
+        Choose a password that is harder to guess${suggestions.length ? ':' : ''}
+    </span>
+  `;
+  const suggestionListHtml = `
+    <ul class="govuk-error-message govuk-list govuk-list--bullet">
+        ${suggestions.map(s => `<li>${s}</li>`).join('')}
+    </ul>
+  `;
+  return suggestions.length ? `${headingHtml}${suggestionListHtml}` : headingHtml;
+};
 
+const getScoreHtml = score => {
+  const scoreText = score < 2 ? 'weak' : (score < 4 ? 'medium' : 'good');
+  return `Password strength: <span class="password-strength-score">${scoreText}</span>`;
+};
+
+const updateScore = score => $('.password-strength').html(getScoreHtml(score));
+
+const updateError = suggestions => {
+  $('#password').addClass('govuk-form-group--error');
+  $('#password')
+    .find('input')
+    .addClass('govuk-input--error');
+  $('.password-error-message')
+    .css('display', 'inline-block')
+    .html(`<span class="govuk-visually-hidden">Error:</span> ${getSuggestionHtml(suggestions)}`);
+};
+
+const removeError = () => {
+  $('#password').removeClass('govuk-form-group--error');
+  $('#password')
+    .find('input')
+    .removeClass('govuk-input--error');
+  $('.govuk-error-message').css('display', 'none');
+};
+
+$('#user_password').on('ajax:success', event => {
+  const detail = event.detail;
+  const [data, status] = detail;
+  if (!data || typeof data !== 'object' || status !== 'OK') return;
+  const { score, suggestions } = data;
+  if (typeof score !== 'number') return;
+  updateScore(score);
   if (score < 4) {
-    $("#password").addClass("govuk-form-group--error");
-    $("#password")
-      .find("input")
-      .addClass("govuk-input--error");
-    $(".govuk-error-message")
-      .css("display", "inline-block")
-      .html(`<span class="govuk-visually-hidden">Error:</span> ${suggestions}`);
-  } else {
-    $("#password").removeClass("govuk-form-group--error");
-    $("#password")
-      .find("input")
-      .removeClass("govuk-input--error");
-    $(".govuk-error-message").css("display", "none");
+    updateError(suggestions);
+    return;
   }
+  removeError();
 });
 
 $("#user_password_visibility_toggle").click(handleVisibilityButton);
