9af3eddbaec666c3303bad725268c1bbd1a05332
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2020-07-29 16:10:01 +0100

    Split up the old and new tfa logic in the application controller
    Use two callbacks instead of one to improve readability.
    
    Remove the redirect to the email handler because it would cause
    a tfa-d user to always redirect.

--- app/controllers/users/two_factor_authentication/email_controller.rb
+++ app/controllers/users/two_factor_authentication/email_controller.rb
@@ -3,6 +3,7 @@ class Users::TwoFactorAuthentication::EmailController < ApplicationController
   skip_before_action :handle_two_factor_authentication
   # Skips 2FA setup confirmation callback in ApplicationController.
   skip_before_action :confirm_two_factor_setup
+  skip_before_action :choose_two_factor_method
 
   def show
     if params[:resend] == "true"
