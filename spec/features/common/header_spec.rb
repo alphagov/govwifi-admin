382ffc797cae7449b2304b1c17c6b54d41152f61
Colin Burn-Murdoch <colin.burn-murdoch@digital.cabinet-office.gov.uk>
2021-09-07 11:17:44 -0400

    Use different header/label when on non-production

---
+++ spec/features/common/header_spec.rb
@@ -0,0 +1,22 @@
+describe "Page header", type: :feature do
+  context "when not on production" do
+    before do
+      visit root_path
+    end
+
+    it "contains the expected test tag" do
+      expect(page).to have_content("GOV.UK GovWifi test")
+    end
+  end
+
+  context "when on production" do
+    before do
+      allow(Rails.env).to receive(:production?).and_return(true)
+      visit root_path
+    end
+
+    it "contains the expected beta tag" do
+      expect(page).to have_content("GOV.UK GovWifi beta")
+    end
+  end
+end
