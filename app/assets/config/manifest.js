174cc31b8fcfb016fedfab96f2852822688a018e
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2021-09-13 20:47:13 +0100

    Unpin Sprockets gem and upgrade to version 4.
    We had this pinned to version 3.7.2 because of a segmentation
    fault that occurred when upgrading to 4.0+
    
    After investigation it seems that Sprockets now uses the manifest.js
    file to compile assets instead of the assets.rb file.
    
    See: https://github.com/sass/sassc-rails/issues/122 and
    https://github.com/rails/sprockets/blob/master/UPGRADING.md#manifestjs
    
    This commit upgrades Sprockets and now uses the manifest.js file
    to precompile assets which ensures the segmentation fault no
    longer occurs.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_directory ../javascripts .js
+//= link accessible-autocomplete.min.css
+//= link html5shiv.js
+//= link govuk-logotype-crown.png
