f7fa5253c26071f981d4f623f630004e72b0023e
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2022-03-16 13:20:08 +0000

    Move redirection of users without organisations to application_controller
    This method has nothing to do with authentication and is better located in
    the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   include Pagy::Backend
   include GovWifiAuthenticatable
 
+  before_action :redirect_user_without_organisation, unless: :current_action_is_valid?
   helper_method :current_organisation, :super_admin?
   helper_method :sidebar
   helper_method :subnav
@@ -20,6 +21,13 @@ class ApplicationController < ActionController::Base
     current_user&.is_super_admin?
   end
 
+  def redirect_user_with_no_organisation
+    if !current_user&.is_super_admin? && current_user&.organisations&.empty?
+      msg = "You do not belong to an organisation. Please mention this in your support request."
+      redirect_to signed_in_new_help_path, notice: msg
+    end
+  end
+
 protected
 
   def subnav
@@ -32,6 +40,10 @@ protected
 
   def authorise_manage_locations
     redirect_to(root_path) unless current_user.can_manage_locations?(current_organisation)
+  end
+
+  def current_action_is_valid?
+    devise_controller? || (controller_name == "help" && valid_help_actions.include?(action_name))
   end
 
   def valid_help_actions
