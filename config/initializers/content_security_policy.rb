dcdbe7c8400fa1ddc566f96e6dd090da6745d952
Jim Narey <jimnarey@me.com>
2022-05-16 16:37:06 +0100

    Add former flexibility in JS execution defaulted-out by new CSP
    The addition of a Content Security Policy prevents, by default,
    the execution of inline scripts from other domains and the
    evalution of strings as JS. The intention of the CSP (for now)
    was simply to restrict which domains scripts could be downloaded
    and executed from.
    
    This commit re-introduces the ability to do both.

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,3 +1,3 @@
 Rails.application.config.content_security_policy do |policy|
-  policy.script_src :self, "https://www.googletagmanager.com", "https://www.google-analytics.com"
+  policy.script_src :self, "https://www.googletagmanager.com", "https://www.google-analytics.com", :unsafe_eval, :unsafe_inline
 end
