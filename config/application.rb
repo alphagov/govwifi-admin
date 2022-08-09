613e4f45cf3f46ca33ee271b786d6aec361232ee
koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2022-08-09 11:44:17 +0100

    Set the default configuration to Rails 7
    This gets rid of the warning that legacy connection handling is
    deprecated

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ Bundler.require(*Rails.groups)
 module GovwifiAdmin
   class Application < Rails::Application
     config.exceptions_app = routes
-    config.load_defaults 6.0
+    config.load_defaults 7.0
     config.action_view.form_with_generates_remote_forms = false
     config.autoloader = :zeitwerk
 
