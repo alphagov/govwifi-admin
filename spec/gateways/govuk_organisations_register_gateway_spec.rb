ab26455708b252c56f725774e2f0ca6b54c64dc6
koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2022-08-10 13:58:01 +0100

    Remove fetch_organisation_register
    This class does nothing and can be removed

--- spec/gateways/govuk_organisations_register_gateway_spec.rb
+++ spec/gateways/govuk_organisations_register_gateway_spec.rb
@@ -11,5 +11,12 @@ describe Gateways::GovukOrganisationsRegisterGateway do
       result = register_gateway.custom_orgs
       expect(result).to eq(["Custom Org 1", "Custom Org 2"])
     end
+
+    it "fetches all organisation names" do
+      result = register_gateway.all_orgs
+      expect(result).to include("Gov Org 1")
+      expect(result).to include("Custom Org 1")
+      expect(result).to include("Local Auth 1")
+    end
   end
 end
