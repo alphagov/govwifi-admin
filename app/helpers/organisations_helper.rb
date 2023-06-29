75fe903282ce3f5c473b9ebcf8c1be3768af0379
A1Robson <aaron.robson@digital.cabinet-office.gov.uk>
2023-06-29 08:36:24 +0000

    Changes to Organisations default sort order

--- app/helpers/organisations_helper.rb
+++ app/helpers/organisations_helper.rb
@@ -2,7 +2,13 @@ module OrganisationsHelper
   def sort_link(column, title = nil)
     title ||= column.titleize
     css_class = column == sort_column ? "current #{sort_direction}" : nil
-    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+
+    direction = if column == "name"
+                  column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+                else
+                  column == sort_column && sort_direction == "desc" ? "asc" : "desc"
+                end
+
     link_to title, { sort: column, direction: }, class: css_class
   end
 end
