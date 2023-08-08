c253f06a1794f81c429365901b40768120c047b2
A1Robson <aaron.robson@digital.cabinet-office.gov.uk>
2023-08-08 13:00:48 +0000

    update to user and wifi user searching

--- app/models/wifi_user.rb
+++ app/models/wifi_user.rb
@@ -3,11 +3,9 @@ class WifiUser < ApplicationRecord
   self.table_name = "userdetails"
 
   def self.search(search_term)
-    is_email = search_term =~ Devise.email_regexp
-    is_phone = search_term =~ /^[\d]{3,}$/
-
-    search_attr = is_email || is_phone ? :contact : :username
-
-    find_by("#{search_attr} like ?", "%#{search_term}%")
+    find_by(:contact => search_term) || 
+    find_by(:username => search_term) || 
+    find_by("contact like ?", "%#{search_term}%") || 
+    find_by("username like ?", "%#{search_term}%")    
   end
 end
