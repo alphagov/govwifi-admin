c1e0dc73c2ca144df7d5b362c6ed0e7335f9a85e
koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2024-05-07 16:09:28 +0100

    Update and refactor certificate specs to use new factory.
    Instead of using complex helpers, we can use the new certificate factories

--- spec/models/certificate_spec.rb
+++ spec/models/certificate_spec.rb
@@ -1,85 +1,53 @@
 describe Certificate do
-  let(:name) { "mycert" }
-  let(:content) { "A certificate" }
-  let(:fingerprint) { "ABC123" }
-  let(:certificate_subject) { "/CN=RootCA" }
-  let(:issuer) { "/CN=RootCA" }
-  let(:not_before) { Time.zone.now }
-  let(:not_after) { Time.zone.now.days_since(4 * 7) }
-  let(:serial) { "12345" }
-
-  subject(:certificate) do
-    build(:certificate, :with_organisation,
-          name:,
-          content:,
-          fingerprint:,
-          subject: certificate_subject,
-          issuer:,
-          not_before:,
-          not_after:,
-          serial:)
-  end
-
-  it { is_expected.to validate_presence_of(:name) }
-  it { is_expected.to validate_presence_of(:content) }
-  it { is_expected.to validate_presence_of(:fingerprint) }
-  it { is_expected.to validate_presence_of(:subject) }
-  it { is_expected.to validate_presence_of(:issuer) }
-  it { is_expected.to validate_presence_of(:not_before) }
-  it { is_expected.to validate_presence_of(:not_after) }
-  it { is_expected.to validate_presence_of(:serial) }
-  it { is_expected.to belong_to(:organisation) }
+  let(:next_week) { Time.zone.now.days_since(7) }
+  let(:last_week) { Time.zone.now.days_ago(7) }
+  let(:key) { OpenSSL::PKey::RSA.new(512) }
+  let(:root_subject) { "/CN=root" }
+  let(:organisation) { create(:organisation) }
 
   it "is valid" do
-    expect(certificate).to be_valid
+    expect(create(:certificate, key:, subject: root_subject)).to be_valid
+    expect(create(:certificate, issuing_subject: root_subject, issuing_key: key)).to be_valid
   end
 
-  context "when loading an intermediate certificate file" do
-    let(:certificate_subject) { "/CN=intermediate" }
+  describe "#root_cert?" do
     it "is not a root certificate" do
-      expect(certificate).to_not be_root_cert
+      create(:certificate, key:, subject: root_subject)
+      expect(create(:certificate, issuing_subject: root_subject, issuing_key: key)).to_not be_root_cert
     end
-  end
 
-  context "when loading a root certificate file" do
     it "is a root certificate" do
-      expect(certificate).to be_root_cert
+      expect(create(:certificate, key:, subject: root_subject)).to be_root_cert
     end
   end
 
   describe "#has_expired" do
-    context "expired" do
-      let(:not_after) { Time.zone.now.days_ago(7) }
-      it "is expired" do
-        expect(certificate).to be_expired
-      end
+    it "is expired" do
+      expect(create(:certificate, not_after: last_week)).to be_expired
     end
-    context "not expired" do
-      it "is not expired" do
-        expect(certificate).to_not be_expired
-      end
+    it "is not expired" do
+      expect(create(:certificate, not_after: next_week)).to_not be_expired
     end
   end
 
   describe "#nearly_expired?" do
-    let(:not_after) { Time.zone.now.days_since(7) }
     it "is nearly expired" do
-      expect(certificate.nearly_expired?(14)).to be true
+      expect(create(:certificate, not_after: next_week).nearly_expired?(14)).to be true
     end
     it "is not nearly expired" do
-      expect(certificate.nearly_expired?(3)).to be false
+      expect(create(:certificate, not_after: next_week).nearly_expired?(3)).to be false
     end
   end
 
   describe "#not_yet_valid?" do
-    context "not yet valid" do
-      let(:not_before) { Time.zone.now.days_since(7) }
-      it "is not yet valid" do
-        expect(certificate).to be_not_yet_valid
-      end
+    it "is not yet valid" do
+      expect(create(:certificate, not_before: next_week)).to be_not_yet_valid
     end
     it "is already valid" do
       expect(certificate).to_not be_not_yet_valid
+      expect(create(:certificate, not_before: last_week)).to_not be_not_yet_valid
+    end
+  end
     end
   end
 end
