19da07fba6d1d0c69e1bb9120def02ef8afb4329
koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2024-05-08 09:45:53 +0100

    Add screen to edit the name of a certificate

---
+++ spec/requests/certificates/edit_spec.rb
@@ -0,0 +1,18 @@
+describe "GET /certificates/new", type: :request do
+  let(:organisation) { create(:organisation, :with_cba_enabled) }
+  let(:certificate) { create(:certificate, organisation:) }
+  let(:user) { create(:user, organisations: [organisation]) }
+  subject(:perform) { get edit_certificate_path(certificate) }
+  before :each do
+    https!
+    sign_in_user(user)
+  end
+
+  include_examples "cba flag and location permissions"
+
+  it "successfully renders the edit certificate template" do
+    perform
+    expect(response).to have_http_status(:success)
+    expect(response).to render_template(:edit)
+  end
+end
---
+++ spec/requests/certificates/patch_spec.rb
@@ -0,0 +1,32 @@
+describe "GET /certificates/edit/:id", type: :request do
+  let(:organisation) { create(:organisation, :with_cba_enabled) }
+  let(:user) { create(:user, organisations: [organisation]) }
+  let(:certificate) { create(:certificate, name: "OldName", organisation:) }
+  let(:new_name) { "NewName" }
+  let(:perform) { patch certificate_path(certificate), params: { certificate: { name: new_name } } }
+  before :each do
+    https!
+    sign_in_user(user)
+  end
+
+  include_examples "cba flag and location permissions"
+  include_examples "user is not a member of the certificate's organisation"
+
+  describe "a valid change" do
+    it "redirects to the index page" do
+      perform
+      expect(response).to redirect_to(certificates_path)
+    end
+    it "updates the certificate" do
+      expect { perform }.to change { Certificate.find(certificate.id).name }.from("OldName").to(new_name)
+    end
+  end
+  describe "an invalid change" do
+    let(:new_name) { "" }
+    it "renders the edit page with an error" do
+      perform
+      expect(response).to render_template(:edit)
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
+end
