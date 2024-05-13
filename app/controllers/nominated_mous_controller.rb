76f1dd2fd9d505876c6d202052cf6a35ff6774f4
Iram S Tahraoui <34037863+iram-shehzadi@users.noreply.github.com>
2024-05-13 23:17:31 +0100

    handling nominations for the mou

---
+++ app/controllers/nominated_mous_controller.rb
@@ -0,0 +1,54 @@
+class NominatedMousController < ApplicationController
+  skip_before_action :authenticate_user!
+  before_action :set_nomination, only: %i[new create]
+  before_action :check_token, only: %i[new create]
+
+  def new
+    @token = token_from_params
+    @mou_form = MouForm.new
+  end
+
+  def create
+    @token = token_from_params
+    @mou_form = MouForm.new(mou_params)
+    if @mou_form.invalid?
+      render :new
+    else
+      mou = @mou_form.save!(organisation: @nomination.organisation)
+      @nomination.destroy!
+      send_thank_you_email(mou)
+      redirect_to confirm_nominated_mous_path(organisation_name: @nomination.organisation_name), success: "MOU signed successfully."
+    end
+  end
+
+  def confirm
+    @organisation_name = params[:organisation_name]
+  end
+
+private
+
+  def check_token
+    redirect_to root_path, error: "Invalid token." if @nomination.nil?
+  end
+
+  def token_from_params
+    params[:token] || params.dig(:mou_form, :token)
+  end
+
+  def set_nomination
+    @nomination = Nomination.find_by(token: token_from_params) if token_from_params.present?
+  end
+
+  def mou_params
+    params.require(:mou_form).permit(:name, :email_address, :job_role, :signed)
+  end
+
+  def send_thank_you_email(mou)
+    AuthenticationMailer.thank_you_for_signing_the_mou(
+      mou.name,
+      mou.email_address,
+      mou.organisation.name,
+      mou.created_at,
+    ).deliver_now
+  end
+end
---
+++ spec/features/mou/nominee_mou_signature_spec.rb
@@ -0,0 +1,50 @@
+describe "Nominated user signs the MOU", type: :feature do
+  include EmailHelpers
+
+  let(:token) { "tZPAY6puVsNpxkUE9sqZ" }
+  let(:organisation) { create(:organisation) }
+  let(:email_gateway) { spy }
+
+  context "when a user follows the URL from their email containing the nomination token" do
+    before do
+      create(:nomination, name: "Maryan Khan", email: "Maryan.Khan@email.gov.uk", organisation:, token:)
+      visit new_nominated_mou_path(token:)
+    end
+
+    it "directs the user to the MOU form" do
+      expect(page).to have_content("Read and Accept the Terms for GovWifi")
+    end
+
+    it "displays the organisation's terms and conditions for acceptance" do
+      expect(page).to have_content("I confirm that I have the authority to accept these terms and that #{organisation.name} will be bound by them.")
+    end
+
+    context "when the user signs the MOU" do
+      let(:email_gateway) { spy }
+
+      before do
+        allow(Services).to receive(:email_gateway).and_return(email_gateway)
+        fill_in "Name", with: "Maryan Khan"
+        fill_in "Email", with: "Maryan.Khan@email.gov.uk"
+        fill_in "Job role", with: "HR"
+        check "I confirm that I have the authority to accept these terms and that #{organisation.name} will be bound by them."
+        click_button "Accept the MOU"
+      end
+
+      it "creates a signature for the MOU when terms are accepted" do
+        expect(Mou.exists?(user: nil)).to be_truthy
+        expect(Mou.last.job_role).to eq "HR"
+        expect(Mou.last.version).to eq Mou.latest_known_version
+      end
+
+      it "sends a confirmation email upon MOU signature" do
+        it_sent_a_thank_you_for_signing_the_mou_email_once
+      end
+    end
+
+    it "does not create a MOU signature when fields are not filled in and terms are not accepted" do
+      click_button "Accept the MOU"
+      expect(Mou.exists?(user: nil)).to be_falsey
+    end
+  end
+end
