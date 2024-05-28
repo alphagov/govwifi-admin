487b5ede0ef8cf470089316fd560759c16babac8
koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2024-05-28 13:06:26 +0100

    Enable the S3 Gateway to write StringIO objects

--- spec/gateways/s3_spec.rb
+++ spec/gateways/s3_spec.rb
@@ -6,6 +6,11 @@ describe Gateways::S3 do
     expect(Gateways::S3.new(bucket: "bucket", key: "key").read).to eq("Hello world")
   end
 
+  it "can write StringIO as well" do
+    Gateways::S3.new(bucket: "bucket", key: "key").write(StringIO.new("Hello world"))
+    expect(Gateways::S3.new(bucket: "bucket", key: "key").read).to eq("Hello world")
+  end
+
   it "it does not read what has not been written" do
     Gateways::S3.new(bucket: "bucket", key: "key").write("Hello world")
     expect(Gateways::S3.new(bucket: "bucket", key: "another-key").read).to be_empty
