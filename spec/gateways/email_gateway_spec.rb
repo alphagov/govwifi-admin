4cece5e7ce237f419c77631a0a752b9a81791aed
koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2024-06-26 19:55:20 +0100

    Allow the default URL options to be set by an environment variable
    The default URL options are needed in the action mailer.

---
+++ spec/gateways/email_gateway_spec.rb
@@ -0,0 +1,49 @@
+require "support/notifications_service"
+
+describe Gateways::EmailGateway do
+  subject(:email_gateway) { described_class.new }
+
+  let(:notification) { instance_spy(Notifications::Client, send_email: nil) }
+  let(:notify_templates) do
+    [instance_double("Notifications::Client::Template", id: "id_one", name: "name_one"),
+     instance_double("Notifications::Client::Template", id: "id_two", name: "name_two")]
+  end
+
+  before do
+    ENV["NOTIFY_API_KEY"] = "dummy_key-00000000-0000-0000-0000-000000000000-00000000-0000-0000-0000-000000000000"
+    allow(Notifications::Client).to receive(:new).and_return(notification)
+  end
+
+  context "when sending a confirmation email" do
+    let(:email) { "test@example.com" }
+    let(:confirmation_url) { "http://example.com/confirm?token=123" }
+    let(:template_id) { 1 }
+    let(:reference) { "confirmation_email" }
+    let(:notifications_payload) do
+      {
+        email_address: "test@example.com",
+        template_id: 1,
+        personalisation: { confirmation_url: },
+        reference: "confirmation_email",
+      }
+    end
+
+    before do
+      email_gateway.send_email(
+        email:,
+        template_id:,
+        locals: { confirmation_url: },
+        reference:,
+      )
+    end
+
+    it "calls the Notify client" do
+      expect(notification).to have_received(:send_email).with(notifications_payload)
+    end
+
+    it "gets all templates" do
+      allow(notification).to receive(:get_all_templates).and_return(notify_templates)
+      expect(email_gateway.all_templates).to eq("name_one" => "id_one", "name_two" => "id_two")
+    end
+  end
+end
