c7787559b490fd7094bf27d880f8c0692b143c75
koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2024-07-15 10:20:15 +0100

    Use DEPLOY_ENV instead of APP_ENV to indicate the platform to deploy on
    APP_ENV overrides RAILS_ENV and is therefore a bad choice for this.

--- lib/deploy.rb
+++ lib/deploy.rb
@@ -2,14 +2,14 @@
 
 module Deploy
   def self.production?
-    ENV["APP_ENV"] == "production"
+    ENV["DEPLOY_ENV"] == "production"
   end
 
   def self.env
-    ENV["APP_ENV"] || "production"
+    ENV["DEPLOY_ENV"] || "production"
   end
 
   def self.env=(env)
-    ENV["APP_ENV"] = env
+    ENV["DEPLOY_ENV"] = env
   end
 end
