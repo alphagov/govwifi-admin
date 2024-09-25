2f5a297ba503c15dd600c3a5221f2f3b61e7baff
koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2024-09-25 16:19:56 +0100

    Add template check to healthcheck
    The healthcheck now includes a call to check if all notify
    templates are defined.

--- app/controllers/monitoring_controller.rb
+++ app/controllers/monitoring_controller.rb
@@ -2,6 +2,7 @@ class MonitoringController < ApplicationController
   skip_before_action :authenticate_user!
 
   def healthcheck
+    NotifyTemplates.verify_templates
     render body: "Healthy"
   end
 end
--- spec/notify_templates_spec.rb
+++ spec/notify_templates_spec.rb
@@ -1,21 +1,46 @@
 describe NotifyTemplates do
+  let(:template_one) { NotifyTemplates::TEMPLATES.first }
+  let(:template_two) { NotifyTemplates::TEMPLATES.last }
+  let(:templates) do
+    [
+      instance_double(Notifications::Client::Template, name: template_one, id: "template_one_id"),
+      instance_double(Notifications::Client::Template, name: "unused", id: "unused"),
+      instance_double(Notifications::Client::Template, name: template_two, id: "template_two_id"),
+    ]
+  end
+  before do
+    template_collection = instance_double(Notifications::Client::TemplateCollection, collection: templates)
+    client = instance_double(Notifications::Client, get_all_templates: template_collection)
+    allow(Services).to receive(:notify_gateway).and_return(client)
+  end
   describe "#template" do
-    context "with stubbed gateway" do
-      before do
-        templates = [instance_double(Notifications::Client::Template, name: "unused", id: "unused")]
-        template_collection = instance_double(Notifications::Client::TemplateCollection, collection: templates)
-        allow(Services).to receive(:notify_gateway).and_return(template_collection)
-      end
-      it "filters out unused templates" do
-        expect { NotifyTemplates.template("unused") }.to raise_error(KeyError)
-      end
+    it "filters out unused templates" do
+      expect { NotifyTemplates.template("unused") }.to raise_error(KeyError)
     end
     it "fetches a template" do
-      expect(NotifyTemplates.template("confirmation_email")).to eq("confirmation_email_template")
-      expect(NotifyTemplates.template("reset_password_email")).to eq("reset_password_email_template")
+      expect(NotifyTemplates.template(template_one)).to eq("template_one_id")
+      expect(NotifyTemplates.template(template_two)).to eq("template_two_id")
     end
     it "accepts symbols" do
-      expect(NotifyTemplates.template(:confirmation_email)).to eq("confirmation_email_template")
+      expect(NotifyTemplates.template(template_one.to_sym)).to eq("template_one_id")
+    end
+  end
+  describe "#verify_templates" do
+    let(:templates) do
+      NotifyTemplates::TEMPLATES.each_with_index.map do |template, index|
+        instance_double(Notifications::Client::Template, name: template, id: index.to_s)
+      end
+    end
+    it "verifies all templates" do
+      expect {
+        NotifyTemplates.verify_templates
+      }.to_not raise_error
+    end
+    it "has a missing template" do
+      missing_template = templates.delete_at(0)
+      expect {
+        NotifyTemplates.verify_templates
+      }.to raise_error(StandardError, /Some templates have not been defined in Notify: #{missing_template.name}/)
     end
   end
 end
