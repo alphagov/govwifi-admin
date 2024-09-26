88b469b260ca793a57d0890691a00e0241544139
koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2024-09-26 16:42:24 +0100

    Remove notify service mocking where possible
    It is simpler and more consistent to use the spy that is already there
    
    Ensure the NotifyTemplates class is reloaded before each class to
    ensure the templates are reloaded fresh each time a test is run

--- spec/features/team/invite_with_permissions_spec.rb
+++ spec/features/team/invite_with_permissions_spec.rb
@@ -5,7 +5,6 @@ describe "Set user permissions on invite", type: :feature do
   let(:invited_user) { User.find_by(email: invited_email) }
 
   before do
-    allow(Services).to receive(:notify_gateway).and_return(spy)
     sign_in_user user
     visit new_user_invitation_path
     fill_in "Email", with: invited_email
